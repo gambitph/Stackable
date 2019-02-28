dee954fee14d563320ff84f598262ea3c32ab41b
Benjamin Intal <bfintal@gmail.com>
2019-03-01 02:43:11 +0800

    Removed react-id-swiper dependency

--- src/components/pro-modal/index.js
+++ src/components/pro-modal/index.js
@@ -1,80 +1,103 @@
 import { __ } from '@wordpress/i18n'
+import { Component } from '@wordpress/element'
 import { Modal } from '@wordpress/components'
 import { pricingURL } from 'stackable'
-import Swiper from 'react-id-swiper'
-import { withState } from '@wordpress/compose'
+import SVGNext from './images/next.svg'
+import SVGPrev from './images/prev.svg'
 
-const ProModal = props => {
-	const {
-		isOpen = false,
-		setState,
-		button = __( 'Learn More' ),
-		buttonClassName = 'button button-secondary',
-		tag = 'div',
-	} = props
-
-	// Slider params.
-	const swiperParams = {
-		loop: true,
-		pagination: {
-			el: '.swiper-pagination',
-			type: 'bullets',
-			clickable: true,
-		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
+class ProModal extends Component {
+	constructor() {
+		super( ...arguments )
+		this.state = {
+			isOpen: false,
+			slide: 0,
+		}
+		this.open = this.open.bind( this )
+		this.close = this.close.bind( this )
+		this.prev = this.prev.bind( this )
+		this.next = this.next.bind( this )
 	}
 
-	const Tag = tag
+	open() {
+		this.setState( { isOpen: true } )
+	}
 
-	return (
-		<Tag>
-			{ /* eslint-disable-next-line */ }
-			<a onClick={ () => setState( { isOpen: true } ) } className={ buttonClassName }>{ button }</a>
-			{ isOpen && (
-				<Modal
-					className="ugb-pro-modal"
-					onRequestClose={ () => setState( { isOpen: false } ) }
-				>
-					<Swiper { ...swiperParams }>
-						<img src="https://gambitph.github.io/Stackable/assets/premium-slider/cover.jpg" alt={ __( 'Do more with Stackable Premium' ) } />
-						<img src="https://gambitph.github.io/Stackable/assets/premium-slider/layouts.jpg" alt={ __( 'Premium Layouts' ) } />
-						<img src="https://gambitph.github.io/Stackable/assets/premium-slider/seamless.jpg" alt={ __( 'Seamless Layouts Switching' ) } />
-						<img src="https://gambitph.github.io/Stackable/assets/premium-slider/effects.jpg" alt={ __( 'Premium Effects' ) } />
-						<img src="https://gambitph.github.io/Stackable/assets/premium-slider/others.jpg" alt={ __( 'Other Features' ) } />
-					</Swiper>
-					<div className="ugb-pro-modal__footer">
-						<a href={ pricingURL }
-							className="button button-secondary"
-							target="_blank"
-							title={ __( 'Go Premium' ) }
-						>
-							{ __( 'Go Premium' ) }
-						</a>
-						<a href="https://rebrand.ly/plugin-premium-demo"
-							className="button button-secondary button-ghost"
-							target="_blank" // eslint-disable-line
-							rel="noopener"
-							title={ __( 'Try Premium Demo' ) }
-						>
-							{ __( 'Try Premium Demo' ) }
-						</a>
-						<a href="https://rebrand.ly/plugin-slider-premium"
-							target="_blank" // eslint-disable-line
-							rel="noopener"
-							title={ __( 'Visit Site' ) }
-						>
-							{ __( 'Visit Site' ) }
-						</a>
-					</div>
-				</Modal>
-			) }
-		</Tag>
-	)
+	close() {
+		this.setState( { isOpen: false } )
+	}
+
+	next() {
+		const slide = this.state.slide >= 4 ? 0 : this.state.slide + 1
+		this.setState( { slide } )
+	}
+
+	prev() {
+		const slide = this.state.slide <= 0 ? 4 : this.state.slide - 1
+		this.setState( { slide } )
+	}
+
+	render() {
+		const {
+			button = __( 'Learn More' ),
+			buttonClassName = 'button button-secondary',
+			tag = 'div',
+		} = this.props
+
+		const Tag = tag
+
+		return (
+			<Tag>
+				{ /* eslint-disable-next-line */ }
+				<a onClick={ this.open } className={ buttonClassName }>{ button }</a>
+				{ this.state.isOpen && (
+					<Modal
+						className="ugb-pro-modal"
+						onRequestClose={ this.close }
+					>
+						<div className={ `ugb-pro-modal__carousel ugb-pro-modal__carousel--slide-${ this.state.slide }` }>
+							<div className="ugb-pro-modal__carousel-wrapper">
+								<img src="https://gambitph.github.io/Stackable/assets/premium-slider/cover.jpg" alt={ __( 'Do more with Stackable Premium' ) } />
+								<img src="https://gambitph.github.io/Stackable/assets/premium-slider/layouts.jpg" alt={ __( 'Premium Layouts' ) } />
+								<img src="https://gambitph.github.io/Stackable/assets/premium-slider/seamless.jpg" alt={ __( 'Seamless Layouts Switching' ) } />
+								<img src="https://gambitph.github.io/Stackable/assets/premium-slider/effects.jpg" alt={ __( 'Premium Effects' ) } />
+								<img src="https://gambitph.github.io/Stackable/assets/premium-slider/others.jpg" alt={ __( 'Other Features' ) } />
+							</div>
+							<div className="ugb-pro-modal__carousel-left" onMouseDown={ this.prev } onKeyPress={ this.prev } role="button" tabIndex="0">
+								<SVGPrev />
+							</div>
+							<div className="ugb-pro-modal__carousel-right" onMouseDown={ this.next } onKeyPress={ this.next } role="button" tabIndex="0">
+								<SVGNext />
+							</div>
+						</div>
+						<div className="ugb-pro-modal__footer">
+							<a href={ pricingURL }
+								className="button button-secondary"
+								target="_blank"
+								title={ __( 'Go Premium' ) }
+							>
+								{ __( 'Go Premium' ) }
+							</a>
+							<a href="https://rebrand.ly/plugin-premium-demo"
+								className="button button-secondary button-ghost"
+								target="_blank" // eslint-disable-line
+								rel="noopener"
+								title={ __( 'Try Premium Demo' ) }
+							>
+								{ __( 'Try Premium Demo' ) }
+							</a>
+							<a href="https://rebrand.ly/plugin-slider-premium"
+								target="_blank" // eslint-disable-line
+								rel="noopener"
+								title={ __( 'Visit Site' ) }
+							>
+								{ __( 'Visit Site' ) }
+							</a>
+						</div>
+					</Modal>
+				) }
+			</Tag>
+		)
+	}
 }
 
-export default withState( {
-	isOpen: false,
-} )( ProModal )
+export default ProModal
