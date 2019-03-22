cf8cd41f115083dd757aaa58b6862983381f6ab2
Benjamin Intal <bfintal@gmail.com>
2019-03-22 17:48:54 +0800

    Cleanup

--- src/components/panel-toggle-settings/index.js
+++ src/components/panel-toggle-settings/index.js
@@ -2,10 +2,9 @@
  * A Panel for selecting designs
  */
 
-import { __, sprintf } from '@wordpress/i18n'
+import { FormToggle, PanelBody } from '@wordpress/components'
+import { __ } from '@wordpress/i18n'
 import { Component } from '@wordpress/element'
-import { ColorIndicator, PanelBody, FormToggle } from '@wordpress/components'
-import { ColorPaletteControl } from '@stackable/components'
 
 class PanelToggleSettings extends Component {
 	constructor( props ) {
@@ -28,15 +27,13 @@ class PanelToggleSettings extends Component {
 				initialOpen={ this.props.initialOpen }
 				onToggle={ this.onToggle }
 				opened={ this.state.opened }
-				// { ...this.props }
 				title={
 					<span className={ `editor-panel-toggle-settings__panel-title` }>
 						<FormToggle
 							className="ugb-toggle-panel-form-toggle"
 							checked={ this.state.checked }
 							onClick={ ev => {
-
-								console.log('checked', ev, ev.stopPropagation())
+								ev.stopPropagation()
 								ev.preventDefault()
 								ev.currentTarget.blur()
 								const checked = this.state.checked
@@ -49,24 +46,8 @@ class PanelToggleSettings extends Component {
 								if ( this.props.onChange ) {
 									this.props.onChange( ! checked )
 								}
-							}}
-							onChange={ ev => {
-								// const checked = this.state.checked
-								// if ( checked && this.state.opened ) {
-								// 	this.onToggle()
-								// } else if ( ! checked && ! this.state.opened ) {
-								// 	this.onToggle()
-								// }
-								// this.setState( { checked: ! checked } )
-								// if ( this.props.onChange ) {
-								// 	this.props.onChange( ! checked )
-								// }
-
 							} }
-							// id={ id }
-							// checked={ checked }
-							// onChange={ this.onChange }
-							// aria-describedby={ title }
+							aria-describedby={ this.props.title }
 						/>
 						{ this.props.title }
 					</span>
