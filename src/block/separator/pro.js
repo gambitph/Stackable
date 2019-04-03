0e8f6cd62d191f1fde08939ac1ba97b273fe1462
Benjamin Intal <bfintal@gmail.com>
2019-04-04 01:05:39 +0800

    testing commit

---
+++ src/block/separator/pro.js
@@ -0,0 +1,54 @@
+/**
+ * This file only contains Premium notices and Premium panel notices.
+ */
+import { __ } from '@wordpress/i18n'
+import { addFilter } from '@wordpress/hooks'
+import { Fragment } from '@wordpress/element'
+import { PanelBody } from '@wordpress/components'
+import { ProControl } from '@stackable/components'
+import { showProNotice } from 'stackable'
+
+if ( showProNotice ) {
+	addFilter( 'stackable.separator.edit.inspector.style.after', 'stackable/separator', output => {
+		return (
+			<Fragment>
+				{ output }
+				<PanelBody
+					initialOpen={ false }
+					title={ __( 'Layer 2' ) }
+				>
+					<ProControl
+						title={ __( 'Say Hello to Gorgeous Separators 👋' ) }
+						description={ __( 'Add a second layer to this separator and make it look even sweeter. This feature is only available on Stackable Premium' ) }
+					/>
+				</PanelBody>
+				<PanelBody
+					initialOpen={ false }
+					title={ __( 'Layer 3' ) }
+				>
+					<ProControl
+						title={ __( 'Say Hello to Gorgeous Separators 👋' ) }
+						description={ __( 'Add a third layer to this separator and make it look even sweeter. This feature is only available on Stackable Premium' ) }
+					/>
+				</PanelBody>
+			</Fragment>
+		)
+	} )
+
+	addFilter( 'stackable.separator.edit.inspector.advanced.after', 'stackable/separator', output => {
+		return (
+			<Fragment>
+				{ output }
+				<PanelBody
+					initialOpen={ false }
+					title={ __( 'Custom CSS' ) }
+				>
+					<ProControl
+						title={ __( 'Say Hello to Custom CSS 👋' ) }
+						description={ __( 'Further tweak this block by adding guided custom CSS rules. This feature is only available on Stackable Premium' ) }
+					/>
+				</PanelBody>
+			</Fragment>
+		)
+	} )
+}
