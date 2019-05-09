06c30f00190a27537231758e9aa0c12fe62139e7
Benjamin Intal <bfintal@gmail.com>
2019-05-09 19:08:32 +0800

    Module initialize

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -1,3 +1,5 @@
+import { modules as modulesToRegister } from './'
+
 const modules = {}
 
 export const registerModule = ( moduleName, moduleFunction ) => {
@@ -7,6 +9,14 @@ export const registerModule = ( moduleName, moduleFunction ) => {
 export const getModule = moduleName => modules[ moduleName ]
 
 export const initBlockModule = ( blockName, moduleName, options = {} ) => {
+	if ( ! getModule( moduleName ) ) {
+		const moduleFound = modulesToRegister.reduce( ( moduleFound, { name, module } ) => {
+			return moduleName === name ? module : moduleFound
+		}, null )
+		if ( moduleFound ) {
+			registerModule( moduleName, moduleFound )
+		}
+	}
 	const module = getModule( moduleName )
 	if ( module ) {
 		module( blockName, options )
