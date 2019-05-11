c97b141735dc5e637b9c1f2a4757408b6be86457
Benjamin Intal <bfintal@gmail.com>
2019-05-12 02:59:29 +0800

    New tests

--- src/block/separator/__test__/index.test.js
+++ src/block/separator/__test__/index.test.js
@@ -1,37 +1,6 @@
-import { blockEditRender, blockSaveRender } from '@stackable/test/helpers'
-import { name, settings } from '../'
-import { applyFilters } from '@wordpress/hooks'
+import { blockAttributeTests } from '@stackable/test/shared'
+import { settings } from '../'
 
 describe( `${ settings.title } block`, () => {
-	test( 'Inspector layouts snapshot', () => {
-		const inspector = applyFilters( 'stackable.separator.edit.inspector.layout.before', null, {
-			setAttributes: () => {}, attributes: {},
-		} )
-		expect( inspector ).toMatchSnapshot()
-	} )
-
-	test( 'Inspector style snapshot', () => {
-		const inspector = applyFilters( 'stackable.separator.edit.inspector.style.before', null, {
-			setAttributes: () => {}, attributes: {},
-		} )
-		expect( inspector ).toMatchSnapshot()
-	} )
-
-	test( 'Edit render snapshot', () => {
-		const wrapper = blockEditRender( name, settings )
-		expect( wrapper ).toMatchSnapshot()
-	} )
-
-	test( 'Save render snapshot', () => {
-		const wrapper = blockSaveRender( name, settings )
-		expect( wrapper ).toMatchSnapshot()
-	} )
-
-	test( 'Save render with attributes snapshot', () => {
-		const attributes = {
-			design: 'wave-2',
-		}
-		const wrapper = blockSaveRender( name, settings, attributes )
-		expect( wrapper ).toMatchSnapshot()
-	} )
+	blockAttributeTests( { settings } )
 } )
