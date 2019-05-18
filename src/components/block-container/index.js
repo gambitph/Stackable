ed02d6e61167a0dd98b30b9053f347185d33397f
Benjamin Intal <bfintal@gmail.com>
2019-05-19 02:36:23 +0800

    Added main block class and inner block class

--- src/components/block-container/index.js
+++ src/components/block-container/index.js
@@ -4,53 +4,61 @@ import classnames from 'classnames'
 const BlockContainer = {}
 
 BlockContainer.Edit = props => {
-	const { blockProps, render, ...containerProps } = props
+	const { blockProps, render, mainClass, ...containerProps } = props
 	const { blockName } = blockProps
 	const design = blockProps.attributes.design
 
-	const mainClasses = classnames(
+	const mainClasses = classnames( [
 		props.className,
-		applyFilters( `stackable.${ blockName }.mainclasses`, {}, blockProps )
-	)
+	], applyFilters( `stackable.${ blockName }.main-block.classes`, {
+		'ugb-main-block': mainClass,
+	}, blockProps ) )
 
 	// TODO Remove `design` from the filters below.
 	return (
 		<div { ...containerProps } className={ mainClasses }>
 			{ blockProps.styles }
-			{ applyFilters( `stackable.${ blockName }.edit.output.before`, null, design, blockProps ) }
-			{ render( blockProps ) }
-			{ applyFilters( `stackable.${ blockName }.edit.output.after`, null, design, blockProps ) }
+			<div className="ugb-inner-block">
+				{ applyFilters( `stackable.${ blockName }.edit.output.before`, null, design, blockProps ) }
+				{ render( blockProps ) }
+				{ applyFilters( `stackable.${ blockName }.edit.output.after`, null, design, blockProps ) }
+			</div>
 		</div>
 	)
 }
 
 BlockContainer.Edit.defaultProps = {
 	styles: null, // provided by `withBlockStyles`
+	mainClass: true,
 }
 
 BlockContainer.Save = props => {
-	const { blockProps, render, ...containerProps } = props
+	const { blockProps, render, mainClass, ...containerProps } = props
 	const { blockName } = blockProps
 	const design = blockProps.attributes.design
 
-	const mainClasses = classnames(
+	const mainClasses = classnames( [
 		props.className,
-		applyFilters( `stackable.${ blockName }.mainclasses`, {}, blockProps )
-	)
+	], applyFilters( `stackable.${ blockName }.main-block.classes`, {
+		'ugb-main-block': mainClass,
+	}, blockProps ) )
 
 	// TODO Remove `design` from the filters below.
 	return (
 		<div { ...containerProps } className={ mainClasses }>
 			{ blockProps.styles }
-			{ applyFilters( `stackable.${ blockName }.save.output.before`, null, design, blockProps ) }
-			{ render( blockProps ) }
-			{ applyFilters( `stackable.${ blockName }.save.output.after`, null, design, blockProps ) }
+			<div className="ugb-inner-block">
+				{ applyFilters( `stackable.${ blockName }.save.output.before`, null, design, blockProps ) }
+				{ render( blockProps ) }
+				{ applyFilters( `stackable.${ blockName }.save.output.after`, null, design, blockProps ) }
+			</div>
 		</div>
 	)
 }
 
 BlockContainer.Save.defaultProps = {
 	styles: null, // provided by `withBlockStyles`
+	mainClass: true,
 }
 
 export default BlockContainer
