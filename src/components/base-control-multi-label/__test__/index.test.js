64d4f5f15d6f5c8a26c320da43ca68d9da398a9c
Benjamin Intal <bfintal@gmail.com>
2019-05-21 14:56:39 +0800

    Rewrite to a stateless component

---
+++ src/components/base-control-multi-label/__test__/index.test.js
@@ -0,0 +1,39 @@
+import BaseControlMultiLabel from '../'
+
+describe( 'Unit switcher', () => {
+	test( 'should have a label', () => {
+		expect( shallow( <BaseControlMultiLabel label="hello" /> ).text() ).toContain( 'hello' )
+	} )
+
+	test( 'should not display units if single', () => {
+		expect( shallow( <BaseControlMultiLabel /> ).find( 'button' ) ).not.toExist()
+		expect( shallow( <BaseControlMultiLabel units={ [ '%' ] } /> ).find( 'button' ) ).not.toExist()
+		expect( shallow( <BaseControlMultiLabel units={ [] } /> ).find( 'button' ) ).not.toExist()
+	} )
+
+	test( 'should display units if multiple', () => {
+		const wrapper2 = shallow( <BaseControlMultiLabel units={ [ 'px', '%' ] } /> )
+		expect( wrapper2.find( 'button' ).length ).toBe( 2 )
+		expect( wrapper2.find( 'button' ).at( 0 ).text() ).toBe( 'px' )
+		expect( wrapper2.find( 'button' ).at( 1 ).text() ).toBe( '%' )
+		expect( wrapper2.find( 'button' ).at( 0 ).hasClass( 'is-active' ) ).toBe( true )
+		expect( wrapper2.find( 'button' ).at( 1 ).hasClass( 'is-active' ) ).toBe( false )
+
+		const wrapper3 = shallow( <BaseControlMultiLabel unit="em" units={ [ 'px', 'em', '%' ] } /> )
+		expect( wrapper3.find( 'button' ).length ).toBe( 3 )
+		expect( wrapper3.find( 'button' ).at( 0 ).text() ).toBe( 'px' )
+		expect( wrapper3.find( 'button' ).at( 1 ).text() ).toBe( 'em' )
+		expect( wrapper3.find( 'button' ).at( 2 ).text() ).toBe( '%' )
+		expect( wrapper3.find( 'button' ).at( 0 ).hasClass( 'is-active' ) ).toBe( false )
+		expect( wrapper3.find( 'button' ).at( 1 ).hasClass( 'is-active' ) ).toBe( true )
+		expect( wrapper3.find( 'button' ).at( 2 ).hasClass( 'is-active' ) ).toBe( false )
+	} )
+
+	test( 'should change units', () => {
+		const onChange = jest.fn()
+
+		const wrapper = shallow( <BaseControlMultiLabel unit="px" units={ [ 'px', '%' ] } onChangeUnit={ onChange } /> )
+		wrapper.find( 'button' ).at( 1 ).simulate( 'click' )
+		expect( onChange ).toHaveBeenCalledWith( '%' )
+	} )
+} )
--- src/components/base-control-multi-label/index.js
+++ src/components/base-control-multi-label/index.js
@@ -1,49 +1,30 @@
-import { Component } from '@wordpress/element'
 import { ResponsiveToggle } from '@stackable/components'
 
-class BaseControlMultiLabel extends Component {
-	constructor() {
-		super( ...arguments )
-		this.state = {
-			unit: this.props.unit || 'px',
-		}
-	}
-
-	onChangeUnit( value ) {
-		this.props.onChangeUnit( value )
-		this.setState( { unit: value } )
-	}
-
-	componentDidUpdate( prevProps ) {
-		if ( this.props.unit !== prevProps.unit ) {
-			this.setState( { unit: this.props.unit } )
-		}
-	}
-
-	render() {
-		return (
-			<div className="ugb-base-control-multi-label">
-				<div className="ugb-base-control-multi-label__label components-base-control__label">{ this.props.label }</div>
-				<ResponsiveToggle
-					screens={ this.props.screens }
-					onChangeScreen={ this.props.onChangeScreen }
-				/>
-				<div className="ugb-base-control-multi-label__units">
-					{ this.props.units.length > 1 &&
-						this.props.units.map( ( unit, i ) => {
-							return (
-								<button
-									key={ i }
-									className={ this.state.unit === unit ? 'is-active' : '' }
-									onClick={ () => this.onChangeUnit( unit ) }
-								>{ unit }</button>
-							)
-						} )
-					}
-				</div>
+const BaseControlMultiLabel = props => {
+	return (
+		<div className="ugb-base-control-multi-label">
+			<div className="ugb-base-control-multi-label__label components-base-control__label">{ props.label }</div>
+			<ResponsiveToggle
+				screens={ props.screens }
+				onChangeScreen={ props.onChangeScreen }
+			/>
+			<div className="ugb-base-control-multi-label__units">
+				{ props.units.length > 1 &&
+					props.units.map( ( unit, i ) => {
+						return (
+							<button
+								key={ i }
+								className={ props.unit === unit ? 'is-active' : '' }
+								onClick={ () => props.onChangeUnit( unit ) }
+							>
+								{ unit }
+							</button>
+						)
+					} )
+				}
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 BaseControlMultiLabel.defaultProps = {
