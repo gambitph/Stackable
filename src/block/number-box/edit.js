be27b371f822c24de135b7a5edfdfbbb8fd39b36
Benjamin Intal <bfintal@gmail.com>
2019-06-03 17:25:03 +0800

    Lower limit of number text

--- src/block/number-box/edit.js
+++ src/block/number-box/edit.js
@@ -208,6 +208,7 @@ addFilter( 'stackable.number-box.edit.inspector.style.before', 'stackable/number
 					blockAttributes={ props.attributes }
 					fontSizeProps={ {
 						max: design === 'basic' || design === 'plain' ? [ 150, 7 ] : [ 500, 15 ],
+						min: 40,
 					} }
 				/>
 				{ show.numberStyle && (
