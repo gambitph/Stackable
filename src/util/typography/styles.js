47aaff0d08eda0a699d44ae64db6ae08b1d08e60
Benjamin Intal <bfintal@gmail.com>
2019-06-28 16:38:41 +0800

    Added !important option to typography style

--- src/util/typography/styles.js
+++ src/util/typography/styles.js
@@ -2,14 +2,18 @@ import { camelCase } from 'lodash'
 import { getFontFamily } from '@stackable/util'
 import { sprintf } from '@wordpress/i18n'
 
-const createTypographyStyles = ( attrNameTemplate = '%s', screen = 'desktop', blockAttributes = {} ) => {
+const createTypographyStyles = ( attrNameTemplate = '%s', screen = 'desktop', blockAttributes = {}, options = {} ) => {
 	const getAttrName = attrName => camelCase( sprintf( attrNameTemplate, attrName ) )
 	const getValue = ( attrName, defaultValue = '' ) => blockAttributes[ getAttrName( attrName ) ] || defaultValue
+
+	const {
+		importantSize = false,
+	} = options
 
 	if ( screen !== 'tablet' && screen !== 'mobile' ) { // Desktop.
 		return {
 			fontFamily: getValue( 'FontFamily' ) !== '' ? getFontFamily( getValue( 'FontFamily' ) ) : undefined,
-			fontSize: getValue( 'FontSize' ) !== '' ? `${ getValue( 'FontSize' ) }${ getValue( 'FontSizeUnit', 'px' ) }` : undefined,
+			fontSize: getValue( 'FontSize' ) !== '' ? `${ getValue( 'FontSize' ) }${ getValue( 'FontSizeUnit', 'px' ) }${ importantSize ? ' !important' : '' }` : undefined,
 			fontWeight: getValue( 'FontWeight' ) !== '' ? getValue( 'FontWeight' ) : undefined,
 			textTransform: getValue( 'TextTransform' ) !== '' ? getValue( 'TextTransform' ) : undefined,
 			letterSpacing: getValue( 'LetterSpacing' ) !== '' ? `${ getValue( 'LetterSpacing' ) }px` : undefined,
@@ -17,14 +21,14 @@ const createTypographyStyles = ( attrNameTemplate = '%s', screen = 'desktop', bl
 		}
 	} else if ( screen === 'tablet' ) { // Tablet.
 		return {
-			fontSize: getValue( 'TabletFontSize' ) !== '' ? `${ getValue( 'TabletFontSize' ) }${ getValue( 'TabletFontSizeUnit', 'px' ) }` : undefined,
+			fontSize: getValue( 'TabletFontSize' ) !== '' ? `${ getValue( 'TabletFontSize' ) }${ getValue( 'TabletFontSizeUnit', 'px' ) }${ importantSize ? ' !important' : '' }` : undefined,
 			lineHeight: getValue( 'TabletLineHeight' ) !== '' ? `${ getValue( 'TabletLineHeight' ) }${ getValue( 'TabletLineHeightUnit', 'em' ) }` : undefined,
 		}
 	}
 
 	// Mobile.
 	return {
-		fontSize: getValue( 'MobileFontSize' ) !== '' ? `${ getValue( 'MobileFontSize' ) }${ getValue( 'MobileFontSizeUnit', 'px' ) }` : undefined,
+		fontSize: getValue( 'MobileFontSize' ) !== '' ? `${ getValue( 'MobileFontSize' ) }${ getValue( 'MobileFontSizeUnit', 'px' ) }${ importantSize ? ' !important' : '' }` : undefined,
 		lineHeight: getValue( 'MobileLineHeight' ) !== '' ? `${ getValue( 'MobileLineHeight' ) }${ getValue( 'MobileLineHeightUnit', 'em' ) }` : undefined,
 	}
 }
