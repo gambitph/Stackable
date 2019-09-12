79135fd18b78e3e82572defd0843ac954ca5edfc
Benjamin Intal <bfintal@gmail.com>
2019-09-12 19:56:40 +0800

    [core] Fixed: IE11 forEach error. Added polyfill for NodeList forEach method

--- src/block-frontend.js
+++ src/block-frontend.js
@@ -4,6 +4,11 @@
  * This is the file that Webpack is compiling into blocks.frontend.build.js
  */
 
+/**
+ * Internal dependencies
+ */
+import './polyfill'
+
 const context = require.context(
 	'./block', // Search within the src/blocks directory.
 	true, // Search recursively.
