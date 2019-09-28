44f6006e4f082d96edb6e5f195df3e18c6d78763
Benjamin Intal <bfintal@gmail.com>
2019-09-28 13:50:46 +0800

    [welcome] Added video tutorial

--- src/welcome/admin.js
+++ src/welcome/admin.js
@@ -196,29 +196,29 @@ const knowledgeBaseList = () => {
 	return (
 		<ul className="s-tabs-list">
 			<li>
-				<a href="https://wpstackable.com/documentation?utm_medium=Welcome%20Page&utm_campaign=Welcome%20Help&utm_source=Plugin" target="_blank" rel="noopener noreferrer"><strong>{ __( 'Documentation', i18n ) }</strong></a>
+				<span role="img" aria-label="book">📖</span> <a href="https://wpstackable.com/documentation?utm_medium=Welcome%20Page&utm_campaign=Welcome%20Help&utm_source=Plugin" target="_blank" rel="noopener noreferrer"><strong>{ __( 'Documentation', i18n ) }</strong></a>
 				<br />
 				<span>{ __( 'Documentation & tutorials for building your site with Stackable.', i18n ) }</span>
 			</li>
 			{ isPro &&
 				<li>
-					<a href={ contactURL }><strong>{ __( 'Contact Email Support', i18n ) }</strong></a>
+					<span role="img" aria-label="envelope">✉️</span> <a href={ contactURL }><strong>{ __( 'Contact Email Support', i18n ) }</strong></a>
 					<br />
 					<span>{ __( 'Stuck with something? Email us and we’ll help you out.', i18n ) }</span>
 				</li>
 			}
-			<li style={ { display: 'none' } }>
-				<a href="https://facebook.com/groups/wpstackable" target="_blank" rel="noopener noreferrer"><strong>{ __( 'Facebook Community Group', i18n ) }</strong></a>
+			<li>
+				<span role="img" aria-label="discuss">💬</span> <a href="https://facebook.com/groups/wpstackable" target="_blank" rel="noopener noreferrer"><strong>{ __( 'Facebook Community Group', i18n ) }</strong></a>
 				<br />
 				<span>{ __( 'Connect with other people using Stackable and join the discussion.', i18n ) }</span>
 			</li>
 			<li>
-				<a href="https://github.com/gambitph/Stackable/issues" target="_blank" rel="noopener noreferrer"><strong>{ __( 'GitHub', i18n ) }</strong></a>
+				<span role="img" aria-label="code">💻</span> <a href="https://github.com/gambitph/Stackable/issues" target="_blank" rel="noopener noreferrer"><strong>{ __( 'GitHub', i18n ) }</strong></a>
 				<br />
 				<span>{ __( 'Discuss technical plugin issues and contribute to the plugin code.', i18n ) }</span>
 			</li>
 			<li>
-				<a href="https://wordpress.org/support/plugin/stackable-ultimate-gutenberg-blocks/" taregt="_blank" rel="noopener noreferrer"><strong>{ __( 'WordPress Plugin Support Forum', i18n ) }</strong></a>
+				<span role="img" aria-label="support">🤝</span> <a href="https://wordpress.org/support/plugin/stackable-ultimate-gutenberg-blocks/" taregt="_blank" rel="noopener noreferrer"><strong>{ __( 'WordPress Plugin Support Forum', i18n ) }</strong></a>
 				<br />
 				<span>{ __( 'Community-powered plugin support forum', i18n ) }</span>
 			</li>
@@ -230,15 +230,51 @@ class HelpTabs extends Component {
 	render() {
 		// If already pro, no need to show the tabs.
 		if ( isPro ) {
-			return knowledgeBaseList()
+			return (
+				<div>
+					<div className="s-video-tutorial-wrapper">
+						<iframe
+							title={ __( 'Video Tutorial', i18n ) }
+							width="560"
+							height="315"
+							src="https://www.youtube.com/embed/OR6wNum6mUg"
+							frameBorder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowFullScreen
+						/>
+					</div>
+					<h3>{ __( 'Knowledge Base', i18n ) }</h3>
+					{ knowledgeBaseList() }
+				</div>
+			)
 		}
 
 		return (
 			<Tabs>
 				<TabList className="s-tabs">
+					<Tab>{ __( 'Video Tutorial', i18n ) }</Tab>
 					<Tab>{ __( 'Knowledge Base', i18n ) }</Tab>
 					<Tab>{ __( 'Get Support', i18n ) }</Tab>
 				</TabList>
+				<TabPanel>
+					<div className="s-video-tutorial-wrapper">
+						<iframe
+							title={ __( 'Video Tutorial', i18n ) }
+							width="560"
+							height="315"
+							src="https://www.youtube.com/embed/OR6wNum6mUg"
+							frameBorder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowFullScreen
+						/>
+					</div>
+					<p><strong>{ __( 'Need help?', i18n ) }</strong></p>
+					<p>{ __( 'Upgrade to Premium and our support team will be there to answer any questions you might have about the usage of Stackable.', i18n ) }</p>
+					<p className="s-link-pair">
+						<a href={ pricingURL } title={ __( 'Get Stackable Premium', i18n ) }>{ __( 'Get Stackable Premium', i18n ) + ' →' }</a>
+						<a href="https://rebrand.ly/plugin-welcome-learn-premium-support" title={ __( 'Learn More', i18n ) } target="_blank" rel="noopener noreferrer">{ __( 'Learn More', i18n ) + ' →' }</a>
+					</p>
+				</TabPanel>
 				<TabPanel>
 					{ knowledgeBaseList() }
 					<p><strong>{ __( 'Need help?', i18n ) }</strong></p>
