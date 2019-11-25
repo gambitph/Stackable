15f44f3b0beb75fb2348ef174a5f5f7a9d3b00ba
yssagambit <51441886+yssagambit@users.noreply.github.com>
2019-11-25 23:17:29 +0800

    Update design.js

--- src/block/blockquote/design.js
+++ src/block/blockquote/design.js
@@ -18,12 +18,112 @@ addFilter( 'stackable.blockquote.edit.designs', 'stackable/blockquote', designs 
 	return {
 		...designs,
 		// TODO: sample, remove this
-		// corporate: {
-		// 	label: __( 'Corporate', i18n ),
-		// 	attributes: {
-		// 		borderRadius: 50,
-		// 		containerBackgroundColor: '#fcb900',
-		// 	},
-		// },
+		corporateLight1: {
+			label: __( 'Corporate Light 1', i18n ),
+			image: "https://gambitph.github.io/Stackable/assets/block-design-previews/stack-blockquote-design-corporate-light-01.jpg"
+			attributes: {
+				{
+    {
+    "quoteIcon": "square",
+    "quoteOpacity": 0.3,
+    "quoteColor": "#ffb20b",
+    "quoteSize": 150,
+    "quoteY": 19,
+    "textFontFamily": "Roboto Condensed",
+    "textFontWeight": "100",
+    "textColor": "#707070",
+    "textAlign": "center",
+    "paddingTop": 80,
+    "paddingBottom": 80,
+    "showBlockBackground": true,
+    "blockInnerWidth": "center",
+    "align": "full",
+    "blockBackgroundBackgroundColor": "#ffffff",
+    "showBottomSeparator": false
+}
+
+			},
+		},
+		corporateLight2: {
+			label: __( 'Corporate Light 2', i18n ),
+			image: "https://gambitph.github.io/Stackable/assets/block-design-previews/stack-blockquote-design-corporate-light-02.jpg"
+			attributes: {
+				{
+    {
+    {
+    "quoteIcon": "square",
+    "quoteOpacity": 0.3,
+    "quoteColor": "#c49732",
+    "quoteSize": 150,
+    "quoteY": 19,
+    "textFontFamily": "Roboto Condensed",
+    "textFontWeight": "100",
+    "textColor": "#6a91eb",
+    "textAlign": "center",
+    "paddingTop": 80,
+    "paddingBottom": 80,
+    "showBlockBackground": true,
+    "blockInnerWidth": "center",
+    "align": "full",
+    "blockBackgroundBackgroundColor": "#ffffff",
+    "showBottomSeparator": false
+}
+
+			},
+		},
+		corporateDark1: {
+			label: __( 'Corporate Dark 1', i18n ),
+			image: "https://gambitph.github.io/Stackable/assets/block-design-previews/stack-blockquote-design-corporate-dark-01.jpg"
+			attributes: {
+				{
+    {
+    {
+    "quoteIcon": "square",
+    "quoteOpacity": 0.3,
+    "quoteColor": "#1785cb",
+    "quoteSize": 150,
+    "quoteY": 19,
+    "textFontFamily": "Roboto Condensed",
+    "textFontWeight": "100",
+    "textColor": "#ffffff",
+    "textAlign": "center",
+    "paddingTop": 80,
+    "paddingBottom": 80,
+    "showBlockBackground": true,
+    "blockInnerWidth": "center",
+    "align": "full",
+    "blockBackgroundBackgroundColor": "#081831",
+    "showBottomSeparator": false
+}
+
+			},
+		},
+		corporateDark2: {
+			label: __( 'Corporate Dark 2', i18n ),
+			image: "https://gambitph.github.io/Stackable/assets/block-design-previews/stack-blockquote-design-corporate-dark-02.jpg"
+			attributes: {
+				{
+    {
+    {
+    "quoteIcon": "square",
+    "quoteOpacity": 0.3,
+    "quoteColor": "#2f2f2f",
+    "quoteSize": 150,
+    "quoteY": 19,
+    "textFontFamily": "Roboto Condensed",
+    "textFontWeight": "100",
+    "textColor": "#06c19d",
+    "textAlign": "center",
+    "paddingTop": 80,
+    "paddingBottom": 80,
+    "showBlockBackground": true,
+    "blockInnerWidth": "center",
+    "align": "full",
+    "blockBackgroundBackgroundColor": "#707070",
+    "showBottomSeparator": false
+}
+
+			},
+		},
 	}
 } )
