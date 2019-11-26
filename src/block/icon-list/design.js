6d6970c922ee2a2827836d2596640f4486bef793
yssagambit <51441886+yssagambit@users.noreply.github.com>
2019-11-26 12:51:35 +0800

    Update design.js (#480)

--- src/block/icon-list/design.js
+++ src/block/icon-list/design.js
@@ -18,12 +18,129 @@ addFilter( 'stackable.icon-list.edit.designs', 'stackable/icon-list', designs =>
 	return {
 		...designs,
 		// TODO: sample, remove this
-		// corporate: {
-		// 	label: __( 'Corporate', i18n ),
-		// 	attributes: {
-		// 		borderRadius: 50,
-		// 		containerBackgroundColor: '#fcb900',
-		// 	},
-		// },
+
+		corporateLight1: {
+			label: __( 'Corporate Light 1', i18n ),
+			image: "https://gambitph.github.io/Stackable/assets/block-design-previews/stack-icon-list-design-corporate-light-01.jpg"
+			attributes: 
+{
+    "iconShape": "circle",
+    "iconColor": "#ff550a",
+    "iconSize": 25,
+    "columns": 3,
+    "listTextColor": "#707070",
+    "listTextFontFamily": "Serif",
+    "paddingTop": 130,
+    "paddingBottom": 130,
+    "showBlockBackground": true,
+    "blockInnerWidth": "wide",
+    "align": "full",
+    "blockBackgroundBackgroundColor": "#ffd781",
+    "showBlockTitle": true,
+    "blockTitle": "This is an Icon List",
+    "blockTitleFontFamily": "Roboto Condensed",
+    "blockTitleFontWeight": "100",
+    "blockTitleLetterSpacing": 0.5,
+    "blockTitleAlign": "center",
+    "blockTitleColor": "#ff550a",
+    "showBlockDescription": true,
+    "blockDescription": "Use this amazing block for items that you want to put in a bullet list layout.",
+    "blockDescriptionFontFamily": "Serif",
+    "blockDescriptionAlign": "center",
+    "blockDescriptionColor": "#707070"
+},
+		},
+		corporateLight2: {
+			label: __( 'Corporate Light 2', i18n ),
+			image: "https://gambitph.github.io/Stackable/assets/block-design-previews/stack-icon-list-design-corporate-light-02.jpg"
+			attributes: 
+{
+    "iconShape": "circle",
+    "iconColor": "#1e55d3",
+    "iconSize": 25,
+    "columns": 3,
+    "listTextColor": "#707070",
+    "listTextFontFamily": "Serif",
+    "paddingTop": 130,
+    "paddingBottom": 130,
+    "showBlockBackground": true,
+    "blockInnerWidth": "wide",
+    "align": "full",
+    "blockBackgroundBackgroundColor": "#ffffff",
+    "showBlockTitle": true,
+    "blockTitle": "This is an Icon List",
+    "blockTitleFontFamily": "Roboto Condensed",
+    "blockTitleFontWeight": "100",
+    "blockTitleLetterSpacing": 0.5,
+    "blockTitleAlign": "center",
+    "blockTitleColor": "#c49732",
+    "showBlockDescription": true,
+    "blockDescription": "Use this amazing block for items that you want to put in a bullet list layout.",
+    "blockDescriptionFontFamily": "Serif",
+    "blockDescriptionAlign": "center",
+    "blockDescriptionColor": "#707070"
+},
+		},
+		corporateDark1: {
+			label: __( 'Corporate Dark 1', i18n ),
+			image: "https://gambitph.github.io/Stackable/assets/block-design-previews/stack-icon-list-design-corporate-dark-01.jpg"
+			attributes: 
+{
+    "iconShape": "circle",
+    "iconColor": "#c4e558",
+    "iconSize": 25,
+    "columns": 3,
+    "listTextColor": "#ffffff",
+    "listTextFontFamily": "Serif",
+    "paddingTop": 130,
+    "paddingBottom": 130,
+    "showBlockBackground": true,
+    "blockInnerWidth": "wide",
+    "align": "full",
+    "blockBackgroundBackgroundColor": "#0e2448",
+    "showBlockTitle": true,
+    "blockTitle": "This is an Icon List",
+    "blockTitleFontFamily": "Roboto Condensed",
+    "blockTitleFontWeight": "100",
+    "blockTitleLetterSpacing": 0.5,
+    "blockTitleAlign": "center",
+    "blockTitleColor": "#c5dfb4",
+    "showBlockDescription": true,
+    "blockDescription": "Use this amazing block for items that you want to put in a bullet list layout.",
+    "blockDescriptionFontFamily": "Serif",
+    "blockDescriptionAlign": "center"
+},
+		},
+		corporateDark2: {
+			label: __( 'Corporate Dark 2', i18n ),
+			image: "https://gambitph.github.io/Stackable/assets/block-design-previews/stack-icon-list-design-corporate-dark-02.jpg"
+			attributes: 
+{
+    "iconShape": "circle",
+    "iconColor": "#06c19d",
+    "iconSize": 25,
+    "columns": 3,
+    "listTextColor": "#ffffff",
+    "listTextFontFamily": "Serif",
+    "paddingTop": 130,
+    "paddingBottom": 130,
+    "showBlockBackground": true,
+    "blockInnerWidth": "wide",
+    "align": "full",
+    "blockBackgroundBackgroundColor": "#707070",
+    "showBlockTitle": true,
+    "blockTitle": "This is an Icon List",
+    "blockTitleFontFamily": "Roboto Condensed",
+    "blockTitleFontWeight": "100",
+    "blockTitleLetterSpacing": 0.5,
+    "blockTitleAlign": "center",
+    "blockTitleColor": "#ffffff",
+    "showBlockDescription": true,
+    "blockDescription": "Use this amazing block for items that you want to put in a bullet list layout.",
+    "blockDescriptionFontFamily": "Serif",
+    "blockDescriptionAlign": "center",
+    "blockDescriptionColor": "#ffffff"
+},
+		},
 	}
 } )
