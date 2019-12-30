07f69a6dd24df71220a2447554f90eb18d2090b7
Benjamin Intal <bfintal@gmail.com>
2019-12-31 01:18:07 +0800

    [testing] [help-snippets] Use actual Components

--- src/help/__test__/events-mouse.test.js
+++ src/help/__test__/events-mouse.test.js
@@ -1,161 +1,75 @@
-import {
-	getByText,
-	fireEvent,
-} from '@testing-library/dom'
 import { addAction, removeAction } from '@wordpress/hooks'
-
 import {
 	startListening, stopListening, getParentControl,
 } from '../events-mouse'
+import { TextControl, PanelBody } from '@wordpress/components'
+import { PanelAdvancedSettings } from '~stackable/components'
+import { render, fireEvent } from '@testing-library/react'
 
 describe( 'getParentControl', () => {
-	it( 'should return the parent element with the tip class', () => {
-		const panel = document.createElement( 'div' )
-		panel.innerHTML = `
-		<div class="components-panel__body ugb--help-tip-column ugb-toggle-panel-body">
-			<h2 class="components-panel__body-title">
-				<button type="button" aria-expanded="false" class="components-button components-panel__body-toggle">
-					<span aria-hidden="true"><svg></svg></span>
-					Panel title
-				</button>
-			</h2>
-		</div>`
-
-		const title = getByText( panel, 'Panel title' )
+	it( 'should return the parent element with the tip class (PanelBody)', () => {
+		const { getByText } = render( <PanelBody title="Panel title" className="ugb--help-tip-column" /> )
+		const title = getByText( 'Panel title' )
 		const parent = getParentControl( title )
-		expect( parent ).toEqual( panel.querySelector( '.ugb--help-tip-column' ) )
+		expect( parent.classList.contains( 'ugb--help-tip-column' ) ).toBeTruthy()
+	} )
+
+	it( 'should return the parent element with the tip class (PanelAdvancedSettings)', () => {
+		const { getByText } = render( <PanelAdvancedSettings title="Panel title" className="ugb--help-tip-column" /> )
+		const title = getByText( 'Panel title' )
+		const parent = getParentControl( title )
+		expect( parent.classList.contains( 'ugb--help-tip-column' ) ).toBeTruthy()
 	} )
 } )
 
 describe( 'Help Video Snippets', () => {
-	let panel
+	let getByText
 
-	beforeAll( () => {
-		panel = document.createElement( 'div' )
-		panel.innerHTML = `
-		<div class="components-panel__body ugb--help-tip-column ugb-toggle-panel-body">
-			<h2 class="components-panel__body-title">
-				<button type="button" aria-expanded="false" class="components-button components-panel__body-toggle">
-					<span aria-hidden="true"><svg></svg></span>
-					Panel with tip no content
-				</button>
-			</h2>
-		</div>
-
-		<div class="components-panel__body ugb-toggle-panel-body">
-			<h2 class="components-panel__body-title">
-				<button type="button" aria-expanded="false" class="components-button components-panel__body-toggle">
-					<span aria-hidden="true"><svg></svg></span>
-					Panel no tip no content
-				</button>
-			</h2>
-		</div>
-
-		<div class="components-panel__body ugb--help-tip-column ugb-toggle-panel-body">
-			<h2 class="components-panel__body-title">
-				<button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">
-					<span aria-hidden="true"><svg></svg></span>
-					Panel with tip with content no tip
-				</button>
-			</h2>
-			<div class="components-base-control ugb-advanced-range-control">
-				<div class="components-base-control__field">
-					<div class="ugb-base-control-multi-label">
-						<div class="ugb-base-control-multi-label__label components-base-control__label">Control no tip in panel with tip</div>
-						<div class="ugb-base-control-multi-label__responsive"></div>
-						<div class="ugb-base-control-multi-label__units"></div>
-					</div>
-					<div class="components-base-control components-range-control">
-						<div class="components-base-control__field">
-							<input class="components-range-control__slider" id="inspector-range-control-4" type="range" min="1" max="3" placeholder="" value="2">
-							<input class="components-range-control__number" type="number" min="1" max="3" placeholder="" value="2">
-						</div>
-					</div>
-				</div>
+	const doRender = () => {
+		( { getByText } = render(
+			<div>
+				<PanelAdvancedSettings
+					title="Panel with tip no content"
+					className="ugb--help-tip-column"
+					initialOpen={ true }
+				/>
+				<PanelAdvancedSettings
+					title="Panel no tip no content"
+					initialOpen={ true }
+				/>
+				<PanelAdvancedSettings
+					title="Panel with tip with content no tip"
+					className="ugb--help-tip-column"
+					initialOpen={ true }
+				>
+					<TextControl label="Control no tip in panel with tip" />
+				</PanelAdvancedSettings>
+				<PanelAdvancedSettings
+					title="Panel with tip with content with tip"
+					className="ugb--help-tip-column"
+					initialOpen={ true }
+				>
+					<TextControl label="Control with tip in panel with tip" className="ugb--help-tip-column" />
+				</PanelAdvancedSettings>
+				<PanelAdvancedSettings
+					title="Panel no tip with content no tip"
+					initialOpen={ true }
+				>
+					<TextControl label="Control no tip in panel no tip" />
+				</PanelAdvancedSettings>
+				<PanelAdvancedSettings
+					title="Panel no tip with content with tip"
+					initialOpen={ true }
+				>
+					<TextControl label="Control with tip in panel no tip" className="ugb--help-tip-column" />
+				</PanelAdvancedSettings>
 			</div>
-		</div>
-
-		<div class="components-panel__body ugb--help-tip-column ugb-toggle-panel-body">
-			<h2 class="components-panel__body-title">
-				<button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">
-					<span aria-hidden="true"><svg></svg></span>
-					Panel with tip with content with tip
-				</button>
-			</h2>
-			<div class="components-base-control ugb-advanced-range-control ugb--help-tip-column">
-				<div class="components-base-control__field">
-					<div class="ugb-base-control-multi-label">
-						<div class="ugb-base-control-multi-label__label components-base-control__label">Control with tip in panel with tip</div>
-						<div class="ugb-base-control-multi-label__responsive"></div>
-						<div class="ugb-base-control-multi-label__units"></div>
-					</div>
-					<div class="components-base-control components-range-control">
-						<div class="components-base-control__field">
-							<input class="components-range-control__slider" id="inspector-range-control-4" type="range" min="1" max="3" placeholder="" value="2">
-							<input class="components-range-control__number" type="number" min="1" max="3" placeholder="" value="2">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="components-panel__body ugb-toggle-panel-body">
-			<h2 class="components-panel__body-title">
-				<button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">
-					<span aria-hidden="true"><svg></svg></span>
-					Panel no tip with content no tip
-				</button>
-			</h2>
-			<div class="components-base-control ugb-advanced-range-control">
-				<div class="components-base-control__field">
-					<div class="ugb-base-control-multi-label">
-						<div class="ugb-base-control-multi-label__label components-base-control__label">Control no tip in panel no tip</div>
-						<div class="ugb-base-control-multi-label__responsive"></div>
-						<div class="ugb-base-control-multi-label__units"></div>
-					</div>
-					<div class="components-base-control components-range-control">
-						<div class="components-base-control__field">
-							<input class="components-range-control__slider" id="inspector-range-control-4" type="range" min="1" max="3" placeholder="" value="2">
-							<input class="components-range-control__number" type="number" min="1" max="3" placeholder="" value="2">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="components-panel__body ugb-toggle-panel-body">
-			<h2 class="components-panel__body-title">
-				<button type="button" aria-expanded="true" class="components-button components-panel__body-toggle">
-					<span aria-hidden="true"><svg></svg></span>
-					Panel no tip with content with tip
-				</button>
-			</h2>
-			<div class="components-base-control ugb-advanced-range-control ugb--help-tip-column">
-				<div class="components-base-control__field">
-					<div class="ugb-base-control-multi-label">
-						<div class="ugb-base-control-multi-label__label components-base-control__label">Control with tip in panel no tip</div>
-						<div class="ugb-base-control-multi-label__responsive"></div>
-						<div class="ugb-base-control-multi-label__units"></div>
-					</div>
-					<div class="components-base-control components-range-control">
-						<div class="components-base-control__field">
-							<input class="components-range-control__slider" id="inspector-range-control-4" type="range" min="1" max="3" placeholder="" value="2">
-							<input class="components-range-control__number" type="number" min="1" max="3" placeholder="" value="2">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>`
-
-		document.body.appendChild( panel )
-	} )
-
-	afterAll( () => {
-		document.body.removeChild( panel )
-	} )
+		) )
+	}
 
 	let onClick, onHoverIn, onHoverOut
 	beforeEach( () => {
+		doRender()
 		startListening()
 		onClick = jest.fn()
 		onHoverIn = jest.fn()
@@ -175,40 +89,40 @@ describe( 'Help Video Snippets', () => {
 
 	it( 'clicking a panel should not trigger anything', () => {
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
-		getByText( document.body, 'Panel no tip no content' ).click()
+		getByText( 'Panel no tip no content' ).click()
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
-		getByText( document.body, 'Panel with tip no content' ).click()
+		getByText( 'Panel with tip no content' ).click()
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
-		getByText( document.body, 'Panel with tip with content no tip' ).click()
+		getByText( 'Panel with tip with content no tip' ).click()
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
-		getByText( document.body, 'Panel with tip with content with tip' ).click()
+		getByText( 'Panel with tip with content with tip' ).click()
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
-		getByText( document.body, 'Panel no tip with content no tip' ).click()
+		getByText( 'Panel no tip with content no tip' ).click()
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
-		getByText( document.body, 'Panel no tip with content with tip' ).click()
+		getByText( 'Panel no tip with content with tip' ).click()
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
 	} )
 
 	it( 'hovering a panel without a help tip should not trigger', () => {
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOver( getByText( document.body, 'Panel no tip no content' ) )
+		fireEvent.mouseOver( getByText( 'Panel no tip no content' ) )
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOver( getByText( document.body, 'Panel no tip with content no tip' ) )
+		fireEvent.mouseOver( getByText( 'Panel no tip with content no tip' ) )
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOver( getByText( document.body, 'Panel no tip with content with tip' ) )
+		fireEvent.mouseOver( getByText( 'Panel no tip with content with tip' ) )
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
 
 		expect( onHoverOut ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOut( getByText( document.body, 'Panel no tip no content' ) )
+		fireEvent.mouseOut( getByText( 'Panel no tip no content' ) )
 		expect( onHoverOut ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOut( getByText( document.body, 'Panel no tip with content no tip' ) )
+		fireEvent.mouseOut( getByText( 'Panel no tip with content no tip' ) )
 		expect( onHoverOut ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOut( getByText( document.body, 'Panel no tip with content with tip' ) )
+		fireEvent.mouseOut( getByText( 'Panel no tip with content with tip' ) )
 		expect( onHoverOut ).toHaveBeenCalledTimes( 0 )
 	} )
 
 	it( 'hovering a panel with a help tip should trigger', () => {
-		const panel = getByText( document.body, 'Panel with tip no content' )
+		const panel = getByText( 'Panel with tip no content' )
 		const panelParent = getParentControl( panel )
 
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
@@ -223,14 +137,14 @@ describe( 'Help Video Snippets', () => {
 
 	it( 'clicking a control without a help tip should not trigger', () => {
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
-		getByText( document.body, 'Control no tip in panel with tip' ).click()
+		getByText( 'Control no tip in panel with tip' ).click()
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
-		getByText( document.body, 'Control no tip in panel no tip' ).click()
+		getByText( 'Control no tip in panel no tip' ).click()
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
 	} )
 
 	it( 'clicking a control with a tip should trigger', () => {
-		let control = getByText( document.body, 'Control with tip in panel with tip' )
+		let control = getByText( 'Control with tip in panel with tip' )
 		let controlParent = getParentControl( control )
 
 		expect( onClick ).toHaveBeenCalledTimes( 0 )
@@ -238,7 +152,7 @@ describe( 'Help Video Snippets', () => {
 		expect( onClick ).toHaveBeenCalledTimes( 1 )
 		expect( onClick ).toHaveBeenLastCalledWith( control, controlParent )
 
-		control = getByText( document.body, 'Control with tip in panel no tip' )
+		control = getByText( 'Control with tip in panel no tip' )
 		controlParent = getParentControl( control )
 
 		expect( onClick ).toHaveBeenCalledTimes( 1 )
@@ -249,20 +163,20 @@ describe( 'Help Video Snippets', () => {
 
 	it( 'hovering a control without a help tip should not trigger', () => {
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOver( getByText( document.body, 'Control no tip in panel with tip' ) )
+		fireEvent.mouseOver( getByText( 'Control no tip in panel with tip' ) )
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOver( getByText( document.body, 'Control no tip in panel no tip' ) )
+		fireEvent.mouseOver( getByText( 'Control no tip in panel no tip' ) )
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
 
 		expect( onHoverOut ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOut( getByText( document.body, 'Control no tip in panel with tip' ) )
+		fireEvent.mouseOut( getByText( 'Control no tip in panel with tip' ) )
 		expect( onHoverOut ).toHaveBeenCalledTimes( 0 )
-		fireEvent.mouseOut( getByText( document.body, 'Control no tip in panel no tip' ) )
+		fireEvent.mouseOut( getByText( 'Control no tip in panel no tip' ) )
 		expect( onHoverOut ).toHaveBeenCalledTimes( 0 )
 	} )
 
 	it( 'hovering a control with a help tip should trigger', () => {
-		let control = getByText( document.body, 'Control with tip in panel with tip' )
+		let control = getByText( 'Control with tip in panel with tip' )
 		let controlParent = getParentControl( control )
 
 		expect( onHoverIn ).toHaveBeenCalledTimes( 0 )
@@ -274,7 +188,7 @@ describe( 'Help Video Snippets', () => {
 		fireEvent.mouseOut( control )
 		expect( onHoverOut ).toHaveBeenCalledTimes( 1 )
 
-		control = getByText( document.body, 'Control with tip in panel no tip' )
+		control = getByText( 'Control with tip in panel no tip' )
 		controlParent = getParentControl( control )
 
 		expect( onHoverIn ).toHaveBeenCalledTimes( 1 )
