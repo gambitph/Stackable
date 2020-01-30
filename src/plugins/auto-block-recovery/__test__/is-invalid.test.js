b5ac4e09ba6847e07992b5aabac0160c3883b3c1
Benjamin Intal <bfintal@gmail.com>
2020-01-30 14:40:02 +0800

    [auto-block-recovery] Better auto-recovery detection using Tokenizer (how Gutenberg does it), also now detects SVGs with Style errors

--- src/plugins/auto-block-recovery/__test__/is-invalid.test.js
+++ src/plugins/auto-block-recovery/__test__/is-invalid.test.js
@@ -1,4 +1,4 @@
-import { isInvalid } from '../is-invalid'
+import { isInvalid, getInequivalentHTMLError } from '../is-invalid'
 
 import {
 	registerBlockType,
@@ -12,7 +12,7 @@ describe( 'isInvalid', () => {
 		console.error = jest.fn() // eslint-disable-line no-console
 	} )
 
-	it( 'should not affect errors outside the <style> tag', () => {
+	it( 'should not affect errors outside the allowed tags', () => {
 		const blockSettings = {
 			category: 'common',
 			save: () => <div><style>test</style><p attr="value">test</p></div>,
@@ -28,7 +28,7 @@ describe( 'isInvalid', () => {
 			innerBlocks: [],
 		} )
 
-		expect( isInvalid( block ) ).toBe( false )
+		expect( isInvalid( block, [ 'style', 'svg' ] ) ).toBe( false )
 
 		const block2 = createBlockWithFallback( {
 			blockName: 'ugb/dummy',
@@ -37,7 +37,7 @@ describe( 'isInvalid', () => {
 			innerBlocks: [],
 		} )
 
-		expect( isInvalid( block2 ) ).toBe( false )
+		expect( isInvalid( block2, [ 'style', 'svg' ] ) ).toBe( false )
 
 		const block3 = createBlockWithFallback( {
 			blockName: 'ugb/dummy',
@@ -46,7 +46,7 @@ describe( 'isInvalid', () => {
 			innerBlocks: [],
 		} )
 
-		expect( isInvalid( block3 ) ).toBe( false )
+		expect( isInvalid( block3, [ 'style', 'svg' ] ) ).toBe( false )
 
 		const block4 = createBlockWithFallback( {
 			blockName: 'ugb/dummy',
@@ -55,7 +55,7 @@ describe( 'isInvalid', () => {
 			innerBlocks: [],
 		} )
 
-		expect( isInvalid( block4 ) ).toBe( false )
+		expect( isInvalid( block4, [ 'style', 'svg' ] ) ).toBe( false )
 
 		const block5 = createBlockWithFallback( {
 			blockName: 'ugb/dummy',
@@ -64,7 +64,7 @@ describe( 'isInvalid', () => {
 			innerBlocks: [],
 		} )
 
-		expect( isInvalid( block5 ) ).toBe( false )
+		expect( isInvalid( block5, [ 'style', 'svg' ] ) ).toBe( false )
 
 		unregisterBlockType( 'ugb/dummy' )
 	} )
@@ -85,7 +85,16 @@ describe( 'isInvalid', () => {
 			innerBlocks: [],
 		} )
 
-		expect( isInvalid( block ) ).toBe( true )
+		expect( isInvalid( block, [ 'style', 'p' ] ) ).toBe( true )
+
+		const block2 = createBlockWithFallback( {
+			blockName: 'ugb/dummy',
+			innerHTML: '<div><style>test</style><p attr="value">changed</p></div>',
+			attrs: {},
+			innerBlocks: [],
+		} )
+
+		expect( isInvalid( block2, [ 'style', 'p' ] ) ).toBe( true )
 
 		unregisterBlockType( 'ugb/dummy' )
 	} )
@@ -151,5 +160,53 @@ describe( 'isInvalid', () => {
 		expect( isInvalid( block ) ).toBe( false )
 
 		unregisterBlockType( 'ugb/dummy' )
+	} )
+
+	it( 'should detect invalid blocks even if nested', () => {
+		const blockSettings = {
+			category: 'common',
+			save: () => <div>test<aside>test</aside></div>,
+			title: 'Test Block',
+		}
+
+		registerBlockType( 'ugb/dummy', blockSettings )
+
+		const block = createBlockWithFallback( {
+			blockName: 'ugb/dummy',
+			innerHTML: '<div>test<aside>changed</aside></div>',
+			attrs: {},
+			innerBlocks: [],
+		} )
+
+		expect( isInvalid( block, [ 'aside' ] ) ).toBe( true )
+		expect( isInvalid( block, [ 'div' ] ) ).toBe( true )
+
+		const block2 = createBlockWithFallback( {
+			blockName: 'ugb/dummy',
+			innerHTML: '<div>test<aside class="test">test</aside></div>',
+			attrs: {},
+			innerBlocks: [],
+		} )
+
+		expect( isInvalid( block2, [ 'aside' ] ) ).toBe( true )
+		expect( isInvalid( block2, [ 'div' ] ) ).toBe( true )
+
+		unregisterBlockType( 'ugb/dummy' )
+	} )
+} )
+
+describe( 'getInequivalentHTMLError', () => {
+	it( 'should return false if no error', () => {
+		expect( getInequivalentHTMLError( '<div>test</div>', '<div>test</div>' ) ).toBe( false )
+		expect( getInequivalentHTMLError( '<div><footer/>test</div><style>test</style>', '<div><footer/>test</div><style>test</style>' ) ).toBe( false )
+		expect( getInequivalentHTMLError( '', '' ) ).toBe( false )
+	} )
+	it( 'should return the html tag tree where the error occurred', () => {
+		expect( getInequivalentHTMLError( '<aside>test</aside>', '<aside>test2</aside>' ) ).toEqual( [ 'aside' ] )
+		expect( getInequivalentHTMLError( '<aside><div></div></aside>', '<aside><div class="test"></div></aside>' ) ).toEqual( [ 'aside', 'div' ] )
+		expect( getInequivalentHTMLError( '<aside><div></div><style>test</style></aside>', '<aside><div></div><style>test2</style></aside>' ) ).toEqual( [ 'aside', 'style' ] )
+		expect( getInequivalentHTMLError( '<aside><div></div></aside><style>test</style>', '<aside><div></div></aside><style>test2</style>' ) ).toEqual( [ 'style' ] )
+		expect( getInequivalentHTMLError( '<aside><div/><style>test</style></aside>', '<aside><div/><style>test2</style></aside>' ) ).toEqual( [ 'aside', 'style' ] )
+		expect( getInequivalentHTMLError( '<aside><div class="test"/><style>test</style></aside>', '<aside><div class="test2"/><style>test</style></aside>' ) ).toEqual( [ 'aside', 'div' ] )
 	} )
 } )
--- src/plugins/auto-block-recovery/is-invalid.js
+++ src/plugins/auto-block-recovery/is-invalid.js
@@ -1,7 +1,18 @@
 // Checks whether the block is invalid because of a styling issue.
-export const isInvalid = block => {
+import { Tokenizer } from 'simple-html-tokenizer'
+import {
+	getSaveContent,
+} from '@wordpress/blocks'
+import {
+	DecodeEntityParser, getNextNonWhitespaceToken, isClosedByToken, isEqualTokensOfType,
+} from '@wordpress/blocks/build/api/validation'
+
+// We will auto-recover if there are errors encountered in these tags.
+const ALLOWED_ERROR_TAGS = [ 'style', 'svg' ]
+
+export const isInvalid = ( block, allowedTags = ALLOWED_ERROR_TAGS ) => {
 	const {
-		name, isValid, validationIssues, originalContent,
+		name, isValid, validationIssues,
 	} = block
 
 	// Only do this for Stackable blocks.
@@ -17,20 +28,137 @@ export const isInvalid = block => {
 		return false
 	}
 
-	// Check whether the error came from the block's <style> contents.
-	// Only the latest issue.
-	const issue = validationIssues[ 0 ]
-	const { args } = issue
-	if ( args.length !== 3 ) {
+	// Get which HTML tags the error occurred.
+	const tags = getInvalidationTags( block )
+	if ( ! tags ) {
 		return false
 	}
 
-	// Get the HTML code that generated the error.
-	const sourceError = args[ 2 ]
-	if ( typeof sourceError !== 'string' ) {
+	// If the error comes from SVG tags or the Style tag, then we can repair this invalid block.
+	return tags.some( tag => allowedTags.includes( tag ) )
+}
+
+/**
+ * Gets the HTML tag tree where the invalid block error ocurred.
+ *
+ * @param {Object} block The invalid block object
+ *
+ * @return {Array|boolean} An array of HTML tags where the error in the invalid
+ * block occurred, or false if not applicable or not detected.
+ */
+export const getInvalidationTags = block => {
+	const {
+		name, attributes, innerBlocks, originalContent, isValid,
+	} = block
+
+	if ( isValid ) {
 		return false
 	}
 
-	// For any errors that come from styling, auto-attempt recovery for these.
-	return originalContent.includes( `<style>${ sourceError }</style>` )
+	let expectedContent
+	try {
+		expectedContent = getSaveContent( name, attributes, innerBlocks )
+	} catch ( error ) {
+		return false
+	}
+
+	return getInequivalentHTMLError( originalContent, expectedContent )
+}
+
+/**
+ * Tokenize an HTML string, gracefully handling any errors thrown during
+ * underlying tokenization.
+ *
+ * Copied from @wordpress/blocks/build/api/validation
+ *
+ * @see getHTMLTokens in @wordpress/blocks/build/api/validation
+ *
+ * @param {string} html   HTML string to tokenize.
+ *
+ * @return {Object[]|null} Array of valid tokenized HTML elements, or null on error
+ */
+function getHTMLTokens( html ) {
+	try {
+		return new Tokenizer( new DecodeEntityParser() ).tokenize( html )
+	} catch ( e ) {
+	}
+
+	return null
+}
+
+/**
+ * Returns the tag trail (array of html tags) where the mismatch occurred during
+ * the test if the given HTML strings are effectively equivalent. Returns false
+ * when the mismatch cannot be pinpointed or if there is no error or for invalid
+ * HTML. This is not a good detector whether 2 HTML strings are equivalent.
+ *
+ * This function is a modification of the isEquivalentHTML function.
+ *
+ * @see isEquivalentHTML in @wordpress/blocks/build/api/validation
+ *
+ * @param {string} actual   Actual HTML string.
+ * @param {string} expected Expected HTML string.
+ *
+ * @return {Array|boolean} An array containing the HTML tags where the error occurred,
+ * or false if it isn't available
+ */
+export function getInequivalentHTMLError( actual, expected ) {
+	// Tokenize input content and reserialized save content
+	const [ actualTokens, expectedTokens ] = [ actual, expected ].map(
+		html => getHTMLTokens( html )
+	)
+
+	// If either is malformed then stop comparing - the strings are not equivalent
+	if ( ! actualTokens || ! expectedTokens ) {
+		return false
+	}
+
+	const htmlTagTrail = []
+	let actualToken, expectedToken
+	while ( ( actualToken = getNextNonWhitespaceToken( actualTokens ) ) ) {
+		expectedToken = getNextNonWhitespaceToken( expectedTokens )
+
+		// Add new opening tags to the tag trail.
+		if ( actualToken.type === 'StartTag' ) {
+			htmlTagTrail.push( actualToken.tagName.toLowerCase() )
+		}
+
+		// Inequal if exhausted all expected tokens
+		if ( ! expectedToken ) {
+			return htmlTagTrail
+		}
+
+		// Inequal if next non-whitespace token of each set are not same type
+		if ( actualToken.type !== expectedToken.type ) {
+			return htmlTagTrail
+		}
+
+		// Defer custom token type equality handling, otherwise continue and
+		// assume as equal
+		const isEqualTokens = isEqualTokensOfType[ actualToken.type ]
+		if ( isEqualTokens && ! isEqualTokens( actualToken, expectedToken ) ) {
+			return htmlTagTrail
+		}
+
+		// Remove tags from the tag trail if they close.
+		if ( actualToken.type === 'StartTag' && actualToken.selfClosing === true ) {
+			htmlTagTrail.pop()
+		} else if ( actualToken.type === 'EndTag' ) {
+			htmlTagTrail.pop()
+		}
+
+		// Peek at the next tokens (actual and expected) to see if they close
+		// a self-closing tag
+		if ( isClosedByToken( actualToken, expectedTokens[ 0 ] ) ) {
+			// Consume the next expected token that closes the current actual
+			// self-closing token
+			getNextNonWhitespaceToken( expectedTokens )
+		} else if ( isClosedByToken( expectedToken, actualTokens[ 0 ] ) ) {
+			// Consume the next actual token that closes the current expected
+			// self-closing token
+			getNextNonWhitespaceToken( actualTokens )
+		}
+	}
+
+	return false
 }
