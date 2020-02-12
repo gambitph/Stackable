3940e7795181d64bc0211f8d4827d510e7f564be
Benjamin Intal <bfintal@gmail.com>
2020-02-13 07:42:38 +0800

    test (component: base control multi-label): added new toggle tests

--- src/components/base-control-multi-label/__test__/index.test.js
+++ src/components/base-control-multi-label/__test__/index.test.js
@@ -5,19 +5,19 @@ import BaseControlMultiLabel from '../'
 import { render, fireEvent } from '@testing-library/react'
 
 describe( 'Unit switcher', () => {
-	test( 'should have a label', () => {
+	it( 'should have a label', () => {
 		const { getByText } = render( <BaseControlMultiLabel label="hello" /> )
 		expect( getByText( 'hello' ) ).toBeTruthy()
 	} )
 
-	test( 'should not display units if single', () => {
+	it( 'should not display units if single', () => {
 		const { queryByRole, queryByText } = render( <BaseControlMultiLabel /> )
 		expect( queryByRole( 'button' ) ).toBeNull()
 		expect( queryByText( 'px' ) ).toBeNull()
 		expect( queryByText( '%' ) ).toBeNull()
 	} )
 
-	test( 'should display units if multiple (2 buttons)', () => {
+	it( 'should display units if multiple (2 buttons)', () => {
 		const { getByText } = render( <BaseControlMultiLabel units={ [ 'px', '%' ] } /> )
 
 		expect( getByText( 'px' ) ).toBeTruthy()
@@ -26,7 +26,7 @@ describe( 'Unit switcher', () => {
 		expect( getByText( '%' ) ).not.toHaveClass( 'is-active' )
 	} )
 
-	test( 'should display units if multiple (3 buttons)', () => {
+	it( 'should display units if multiple (3 buttons)', () => {
 		const { getByText } = render( <BaseControlMultiLabel unit="em" units={ [ 'px', 'em', '%' ] } /> )
 
 		expect( getByText( 'px' ) ).toBeTruthy()
@@ -37,11 +37,38 @@ describe( 'Unit switcher', () => {
 		expect( getByText( '%' ) ).not.toHaveClass( 'is-active' )
 	} )
 
-	test( 'should change units', () => {
+	it( 'should change units', () => {
 		const onChange = jest.fn()
 
 		const { getByText } = render( <BaseControlMultiLabel units={ [ 'px', '%' ] } onChangeUnit={ onChange } /> )
 		fireEvent.click( getByText( '%' ) )
 		expect( onChange ).toHaveBeenCalledWith( '%' )
+	} )
+
+	it( 'should not have responsive if only 1 screen', () => {
+		const { queryByLabelText } = render( <BaseControlMultiLabel screens={ [ 'desktop' ] } /> )
+		expect( queryByLabelText( 'Desktop' ) ).toBeNull()
+	} )
+
+	it( 'should have responsive toggles for multiple screens', () => {
+		const onChangeScreen = jest.fn()
+
+		const { getByLabelText, queryByLabelText } = render( <BaseControlMultiLabel screens={ [ 'desktop', 'tablet', 'mobile' ] } onChangeScreen={ onChangeScreen } /> )
+		expect( getByLabelText( 'Desktop' ) ).toBeTruthy()
+		expect( queryByLabelText( 'Tablet' ) ).toBeNull()
+		expect( queryByLabelText( 'Mobile' ) ).toBeNull()
+
+		// Click desktop to show the other responsive buttons.
+		fireEvent.click( getByLabelText( 'Desktop' ) )
+		expect( getByLabelText( 'Tablet' ) ).toBeTruthy()
+		expect( getByLabelText( 'Mobile' ) ).toBeTruthy()
+
+		expect( onChangeScreen ).toHaveBeenCalledWith( 'desktop' )
+		fireEvent.click( getByLabelText( 'Tablet' ) )
+		expect( onChangeScreen ).toHaveBeenCalledWith( 'tablet' )
+		fireEvent.click( getByLabelText( 'Mobile' ) )
+		expect( onChangeScreen ).toHaveBeenCalledWith( 'mobile' )
+		fireEvent.click( getByLabelText( 'Desktop' ) )
+		expect( onChangeScreen ).toHaveBeenCalledWith( 'desktop' )
 	} )
 } )
