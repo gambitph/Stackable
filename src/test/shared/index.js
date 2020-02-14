845c68b0918a7f9d08360c5c5dc5170fad08cf18
Benjamin Intal <bfintal@gmail.com>
2020-02-14 19:09:15 +0800

    test: removed shared testing functions

--- src/test/shared/index.js
+++ src/test/shared/index.js
@@ -1,8 +1,2 @@
-// TODO: remove these
-export { default as blockEditableAfterSaveTests } from './block-editable-after-save'
-export { default as blockMigrationTests } from './block-migration'
-export { default as blockSaveSnapshotTests } from './block-save-snapshot'
-
 export { default as blockAttributeTests } from './block-attributes'
-export { default as blockInspectorTests } from './block-inspector'
 export { default as blockStyleTests } from './block-style'
