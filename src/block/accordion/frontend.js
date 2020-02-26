fc8b17babce55d0c56db751051c0193e46e571f1
Benjamin Intal <bfintal@gmail.com>
2020-02-26 23:37:19 +0800

    fix (accordion block): allow dynamic content inside accordions. better open/close calculations

--- src/block/accordion/frontend.js
+++ src/block/accordion/frontend.js
@@ -3,52 +3,90 @@
  */
 import domReady from '@wordpress/dom-ready'
 
-const toggleAccordion = el => {
+const isPanelOpen = el => {
+	return el.classList.contains( 'ugb-accordion--open' )
+}
+
+const toggleAccordion = ( el, skipAdjacent = false ) => {
+	const content = el.querySelector( '.ugb-accordion__text, .ugb-accordion__content' )
+
+	// Clear any animations currently happening.
+	clearTimeout( el.aTimeout )
+
+	if ( ! isPanelOpen( el ) ) {
+		// Set the max height to animate.
+		content.style.maxHeight = el.getAttribute( 'data-height' )
+
+		// After the animation, remove the max-height so that just incase it's
+		// wrong, we can still show the rest of the contents.
+		el.aTimeout = setTimeout( () => {
+			content.style.maxHeight = ''
+		}, 350 )
+	} else {
+		// Remember the current height of the open state.
+		el.setAttribute( 'data-height', `${ content.clientHeight + 50 }px` )
+		// We need to set the maxHeight first because we previously cleared it.
+		content.style.maxHeight = el.getAttribute( 'data-height' )
+
+		// Set the height to 0 in a setTimeout to animate the closing.
+		el.aTimeout = setTimeout( () => {
+			content.style.maxHeight = 0
+		}, 1 )
+	}
+
+	// Set the accordion classes.
+	el.setAttribute( 'aria-expanded', isPanelOpen( el ) ? 'true' : 'false' )
 	el.classList.toggle( 'ugb-accordion--open' )
-	el.setAttribute( 'aria-expanded', el.classList.contains( 'ugb-accordion--open' ) ? 'true' : 'false' )
+
+	// This is true if called by an adjacent close.
+	if ( skipAdjacent ) {
+		return
+	}
 
 	// Close other adjacent accordions if needed.
 	if ( el.classList.contains( 'ugb-accordion--single-open' ) ) {
 		let adjacent = el.nextElementSibling
 		while ( adjacent && adjacent.classList.contains( 'ugb-accordion' ) ) {
-			forceCloseAccordion( adjacent )
+			if ( isPanelOpen( adjacent ) ) {
+				toggleAccordion( adjacent, true )
+			}
+			// forceCloseAccordion( adjacent )
 			adjacent = adjacent.nextElementSibling
 		}
 		adjacent = el.previousElementSibling
 		while ( adjacent && adjacent.classList.contains( 'ugb-accordion' ) ) {
-			forceCloseAccordion( adjacent )
+			if ( isPanelOpen( adjacent ) ) {
+				toggleAccordion( adjacent, true )
+			}
+			// forceCloseAccordion( adjacent )
 			adjacent = adjacent.previousElementSibling
 		}
 	}
 }
 
-const forceCloseAccordion = el => {
-	el.classList.remove( 'ugb-accordion--open' )
-	el.setAttribute( 'aria-expanded', 'false' )
-}
-
 const detectMaxHeight = el => {
 	const isOpen = el.classList.contains( 'ugb-accordion--open' )
+	const content = el.querySelector( '.ugb-accordion__text, .ugb-accordion__content' )
 
 	// Open the accordion if needed.
 	if ( ! isOpen ) {
 		el.style.display = 'none'
+		content.style.maxHeight = 'none'
 		el.classList.toggle( 'ugb-accordion--open' )
 		el.style.display = ''
 	}
 
 	// Get the height in its open state.
 	const contentHeight = el.querySelector( '.ugb-accordion__text, .ugb-accordion__content' ).clientHeight
+	el.setAttribute( 'data-height', `${ contentHeight + 50 }px` )
 
 	// Bring back the previous state.
 	if ( ! isOpen ) {
 		el.style.display = 'none'
 		el.classList.toggle( 'ugb-accordion--open' )
+		content.style.maxHeight = ''
 		el.style.display = ''
 	}
-
-	// Set the max height that we'll use
-	el.style.setProperty( '--max-height', `${ contentHeight + 50 }px` )
 }
 
 let instanceID = 1
