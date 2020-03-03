d8968708a3683fb925a7ca2ba43a4921907172ae
Benjamin Intal <bfintal@gmail.com>
2020-03-03 17:45:11 +0800

    fix (design library): switching designs from a block created from the design library now switched images correctly. closes #618

--- src/block/design-library/edit.js
+++ src/block/design-library/edit.js
@@ -16,6 +16,7 @@ import { createBlock, parse } from '@wordpress/blocks'
 import { withDispatch } from '@wordpress/data'
 import { useState } from '@wordpress/element'
 import { __ } from '@wordpress/i18n'
+import { applyFilters } from '@wordpress/hooks'
 
 const edit = ( { replaceBlockWithAttributes, replaceBlocWithContent } ) => {
 	const [ isLibraryOpen, setIsLibraryOpen ] = useState( false ) // eslint-disable-line react-hooks/rules-of-hooks
@@ -67,7 +68,10 @@ export default compose( [
 		return {
 			// Replaces the current block with a block made out of attributes.
 			replaceBlockWithAttributes: ( blockName, attributes, innerBlocks ) => {
-				const block = createBlock( blockName, attributes, innerBlocks )
+				const shortBlockName = blockName.replace( /^\w+\//g, '' )
+				const blockAttributes = applyFilters( `stackable.${ shortBlockName }.design.filtered-block-attributes`, attributes )
+
+				const block = createBlock( blockName, blockAttributes, innerBlocks )
 				replaceBlock( clientId, block )
 			},
 			// Replaces the current block with one or more blocks from a string.
