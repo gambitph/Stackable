f82f96fd6033f86f31760d6aa3029b8c60c3e2f5
Benjamin Intal <bfintal@gmail.com>
2020-05-26 15:20:43 +0800

    test: try mkdir build error fix

--- src/welcome/build.js
+++ src/welcome/build.js
@@ -7,20 +7,26 @@ const glob = require( 'glob' )
 const path = require( 'path' )
 
 // Create folder if it doesn't exist yet.
-fs.mkdir( path.resolve( './dist/videos/welcome/' ), { recursive: true }, err => {
+fs.mkdir( path.resolve( './dist/videos/' ), { recursive: true }, err => {
 	if ( err ) {
 		throw err
 	}
 
-	glob.sync( path.resolve( __dirname, './videos/*.mp4' ) ).forEach( video => {
-		const dest = './dist/videos/welcome/' + path.basename( video )
-		const destAbs = path.resolve( `./dist/videos/welcome/${ path.basename( video ) }` )
+	fs.mkdir( path.resolve( './dist/videos/welcome/' ), { recursive: true }, err => {
+		if ( err ) {
+			throw err
+		}
 
-		fs.copyFile( video, destAbs, err => {
-			if ( err ) {
-				throw err
-			}
-			console.log( `✔️  Sucessfully writen ${ dest }` ) // eslint-disable-line
+		glob.sync( path.resolve( __dirname, './videos/*.mp4' ) ).forEach( video => {
+			const dest = './dist/videos/welcome/' + path.basename( video )
+			const destAbs = path.resolve( `./dist/videos/welcome/${ path.basename( video ) }` )
+
+			fs.copyFile( video, destAbs, err => {
+				if ( err ) {
+					throw err
+				}
+				console.log( `✔️  Sucessfully writen ${ dest }` ) // eslint-disable-line
+			} )
 		} )
 	} )
 } )
