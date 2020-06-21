b210f803ceb65879649a7ef705b720c87a5209ac
Benjamin Intal <bfintal@gmail.com>
2020-06-21 21:02:23 +0800

    load font awesome icon loading and settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,6 +52,8 @@ module.exports = {
 		// Off since returning false positives.
 		'@wordpress/no-unused-vars-before-return': 'off',
 
+		'jsdoc/no-undefined-types': 'off',
+
 		'@wordpress/no-unguarded-get-range-at': 'off',
 	},
 	globals: {
--- plugin.php
+++ plugin.php
@@ -116,6 +116,7 @@ require_once( plugin_dir_path( __FILE__ ) . 'src/block/disabled-blocks.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'src/init-deprecated.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'src/init.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'src/fonts.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'src/icons.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'src/block/blog-posts/index.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'src/pro.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'src/help/help-tooltip.php' );
