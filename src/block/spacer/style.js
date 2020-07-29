3a182666599c5b870caefd40d9c1157ab6f64db5
Nico Mendoza <blankartwp@gmail.com>
2020-07-29 14:09:59 +0800

    Added default height values for Spacer block

--- src/block/spacer/style.js
+++ src/block/spacer/style.js
@@ -4,10 +4,20 @@
 import { createBackgroundStyleSet, __getValue } from '~stackable/util'
 import deepmerge from 'deepmerge'
 
+/**
+ * Default attribute values if
+ * no value is passed
+ */
+const defaultValues = {
+	height: '50px',
+}
+
 export const createStyles = props => {
 	const getValue = __getValue( props.attributes )
 
 	const styles = []
+
+	const { height } = defaultValues
 
 	styles.push( {
 		'.ugb-spacer': {
@@ -15,12 +25,12 @@ export const createStyles = props => {
 		},
 		tablet: {
 			'.ugb-spacer': {
-				height: getValue( 'tabletHeight', `%s${ getValue( 'tabletHeightUnit' ) || 'px' }` ),
+				height: getValue( 'tabletHeight', `%s${ getValue( 'tabletHeightUnit' ) || 'px' }` ) || height,
 			},
 		},
 		mobile: {
 			'.ugb-spacer': {
-				height: getValue( 'mobileHeight', `%s${ getValue( 'mobileHeightUnit' ) || 'px' }` ),
+				height: getValue( 'mobileHeight', `%s${ getValue( 'mobileHeightUnit' ) || 'px' }` ) || height,
 			},
 		},
 	} )
