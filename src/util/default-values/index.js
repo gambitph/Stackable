5ea75b2e43d1c7bfec292f6dd80a6bdd73eb58ec
Nico Mendoza <blankartwp@gmail.com>
2020-07-30 12:40:13 +0800

    Fixed the inheritDesktopAttribute function.

--- src/util/default-values/index.js
+++ src/util/default-values/index.js
@@ -11,17 +11,18 @@ or tablet attribute is not yet defined.
  * @param {string} unit the unit used by the attribute
  * @param {number} maxValue if defined, only set the value less than or equal to maxValue
  * @param {boolean} doImportant if this is false, !important won't be appended
+ * @param {*} fallback if no maxValue is defined or value is less than or equal to maxValue, this will be returned
  *
  * @return {*} attribute to be passed
  */
-export const inheritDesktopAttribute = ( attribute = '', unit = 'px', maxValue = null, doImportant = true ) => {
+export const inheritDesktopAttribute = ( attribute = '', unit = 'px', maxValue = null, doImportant = true, fallback ) => {
 	if ( attribute !== '' ) {
-		if ( maxValue ) {
-			if ( attribute <= maxValue ) {
-				return appendImportant( `${ attribute }${ unit || 'px' }`, doImportant )
+		if ( maxValue !== null ) {
+			if ( parseFloat( attribute ) <= parseFloat( maxValue ) ) {
+				return fallback
 			}
-			return appendImportant( `${ maxValue }${ unit || 'px' }`, doImportant )
+			return appendImportant( `${ maxValue }${ unit }`, doImportant )
 		}
-		return appendImportant( `${ attribute }${ unit || 'px' }`, doImportant )
+		return undefined
 	}
 }
