75ef6d6c31c5c5fa782048beed110d274766ae1e
Nico Mendoza <blankartwp@gmail.com>
2020-08-14 18:41:58 +0800

    Omit texts 1-6

--- src/block/icon-list/design.js
+++ src/block/icon-list/design.js
@@ -12,5 +12,11 @@ import { addFilter } from '@wordpress/hooks'
 addFilter( 'stackable.icon-list.design.no-text-attributes', 'stackable/icon-list', attributes => {
 	return omit( attributes, [
 		'text',
+		'text1',
+		'text2',
+		'text3',
+		'text4',
+		'text5',
+		'text6',
 	] )
 } )
