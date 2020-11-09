d52ad668a8007d4f5da3c00a6982d34a9472f246
Nico Mendoza <blankartwp@gmail.com>
2020-11-09 16:35:23 +0800

    Transitioned blog-posts

--- src/block/blog-posts/edit.js
+++ src/block/blog-posts/edit.js
@@ -16,6 +16,7 @@ import {
 	ProControl,
 	BlockContainer,
 	ContentAlignControl,
+	ColumnPaddingControl,
 	BackgroundControlsHelper,
 	PanelAdvancedSettings,
 	TypographyControlHelper,
@@ -29,6 +30,7 @@ import {
 	AdvancedSelectControl,
 	DivBackground,
 	ButtonEditHelper,
+	ButtonIconPopoverControl,
 } from '~stackable/components'
 import {
 	createTypographyAttributeNames,
@@ -163,7 +165,7 @@ addFilter( 'stackable.blog-posts.edit.inspector.style.before', 'stackable/blog-p
 					max={ 4 }
 					className="ugb--help-tip-general-columns"
 				/>
-				{ show.borderRadius &&
+				{ ! show.columnBackground && show.borderRadius &&
 					<AdvancedRangeControl
 						label={ __( 'Border Radius', i18n ) }
 						value={ borderRadius }
@@ -175,7 +177,7 @@ addFilter( 'stackable.blog-posts.edit.inspector.style.before', 'stackable/blog-p
 						className="ugb--help-tip-general-border-radius"
 					/>
 				}
-				{ show.shadow &&
+				{ ! show.columnBackground && show.shadow &&
 					<AdvancedRangeControl
 						label={ __( 'Shadow / Outline', i18n ) }
 						value={ shadow }
@@ -255,20 +257,41 @@ addFilter( 'stackable.blog-posts.edit.inspector.style.before', 'stackable/blog-p
 
 			{ show.columnBackground &&
 				<PanelAdvancedSettings
-					title={ __( 'Column Background', i18n ) }
+					title={ __( 'Container', i18n ) }
 					id="column-background"
 					initialOpen={ false }
 					className="ugb--help-tip-column-background-on-off"
 				>
-					{ ( show.showBackgroundInItem || show.showBackgroundInContent ) &&
+					<ButtonIconPopoverControl
+						label={ __( 'Background', i18n ) }
+						popoverLabel={ __( 'Background', i18n ) }
+						onReset={ () => {
+							setAttributes( {
+								columnBackgroundColorType: '',
+								columnBackgroundColor: '',
+								columnBackgroundColor2: '',
+								columnBackgroundColorOpacity: '',
+								columnBackgroundMediaID: '',
+								columnBackgroundMediaUrl: '',
+								columnBackgroundTintStrength: '',
+								columnFixedBackground: '',
+							} )
+						} }
+						allowReset={ props.attributes.columnBackgroundColor || props.attributes.columnBackgroundMediaUrl }
+						hasColorPreview={ props.attributes.columnBackgroundColor }
+						hasImagePreview={ props.attributes.columnBackgroundMediaUrl }
+						colorPreview={ props.attributes.columnBackgroundColorType === 'gradient' ? [ props.attributes.columnBackgroundColor, props.attributes.columnBackgroundColor2 ] : props.attributes.columnBackgroundColor }
+						imageUrlPreview={ props.attributes.columnBackgroundMediaUrl }
+					>
+						{ ( show.showBackgroundInItem || show.showBackgroundInContent ) &&
 						<BackgroundControlsHelper
 							attrNameTemplate="column%s"
 							setAttributes={ setAttributes }
 							blockAttributes={ props.attributes }
 							backgroundMediaAllowVideo={ false }
 						/>
-					}
-					{ ! ( show.showBackgroundInItem || show.showBackgroundInContent ) && show.columnBackground &&
+						}
+						{ ! ( show.showBackgroundInItem || show.showBackgroundInContent ) && show.columnBackground &&
 						<BackgroundControlsHelper
 							attrNameTemplate="column%s"
 							setAttributes={ setAttributes }
@@ -277,7 +300,37 @@ addFilter( 'stackable.blog-posts.edit.inspector.style.before', 'stackable/blog-p
 							onChangeBackgroundColorOpacity={ false }
 							onChangeBackgroundGradientBlendMode={ false }
 						/>
+						}
+					</ButtonIconPopoverControl>
+					{ show.borderRadius &&
+					<AdvancedRangeControl
+						label={ __( 'Border Radius', i18n ) }
+						value={ borderRadius }
+						onChange={ borderRadius => setAttributes( { borderRadius } ) }
+						min={ 0 }
+						max={ 50 }
+						allowReset={ true }
+						placeholder="12"
+						className="ugb--help-tip-general-border-radius"
+					/>
 					}
+					{ show.shadow &&
+					<AdvancedRangeControl
+						label={ __( 'Shadow / Outline', i18n ) }
+						value={ shadow }
+						onChange={ shadow => setAttributes( { shadow } ) }
+						min={ 0 }
+						max={ 9 }
+						allowReset={ true }
+						placeholder="3"
+						className="ugb--help-tip-general-shadow"
+					/>
+					}
+					<ColumnPaddingControl
+						label={ __( 'Paddings', i18n ) }
+						setAttributes={ setAttributes }
+						attributes={ props.attributes }
+					/>
 				</PanelAdvancedSettings>
 			}
 
