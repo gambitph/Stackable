74f07e9d2f2e0b2ef69323dd16274aec830e8f64
Benjamin Intal <bfintal@gmail.com>
2020-11-13 17:06:50 +0800

    partially working

---
+++ src/components/panel-advanced-settings/with-memory.js
@@ -0,0 +1,82 @@
+/**
+ * In charge of keeping the previous state of the panels, whether the panels are
+ * open or closed. This assumes that there can only be one panel open at a time.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { Fragment, useMemo } from '@wordpress/element'
+import { select } from '@wordpress/data'
+import { addAction } from '@wordpress/hooks'
+
+const panelCache = {}
+const tabCache = {}
+
+// If the tab was changed, remember it.
+addAction( 'stackable.inspector.tab.click', 'stackable/panel-memory', ( clientId, tab ) => {
+	tabCache[ clientId ] = tab
+} )
+
+// Keep the tab position at the start.
+addAction( 'stackable.inspector.tab.initial', 'stackable/panel-memory', ( clientId, tab ) => {
+	if ( typeof tabCache[ clientId ] === 'undefined' ) {
+		tabCache[ clientId ] = tab
+	}
+} )
+
+const getTab = panelEl => {
+	const tabEl = panelEl.closest( '[data-ugb-tab]' )
+	if ( tabEl ) {
+		return tabEl.getAttribute( 'data-ugb-tab' )
+	}
+	return ''
+}
+
+const withMemory = WrappedComponent => {
+	const NewComp = props => {
+		const clientId = useMemo( () => select( 'core/block-editor' ).getSelectedBlockClientId(), [] )
+
+		// When a panel is opened, remember that.
+		const onToggle = ( isOpen, panelRef ) => {
+			const tab = getTab( panelRef.current )
+			const uid = `${ clientId }-${ tab }`
+			tabCache[ clientId ] = getTab( panelRef.current )
+			panelCache[ uid ] = isOpen ? props.title : null
+
+			props.onToggle( isOpen )
+		}
+
+		const propsToPass = {
+			...props,
+			onToggle,
+		}
+
+		const currentTab = tabCache[ clientId ] || ''
+		const uid = `${ clientId }-${ currentTab }`
+
+		// Open the last panel that was opened.
+		const lastOpenPanel = panelCache[ uid ]
+		if ( lastOpenPanel ) {
+			// Only 1 panel is open
+			propsToPass.initialOpen = lastOpenPanel === props.title
+		} else if ( lastOpenPanel === null ) {
+			// All are closed
+			propsToPass.initialOpen = false
+		}
+
+		return (
+			<Fragment>
+				<WrappedComponent { ...propsToPass } />
+			</Fragment>
+		)
+	}
+
+	NewComp.defaultProps = {
+		...( WrappedComponent.defaultProps || {} ),
+	}
+
+	return NewComp
+}
+
+export default withMemory
--- src/components/panel-tabs/with-memory.js
+++ src/components/panel-tabs/with-memory.js
@@ -1,7 +1,7 @@
 /**
- * Internal dependencies
+ * This is in charge of preserving the last selected tab for a block. So when
+ * selecting different blocks, the last selected tab is preserved.
  */
-import { closeAllOpenPanels } from './'
 
 /**
  * External dependencies
@@ -11,111 +11,46 @@ import { isUnmodifiedBlock } from '~stackable/util'
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element'
-import { createHigherOrderComponent } from '@wordpress/compose'
-import PropTypes from 'prop-types'
+import { Fragment } from '@wordpress/element'
+import { doAction } from '@wordpress/hooks'
 
-const panelStatus = {}
+const tabCache = {}
 
-const withMemory = createHigherOrderComponent(
-	WrappedComponent => class extends Component {
-		static propTypes = {
-			blockProps: PropTypes.shape( {
-				clientId: PropTypes.string.isRequired,
-				name: PropTypes.string.isRequired,
-				attributes: PropTypes.object.isRequired,
-			} ),
+const withMemory = WrappedComponent => {
+	const NewComp = props => {
+		const onClick = tab => {
+			// Cache it.
+			tabCache[ props.blockProps.clientId ] = tab
+
+			doAction( 'stackable.inspector.tab.click', props.blockProps.clientId, tab )
+
+			props.onClick( tab )
 		}
 
-		constructor() {
-			super( ...arguments )
+		const initialTab = props.initialTab || // If there's a prop, use it.
+			tabCache[ props.blockProps.clientId ] || // Or check if there's a previously selected tab.
+			( isUnmodifiedBlock( props.blockProps ) ? 'layout' : 'style' ) // Or if not, default to layout or the style tab.
 
-			// Create a cache for the status of this panel.
-			this.blockClientId = this.props.blockProps.clientId
-			if ( typeof panelStatus[ this.blockClientId ] === 'undefined' ) {
-				panelStatus[ this.blockClientId ] = {
-					// New/default blocks should start at the layout tab, others in the style tab.
-					tab: isUnmodifiedBlock( this.props.blockProps ) ? '' : 'style',
-					panel: 0,
-				}
-			}
+		doAction( 'stackable.inspector.tab.initial', props.blockProps.clientId, initialTab )
 
-			this.onClickPanel = this.onClickPanel.bind( this )
-			this.onClickTab = this.onClickTab.bind( this )
+		const propsToPass = {
+			...props,
+			initialTab,
+			onClick,
 		}
 
-		getActiveTab() {
-			const mainContainer = document.querySelector( '.components-panel[data-ugb-tab]' )
-			return mainContainer ? mainContainer.getAttribute( 'data-ugb-tab' ) : undefined
-		}
+		return (
+			<Fragment>
+				<WrappedComponent { ...propsToPass } />
+			</Fragment>
+		)
+	}
 
-		getActiveTabPanelContainer( tab = this.getActiveTab() ) {
-			return tab ? document.querySelector( `.ugb-panel-${ tab }` ) : undefined
-		}
+	NewComp.defaultProps = {
+		...( WrappedComponent.defaultProps || {} ),
+	}
 
-		getPanelFromIndex( panelIndex = -1 ) {
-			if ( panelIndex === -1 ) {
-				return null
-			}
-			const container = this.getActiveTabPanelContainer()
-			if ( container ) {
-				return container.querySelector( `.components-panel__body:nth-child(${ panelIndex + 1 }):not(.is-opened) .components-panel__body-toggle` )
-			}
-			return null
-		}
-
-		getPanelIndex( panel ) {
-			return panel ? Array.prototype.slice.call( panel.parentElement.children ).indexOf( panel ) : -1
-		}
-
-		getOpenPanelIndex( tab = this.getActiveTab() ) {
-			const container = this.getActiveTabPanelContainer( tab )
-			const openPanel = container.querySelector( '.components-panel__body.is-opened' )
-			return this.getPanelIndex( openPanel )
-		}
-
-		componentDidMount() {
-			// Or if no panel was open, make sure all panels are closed.
-			if ( panelStatus[ this.blockClientId ].panel === -1 ) {
-				closeAllOpenPanels()
-				return
-			}
-
-			// Open the previous panel.
-			const panel = this.getPanelFromIndex( panelStatus[ this.blockClientId ].panel )
-			if ( panel ) {
-				panel.click()
-			}
-		}
-
-		// Update the cache when closing/opening panels.
-		onClickPanel( panel ) {
-			panelStatus[ this.blockClientId ] = {
-				...panelStatus[ this.blockClientId ],
-				panel: ! panel.classList.contains( 'is-opened' ) ? this.getPanelIndex( panel ) : -1,
-			}
-		}
-
-		// Update the cache when changing tabs.
-		onClickTab( tab ) {
-			panelStatus[ this.blockClientId ] = {
-				tab,
-				panel: this.getOpenPanelIndex( tab ),
-			}
-		}
-
-		render() {
-			return (
-				<WrappedComponent
-					initialTab={ panelStatus[ this.blockClientId ].tab }
-					onClick={ this.onClickTab }
-					onClickPanel={ this.onClickPanel }
-					{ ...this.props }
-				/>
-			)
-		}
-	},
-	'withMemory'
-)
+	return NewComp
+}
 
 export default withMemory
