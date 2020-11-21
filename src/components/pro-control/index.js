16170698e1368f0efd1ea3ca156626d5a53770e7
Nico Mendoza <blankartwp@gmail.com>
2020-11-21 11:44:21 +0800

    Refactor

--- src/components/pro-control/index.js
+++ src/components/pro-control/index.js
@@ -7,11 +7,13 @@ import SVGProIcon from './images/pro-icon.svg'
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n' /**
-iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii * External dependencies
-iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii */
-import { i18n } from 'stackable'
+import { __ } from '@wordpress/i18n'
 import { Button } from '@wordpress/components'
+
+/**
+ * External dependencies
+ */
+import { i18n } from 'stackable'
 import classnames from 'classnames'
 
 const LABELS = {
