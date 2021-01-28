acc48edb851c19eaa8e67b9604cd7a7442f70f90
Benjamin Intal <bfintal@gmail.com>
2021-01-28 14:46:33 +0800

    Removed withHelpTip, not really used much

--- src/components/blend-mode-control/index.js
+++ src/components/blend-mode-control/index.js
@@ -3,18 +3,18 @@
  */
 import { __ } from '@wordpress/i18n'
 import { SelectControl } from '@wordpress/components'
-import { compose } from '@wordpress/compose'
 
 /**
  * External dependencies
  */
 import { i18n } from 'stackable'
-import { withHelpTip } from '~stackable/higher-order'
+import classnames from 'classnames'
 
 const BlendModeControl = props => {
 	return (
 		<SelectControl
 			{ ...props }
+			className={ classnames( [ 'ugb--help-tip-background-blend-mode', props.className ] ) }
 			options={ [
 				{ value: '', label: __( 'None', i18n ) },
 				{ value: 'normal', label: __( 'Normal', i18n ) },
@@ -42,10 +42,9 @@ const BlendModeControl = props => {
 }
 
 BlendModeControl.defaultProps = {
+	className: '',
 	label: __( 'Mix Blend Mode', i18n ),
 	help: __( 'Not supported in all browsers.', i18n ),
 }
 
-export default compose(
-	withHelpTip( 'background-blend-mode' )
-)( BlendModeControl )
+export default BlendModeControl
