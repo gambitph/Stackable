fe8dc25a086edc21c6aab71c46a0c6fb2628ceb6
Benjamin Intal <bfintal@gmail.com>
2021-02-21 11:48:57 +0800

    optimized code

--- src/hooks/use-block-context.js
+++ src/hooks/use-block-context.js
@@ -3,20 +3,19 @@ import {
 	first, last, indexOf, nth,
 } from 'lodash'
 
-const useBlockContext = props => {
+const useBlockContext = ( { clientId } ) => {
 	const blockInfo = useSelect(
 		select => {
 			const { getBlock, getBlockParents } = select( 'core/block-editor' )
-			const block = getBlock( props.clientId )
-			const parentClientId = last( getBlockParents( props.clientId ) )
+			const block = getBlock( clientId )
+			const parentClientId = last( getBlockParents( clientId ) )
 			const parent = parentClientId ? getBlock( parentClientId ) : null
-			const index = indexOf( parent?.innerBlocks, getBlock( props.clientId ) )
-			const isLastBlock = last( parent?.innerBlocks )?.clientId === props.clientId
-			// getAdjacentBlockClientId
+			const index = indexOf( parent?.innerBlocks, getBlock( clientId ) )
+			const isLastBlock = last( parent?.innerBlocks )?.clientId === clientId
 			return {
 				blockIndex: index,
 				parentBlock: parent,
-				isFirstBlock: first( parent?.innerBlocks )?.clientId === props.clientId,
+				isFirstBlock: first( parent?.innerBlocks )?.clientId === clientId,
 				isLastBlock,
 				isOnlyBlock: ! parent || parent?.innerBlocks?.length <= 1,
 				adjacentBlock: nth( parent?.innerBlocks, ! isLastBlock ? index + 1 : index - 1 ),
@@ -25,7 +24,7 @@ const useBlockContext = props => {
 				hasInnerBlocks: !! block?.innerBlocks?.length,
 			}
 		},
-		[ props.clientId ]
+		[ clientId ]
 	)
 
 	return blockInfo
