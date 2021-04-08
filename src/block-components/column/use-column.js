1f9df397b778001d9a17f2801d0313816263949e
Benjamin Intal <bfintal@gmail.com>
2021-04-08 18:51:44 +0800

    dragging columns with more than 3 columns can throw an error and won't stop the column from being dragged

--- src/block-components/column/use-column.js
+++ src/block-components/column/use-column.js
@@ -1,57 +1,64 @@
+import { last } from 'lodash'
+
 /**
  * WordPress Dependencies
  */
-import { useDispatch } from '@wordpress/data'
+import { useDispatch, select } from '@wordpress/data'
 import { useCallback } from '@wordpress/element'
 import { useBlockEditContext } from '@wordpress/block-editor'
-import { useBlockContext } from '~stackable/hooks'
 import classnames from 'classnames'
 
 export const useColumn = () => {
-	 const { clientId } = useBlockEditContext()
-	 const { updateBlockAttributes } = useDispatch( 'core/block-editor' )
+	const { clientId } = useBlockEditContext()
+	const { updateBlockAttributes } = useDispatch( 'core/block-editor' )
 
-	 const { adjacentBlocks } = useBlockContext()
+	const onChangeDesktop = useCallback(
+		widths => {
+			const parentClientId = last( select( 'core/block-editor' ).getBlockParents( clientId ) )
+			const adjacentBlocks = select( 'core/block-editor' ).getBlock( parentClientId )?.innerBlocks || []
 
-	 const onChangeDesktop = useCallback(
-		 widths => {
-			 widths.forEach( ( width, i ) => {
-				 // TODO: When Gutenberg 10.1 comes out, update this to just one updateBlockAttributes call for all client Ids
-				 updateBlockAttributes( adjacentBlocks[ i ].clientId, { columnWidth: width } )
-			 } )
-		 },
-		 [ clientId, updateBlockAttributes ]
-	 )
+			if ( adjacentBlocks.length ) {
+				widths.forEach( ( width, i ) => {
+				// TODO: When Gutenberg 10.1 comes out, update this to just one updateBlockAttributes call for all client Ids
+					updateBlockAttributes( adjacentBlocks[ i ].clientId, { columnWidth: width } )
+				} )
+			}
+		},
+		[ clientId, updateBlockAttributes ]
+	)
 
-	 const onChangeTablet = useCallback(
-		 width => {
-			 updateBlockAttributes( clientId, { columnWidthTablet: width } )
-		 },
-		 [ clientId, updateBlockAttributes ]
-	 )
+	const onChangeTablet = useCallback(
+		width => {
+			updateBlockAttributes( clientId, { columnWidthTablet: width } )
+		},
+		[ clientId, updateBlockAttributes ]
+	)
 
-	 const onChangeMobile = useCallback(
-		 width => {
-			 updateBlockAttributes( clientId, { columnWidthMobile: width } )
-		 },
-		 [ clientId, updateBlockAttributes ]
-	 )
+	const onChangeMobile = useCallback(
+		width => {
+			updateBlockAttributes( clientId, { columnWidthMobile: width } )
+		},
+		[ clientId, updateBlockAttributes ]
+	)
 
-	 const onResetDesktop = useCallback(
-		 () => {
-			 adjacentBlocks.forEach( ( { clientId } ) => {
-				 updateBlockAttributes( clientId, { columnWidth: '' } )
-			 } )
-		 },
-		 [ clientId, updateBlockAttributes ]
-	 )
+	const onResetDesktop = useCallback(
+		() => {
+			const parentClientId = last( select( 'core/block-editor' ).getBlockParents( clientId ) )
+			const adjacentBlocks = select( 'core/block-editor' ).getBlock( parentClientId )?.innerBlocks || []
 
-	 return {
+			adjacentBlocks.forEach( ( { clientId } ) => {
+				updateBlockAttributes( clientId, { columnWidth: '' } )
+			} )
+		},
+		[ clientId, updateBlockAttributes ]
+	)
+
+	return {
 		onChangeDesktop,
 		onChangeTablet,
 		onChangeMobile,
 		onResetDesktop,
-	 }
+	}
 }
 
 export const getColumnClasses = attributes => {
