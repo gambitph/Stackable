b9b2236507b2284a2909d9da533ba71d83884aae
Benjamin Intal <bfintal@gmail.com>
2021-04-08 17:35:46 +0800

    fix issue for multiple unique id attributes being changed in multiple blocks

--- src/block-components/block-div/use-unique-id.js
+++ src/block-components/block-div/use-unique-id.js
@@ -1,24 +1,27 @@
-import { useBlockAttributes } from '~stackable/hooks'
-
 import { useEffect } from '@wordpress/element'
+import { select } from '@wordpress/data'
 import { useBlockEditContext } from '@wordpress/block-editor'
 
 export const createUniqueClass = uid => `${ uid.substring( 0, 7 ) }`
 
 export const useUniqueId = () => {
 	const { clientId } = useBlockEditContext()
-	const attributes = useBlockAttributes( clientId )
 
 	useEffect( () => {
-		// When there's no unique ID yet, create one.
-		if ( ! attributes.uniqueId ) {
-			attributes.uniqueId = createUniqueClass( clientId )
-		// If there's one already, check whether the we need to re-create one.
-		// Duplicating a block or copy pasting a block may give us duplicate IDs.
-		} else if ( createUniqueClass( clientId ) !== attributes.uniqueId ) {
-			if ( document.querySelectorAll( `[data-id="${ attributes.uniqueId }"]` ).length > 1 ) {
+		// Need to do this in a delay so that this will not trigger in multiple blocks.
+		setTimeout( () => {
+			const attributes = select( 'core/block-editor' ).getBlockAttributes( clientId )
+
+			// When there's no unique ID yet, create one.
+			if ( ! attributes.uniqueId ) {
 				attributes.uniqueId = createUniqueClass( clientId )
+			// If there's one already, check whether the we need to re-create one.
+			// Duplicating a block or copy pasting a block may give us duplicate IDs.
+			} else if ( createUniqueClass( clientId ) !== attributes.uniqueId ) {
+				if ( document.querySelectorAll( `[data-id="${ attributes.uniqueId }"]` ).length > 1 ) {
+					attributes.uniqueId = createUniqueClass( clientId )
+				}
 			}
-		}
+		}, 0 )
 	}, [ clientId ] )
 }
