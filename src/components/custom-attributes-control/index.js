914b199d0c7b9e7c754392f5a9b6224af7b6da50
Nico Mendoza <blankartwp@gmail.com>
2021-06-11 16:35:45 +0800

    Fixes ##1225

--- src/components/custom-attributes-control/index.js
+++ src/components/custom-attributes-control/index.js
@@ -8,12 +8,13 @@ import { escape } from 'lodash'
  * WordPress dependencies
  */
 import {
-	Fragment, useState, useEffect, render, unmountComponentAtNode,
+	Fragment, useState, useEffect, render, unmountComponentAtNode, useRef,
 } from '@wordpress/element'
 import { __ } from '@wordpress/i18n'
 import { TextControl } from '@wordpress/components'
 
 const CustomAttributesControl = props => {
+	const inputRef = useRef()
 	const [ customAttributes, setCustomAttributes ] = useState(
 		Array.isArray( props.value ) ?
 			props.value.map( attribute => {
@@ -25,35 +26,23 @@ const CustomAttributesControl = props => {
 	)
 
 	useEffect( () => {
-		const timeout = setTimeout( () => {
-			const el = document.createElement( 'div' )
-			el.innerHTML = `<div ${ customAttributes } />`
-			// Get all the "fixed" attributes:
-			const attrNodeMap = el.children[ 0 ]?.attributes || []
-			const attributes = []
-			for ( let i = attrNodeMap.length - 1; i >= 0; i-- ) {
-				if ( ! props.invalidHtmlAttributes.includes( attrNodeMap[ i ].name ) ) {
-					try {
-						// Checks if the attribute key and value can be a valid react prop
-						// Throws an error if not valid.
-						const dummyNode = document.createElement( 'div' )
-						render( <div { ...{ [ attrNodeMap[ i ].name ]: attrNodeMap[ i ].value } } />, dummyNode )
-						unmountComponentAtNode( dummyNode )
-						document.createElement( 'div' ).setAttribute( attrNodeMap[ i ].name, attrNodeMap[ i ].value )
-
-						attributes.push( [ attrNodeMap[ i ].name, attrNodeMap[ i ].value ] )
-					} catch {}
-				}
-			}
-
-			props.onChange( attributes )
-		}, 300 )
-
-		return () => clearTimeout( timeout )
-	}, [ customAttributes ] )
+		// Only trigger when not focused.
+		if ( inputRef.current !== document.activeElement ) {
+			setCustomAttributes(
+				Array.isArray( props.value ) ?
+					props.value.map( attribute => {
+						const [ key, _value ] = attribute
+						const value = `"${ escape( _value ) }"`
+						return [ key, value ].join( '=' )
+					} ).join( ' ' ) :
+					''
+			)
+		}
+	}, [ JSON.stringify( props.value ) ] )
 
 	return (
 		<TextControl
+			ref={ inputRef }
 			data-testid="custom-attributes"
 			label={ __( 'Custom Attributes', i18n ) }
 			value={ customAttributes }
@@ -69,9 +58,32 @@ const CustomAttributesControl = props => {
 						''
 				)
 			} }
-			onChange={ setCustomAttributes }
+			onChange={ newCustomAttributes => {
+				setCustomAttributes( newCustomAttributes )
+				const el = document.createElement( 'div' )
+				el.innerHTML = `<div ${ newCustomAttributes } />`
+				// Get all the "fixed" attributes:
+				const attrNodeMap = el.children[ 0 ]?.attributes || []
+				const attributes = []
+				for ( let i = attrNodeMap.length - 1; i >= 0; i-- ) {
+					if ( ! props.invalidHtmlAttributes.includes( attrNodeMap[ i ].name ) ) {
+						try {
+						// Checks if the attribute key and value can be a valid react prop
+						// Throws an error if not valid.
+							const dummyNode = document.createElement( 'div' )
+							render( <div { ...{ [ attrNodeMap[ i ].name ]: attrNodeMap[ i ].value } } />, dummyNode )
+							unmountComponentAtNode( dummyNode )
+							document.createElement( 'div' ).setAttribute( attrNodeMap[ i ].name, attrNodeMap[ i ].value )
+
+							attributes.push( [ attrNodeMap[ i ].name, attrNodeMap[ i ].value ] )
+						} catch {}
+					}
+				}
+
+				props.onChange( attributes )
+			} }
 			help={ <Fragment>
-				{ __( 'You can type in custom HTML attributes for this block in the field above.', i18n ) }
+				{ __( 'You can type in custom HTML attributes for this block in the field above.', i18n ) } &nbsp;
 				<a href="https://docs.wpstackable.com/article/461-how-to-use-custom-attributes?utm_source=inspector&utm_campaign=learnmore&utm_medium=gutenberg" target="_docs">{ __( 'Learn more about Custom Attributes', i18n ) }</a>
 			</Fragment>
 			}
