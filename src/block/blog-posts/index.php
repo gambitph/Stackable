d2af8cd3070330b11915b9527939dd501faf611f
Benjamin Intal <bfintal@gmail.com>
2021-06-11 11:00:32 +0800

    Update src/block/blog-posts/index.php

--- src/block/blog-posts/index.php
+++ src/block/blog-posts/index.php
@@ -210,7 +210,7 @@ if ( ! function_exists( 'stackable_render_blog_posts_block' ) ) {
 						$post_id,
 						$attributes['imageSize'],
 						array(
-							'alt' => esc_attr( ( $post_meta !== false && $post_meta !== null ) ? $post_meta : '' ),
+							'alt' => esc_attr( ! empty( $post_meta ) ? $post_meta : '' ),
 							'width' => esc_attr( $featured_image_src[1] ),
 							'height' => esc_attr( $featured_image_src[2] ),
 						)
