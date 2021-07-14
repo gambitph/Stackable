602caac6a41d654514751f91d45e995e395bae83
blankart <blankartwp@gmail.com>
2021-07-14 22:25:44 +0800

    fix (wordpress 5.8): open and closing panels compatibility. Fixes #1319 (#1338)
    * Fixes #1319
    
    * Update src/components/panel-tabs/index.js
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

--- src/components/panel-tabs/index.js
+++ src/components/panel-tabs/index.js
@@ -39,7 +39,7 @@ const TABS = [
 ]
 
 export const closeAllOpenPanels = clickedEl => {
-	[].forEach.call( document.querySelectorAll( '.edit-post-sidebar,.edit-widgets-sidebar .components-panel__body .components-panel__body-toggle' ), el => {
+	[].forEach.call( document.querySelector( '.edit-post-sidebar, .edit-widgets-sidebar' )?.querySelectorAll( '.components-panel__body .components-panel__body-toggle' ) || [], el => {
 		if ( el.offsetHeight === 0 ) {
 			return
 		}
