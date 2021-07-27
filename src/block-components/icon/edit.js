2e74380e47eac229498983ef46a8eb38255373e1
Nico Mendoza <blankartwp@gmail.com>
2021-07-27 18:23:58 +0800

    changed block transition selector

--- src/block-components/icon/edit.js
+++ src/block-components/icon/edit.js
@@ -162,156 +162,160 @@ export const Edit = props => {
 					/>
 				) }
 
-				{ hasShape && (
-					<Fragment>
-						<ToggleControl
-							label={ __( 'Shaped', i18n ) }
-							checked={ getAttribute( 'shaped' ) }
-							onChange={ updateAttributeHandler( 'shaped' ) }
-						/>
-
-						{ getAttribute( 'shaped' ) && (
-							<Fragment>
-
-								<ColorPaletteControl
-									label={ __( 'Shape Color', i18n ) }
-									attribute="shapeColor"
-									hover="all"
-									hasTransparent={ true }
-								/>
-
-								<AdvancedRangeControl
-									label={ __( 'Shape Border Radius', i18n ) }
-									attribute="shapeBorderRadius"
-									hover="all"
-									min={ 0 }
-									max={ 100 }
-									step={ 1 }
-									allowReset={ true }
-									placeholder={ 50 }
-								/>
-
-								<AdvancedRangeControl
-									label={ __( 'Shape Padding', i18n ) }
-									attribute="shapePadding"
-									hover="all"
-									sliderMin={ 0 }
-									sliderMax={ 150 }
-									step={ 1 }
-									allowReset={ true }
-									placeholder={ 20 }
-								/>
-
-								<ToggleControl
-									label={ __( 'Shape Outline', i18n ) }
-									checked={ getAttribute( 'shapeOutline' ) }
-									onChange={ updateAttributeHandler( 'shapeOutline' ) }
-								/>
-
-								{ getAttribute( 'shapeOutline' ) && (
-									<Fragment>
-
-										<ColorPaletteControl
-											label={ __( 'Shape Outline Color', i18n ) }
-											attribute="shapeOutlineColor"
-											hover="all"
-										/>
-
-										<FourRangeControl
-											label={ __( 'Shape Outline Width' ) }
-											units={ [ 'px' ] }
-											min={ 0 }
-											max={ 99 }
-											step={ 1 }
-											sliderMax={ 20 }
-											defaultLocked={ true }
-											attribute="shapeOutlineWidth"
-											responsive="all"
-											hover="all"
-											default="1"
-										/>
-
-									</Fragment>
-								) }
-
-							</Fragment>
-						) }
-					</Fragment>
-				) }
-
-				{ hasBackgroundShape && (
-					<Fragment>
-						<ToggleControl
-							label={ __( 'Background Shape', i18n ) }
-							checked={ getAttribute( 'showBackgroundShape' ) }
-							onChange={ updateAttributeHandler( 'showBackgroundShape' ) }
-						/>
-
-						{ getAttribute( 'showBackgroundShape' ) && (
-							<Fragment>
-
-								<ImageShapeControl
-									label={ __( 'Shape', i18n ) }
-									selected={ getAttribute( 'backgroundShape' ) }
-									onChange={ updateAttributeHandler( 'backgroundShape' ) }
-								/>
-
-								<ColorPaletteControl
-									label={ __( 'Shape Color', i18n ) }
-									attribute="backgroundShapeColor"
-									hover="all"
-								/>
-
-								<AdvancedRangeControl
-									label={ __( 'Shape Opacity', i18n ) }
-									attribute="backgroundShapeOpacity"
-									hover="all"
-									min={ 0 }
-									max={ 1 }
-									step={ 0.1 }
-									placeholder="1"
-									allowReset={ true }
-								/>
-
-								<AdvancedRangeControl
-									label={ __( 'Shape Size', i18n ) }
-									attribute="backgroundShapeSize"
-									hover="all"
-									min={ 0 }
-									max={ 3 }
-									step={ 0.1 }
-									placeholder="1"
-									allowReset={ true }
-								/>
-
-								<AdvancedRangeControl
-									label={ __( 'Horizontal Offset', i18n ) }
-									attribute="backgroundShapeOffsetHorizontal"
-									hover="all"
-									min={ -30 }
-									max={ 30 }
-									step={ 1 }
-									placeholder="0"
-									allowReset={ true }
-								/>
-
-								<AdvancedRangeControl
-									label={ __( 'Vertical Offset', i18n ) }
-									attribute="backgroundShapeOffsetVertical"
-									hover="all"
-									min={ -30 }
-									max={ 30 }
-									step={ 1 }
-									placeholder="0"
-									allowReset={ true }
-								/>
-
-							</Fragment>
-						) }
-					</Fragment>
-				) }
-
 			</PanelAdvancedSettings>
+			{ hasShape && (
+				<PanelAdvancedSettings
+					title={ __( 'Icon Shape', i18n ) }
+					id="icon-shape"
+				>
+					<ToggleControl
+						label={ __( 'Shaped', i18n ) }
+						checked={ getAttribute( 'shaped' ) }
+						onChange={ updateAttributeHandler( 'shaped' ) }
+					/>
+
+					{ getAttribute( 'shaped' ) && (
+						<Fragment>
+
+							<ColorPaletteControl
+								label={ __( 'Shape Color', i18n ) }
+								attribute="shapeColor"
+								hover="all"
+								hasTransparent={ true }
+							/>
+
+							<AdvancedRangeControl
+								label={ __( 'Shape Border Radius', i18n ) }
+								attribute="shapeBorderRadius"
+								hover="all"
+								min={ 0 }
+								max={ 100 }
+								step={ 1 }
+								allowReset={ true }
+								placeholder={ 50 }
+							/>
+
+							<AdvancedRangeControl
+								label={ __( 'Shape Padding', i18n ) }
+								attribute="shapePadding"
+								hover="all"
+								sliderMin={ 0 }
+								sliderMax={ 150 }
+								step={ 1 }
+								allowReset={ true }
+								placeholder={ 20 }
+							/>
+
+							<ToggleControl
+								label={ __( 'Shape Outline', i18n ) }
+								checked={ getAttribute( 'shapeOutline' ) }
+								onChange={ updateAttributeHandler( 'shapeOutline' ) }
+							/>
+
+							{ getAttribute( 'shapeOutline' ) && (
+								<Fragment>
+
+									<ColorPaletteControl
+										label={ __( 'Shape Outline Color', i18n ) }
+										attribute="shapeOutlineColor"
+										hover="all"
+									/>
+
+									<FourRangeControl
+										label={ __( 'Shape Outline Width' ) }
+										units={ [ 'px' ] }
+										min={ 0 }
+										max={ 99 }
+										step={ 1 }
+										sliderMax={ 20 }
+										defaultLocked={ true }
+										attribute="shapeOutlineWidth"
+										responsive="all"
+										hover="all"
+										default="1"
+									/>
+
+								</Fragment>
+							) }
+
+						</Fragment>
+					) }
+				</PanelAdvancedSettings>
+			) }
+			{ hasBackgroundShape && (
+				<PanelAdvancedSettings
+					title={ __( 'Icon Background Shape', i18n ) }
+					id="icon-background-shape"
+				>
+					<ToggleControl
+						label={ __( 'Background Shape', i18n ) }
+						checked={ getAttribute( 'showBackgroundShape' ) }
+						onChange={ updateAttributeHandler( 'showBackgroundShape' ) }
+					/>
+
+					{ getAttribute( 'showBackgroundShape' ) && (
+						<Fragment>
+
+							<ImageShapeControl
+								label={ __( 'Shape', i18n ) }
+								selected={ getAttribute( 'backgroundShape' ) }
+								onChange={ updateAttributeHandler( 'backgroundShape' ) }
+							/>
+
+							<ColorPaletteControl
+								label={ __( 'Shape Color', i18n ) }
+								attribute="backgroundShapeColor"
+								hover="all"
+							/>
+
+							<AdvancedRangeControl
+								label={ __( 'Shape Opacity', i18n ) }
+								attribute="backgroundShapeOpacity"
+								hover="all"
+								min={ 0 }
+								max={ 1 }
+								step={ 0.1 }
+								placeholder="1"
+								allowReset={ true }
+							/>
+
+							<AdvancedRangeControl
+								label={ __( 'Shape Size', i18n ) }
+								attribute="backgroundShapeSize"
+								hover="all"
+								min={ 0 }
+								max={ 3 }
+								step={ 0.1 }
+								placeholder="1"
+								allowReset={ true }
+							/>
+
+							<AdvancedRangeControl
+								label={ __( 'Horizontal Offset', i18n ) }
+								attribute="backgroundShapeOffsetHorizontal"
+								hover="all"
+								min={ -30 }
+								max={ 30 }
+								step={ 1 }
+								placeholder="0"
+								allowReset={ true }
+							/>
+
+							<AdvancedRangeControl
+								label={ __( 'Vertical Offset', i18n ) }
+								attribute="backgroundShapeOffsetVertical"
+								hover="all"
+								min={ -30 }
+								max={ 30 }
+								step={ 1 }
+								placeholder="0"
+								allowReset={ true }
+							/>
+
+						</Fragment>
+					) }
+				</PanelAdvancedSettings>
+			) }
 		</InspectorStyleControls>
 	)
 }
