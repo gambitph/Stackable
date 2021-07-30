55382247da43d67780ed7dba4bb52e4423f9620a
Nico Mendoza <blankartwp@gmail.com>
2021-07-30 18:12:34 +0800

    [V3] Add stackable/ blocks support for get_the_excerpt API

--- src/blocks.php
+++ src/blocks.php
@@ -21,6 +21,7 @@ if ( ! function_exists( 'stackable_register_blocks' ) ) {
 			return;
 		}
 
+		// TODO: Include all blocks.
 		$block_folders = array(
 			'button',
 			'button-group',
@@ -59,4 +60,86 @@ if ( ! function_exists( 'stackable_register_blocks' ) ) {
 		}
 	}
 	add_action( 'init', 'stackable_register_blocks' );
+}
+
+/**
+ * Allow our blocks to display post excerpts
+ * when calling `get_the_excerpt` function.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/excerpt_allowed_blocks/
+ */
+if ( ! function_exists( 'stackable_add_excerpt_wrapper_blocks' ) ) {
+	/**
+	 * Register stackable blocks with inner blocks.
+	 */
+	function stackable_add_excerpt_wrapper_blocks( $allowed_wrapper_blocks ) {
+		$allowed_stackable_wrapper_blocks = [
+			// ugb blocks.
+			'ugb/accordion',
+			'ugb/column',
+			'ugb/columns',
+			'ugb/container',
+
+			// stackable blocks.
+			// TODO: add more blocks
+			'stackable/button-group',
+			'stackable/card-group',
+			'stackable/card',
+			'stackable/column',
+			'stackable/columns',
+			'stackable/container',
+			'stackable/feature',
+			'stackable/number-box'
+		];
+
+		return array_merge( $allowed_stackable_wrapper_blocks, $allowed_wrapper_blocks );
+	}
+
+	add_filter( 'excerpt_allowed_wrapper_blocks', 'stackable_add_excerpt_wrapper_blocks' );
+}
+
+if ( ! function_exists( 'stackable_add_excerpt_blocks' ) ) {
+	/**
+	 * Register "unit" stackable blocks (blocks without inner blocks)
+	 */
+	function stackable_add_excerpt_blocks( $allowed_blocks ) {
+		$allowed_stackable_blocks = [
+			// ugb blocks.
+			'ugb/blockquote',
+			'ugb/blog-posts',
+			'ugb/button',
+			'ugb/cta',
+			'ugb/card',
+			'ugb/count-up',
+			'ugb/expand',
+			'ugb/feature-grid',
+			'ugb/feature',
+			'ugb/header',
+			'ugb/heading',
+			'ugb/icon-list',
+			'ugb/icon',
+			'ugb/image-box',
+			'ugb/notification',
+			'ugb/number-box',
+			'ugb/pricing-box',
+			'ugb/team-member',
+			'ugb/testimonial',
+			'ugb/text',
+			'ugb/video-popup',
+
+			// stackable blocks.
+			// TODO: add more blocks
+			'stackable/button',
+			'stackable/heading',
+			'stackable/icon',
+			'stackable/icon-label',
+			'stackable/icon-list',
+			'stackable/image',
+			'stackable/text'
+		];
+
+		return array_merge( $allowed_stackable_blocks, $allowed_blocks );
+	}
+
+	add_filter( 'excerpt_allowed_blocks', 'stackable_add_excerpt_blocks' );
 }
