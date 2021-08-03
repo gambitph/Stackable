f7765df3280390d0566177d9d8af20fd955f394e
Nico Mendoza <blankartwp@gmail.com>
2021-08-03 11:34:21 +0800

    feat: spacer block

---
+++ src/block/spacer/edit.js
@@ -0,0 +1,99 @@
+/**
+ * Internal dependencies
+ */
+import { SpacerStyles } from './style'
+
+/**
+ * External dependencies
+ */
+import classnames from 'classnames'
+import { i18n, version as VERSION } from 'stackable'
+import {
+	Advanced,
+	BlockDiv,
+	CustomCSS,
+	Responsive,
+	CustomAttributes,
+	EffectsAnimations,
+	ConditionalDisplay,
+} from '~stackable/block-components'
+import {
+	useBlockHoverClass, useDeviceType,
+} from '~stackable/hooks'
+import {
+	InspectorTabs, InspectorStyleControls, PanelAdvancedSettings, AdvancedRangeControl,
+} from '~stackable/components'
+import { getAttributeName } from '~stackable/util'
+
+/**
+ * WordPress dependencies
+ */
+import { ResizableBox } from '@wordpress/components'
+import { __ } from '@wordpress/i18n'
+
+const Edit = props => {
+	const {
+		className,
+		attributes,
+		setAttributes,
+	} = props
+
+	const deviceType = useDeviceType()
+	const blockHoverClass = useBlockHoverClass()
+	const blockClassNames = classnames( [
+		className,
+		'stk-block-spacer',
+		blockHoverClass,
+	] )
+
+	const heightAttrName = getAttributeName( 'height', deviceType )
+
+	return (
+		<>
+			<InspectorTabs />
+			<BlockDiv.InspectorControls />
+
+			<InspectorStyleControls>
+				<PanelAdvancedSettings
+					title={ __( 'General', i18n ) }
+					id="general"
+					initialOpen={ true }
+				>
+					<AdvancedRangeControl
+						label={ __( 'Height', i18n ) }
+						responsive="all"
+						attribute="height"
+						sliderMin="0"
+						sliderMax="500"
+					/>
+				</PanelAdvancedSettings>
+			</InspectorStyleControls>
+			<Advanced.InspectorControls />
+			<EffectsAnimations.InspectorControls />
+			<CustomAttributes.InspectorControls />
+			<CustomCSS.InspectorControls mainBlockClass="stk-block-spacer" />
+			<Responsive.InspectorControls />
+			<ConditionalDisplay.InspectorControls />
+
+			<SpacerStyles version={ VERSION } />
+			<CustomCSS mainBlockClass="stk-block-spacer" />
+			<BlockDiv className={ blockClassNames }>
+				<ResizableBox
+					size={ {
+						height: attributes[ heightAttrName ] === '' ? undefined : attributes[ heightAttrName ],
+					} }
+					enable={ { bottom: true } }
+					onResizeStop={ ( event, direction, elt, delta ) => {
+						let height = attributes[ heightAttrName ]
+						if ( height === '' ) {
+							height = 0
+						}
+						setAttributes( { [ heightAttrName ]: height + delta.height } )
+					} }
+				/>
+			</BlockDiv>
+		</>
+	)
+}
+
+export default Edit
---
+++ src/block/spacer/style.js
@@ -0,0 +1,93 @@
+/**
+ * External dependencies
+ */
+import {
+	Advanced,
+	BlockDiv,
+	EffectsAnimations,
+} from '~stackable/block-components'
+import {
+	getUniqueBlockClass, useStyles, getStyles,
+} from '~stackable/util'
+import { useDeviceType, useBlockAttributes } from '~stackable/hooks'
+import { Style as StyleComponent } from '~stackable/components'
+
+const getStyleParams = () => {
+	return [
+		{
+			renderIn: 'save',
+			selector: '',
+			styleRule: 'height',
+			attrName: 'height',
+			format: '%spx',
+		},
+	]
+}
+
+/**
+ * WordPress dependencies
+ */
+import { renderToString } from '@wordpress/element'
+import { useBlockEditContext } from '@wordpress/block-editor'
+
+export const SpacerStyles = props => {
+	const {
+		...propsToPass
+	} = props
+
+	const deviceType = useDeviceType()
+	const { clientId } = useBlockEditContext()
+	const attributes = useBlockAttributes( clientId )
+
+	const spacerStyles = useStyles( propsToPass.attributes, getStyleParams() )
+
+	propsToPass.blockUniqueClassName = getUniqueBlockClass( attributes.uniqueId )
+	propsToPass.deviceType = deviceType
+	propsToPass.attributes = { ...attributes, clientId }
+
+	return (
+		<>
+			<BlockDiv.Style { ...propsToPass } />
+			<Advanced.Style { ...propsToPass } />
+			<EffectsAnimations.Style { ...propsToPass } />
+			<StyleComponent
+				styles={ spacerStyles }
+				versionAdded="3.0.0"
+				versionDeprecated=""
+				{ ...propsToPass }
+			/>
+		</>
+	)
+}
+
+SpacerStyles.defaultProps = {
+	isEditor: false,
+	attributes: {},
+	options: {},
+}
+
+SpacerStyles.Content = props => {
+	const {
+		...propsToPass
+	} = props
+
+	propsToPass.blockUniqueClassName = getUniqueBlockClass( propsToPass.attributes.uniqueId )
+	const spacerStyles = getStyles( propsToPass.attributes, getStyleParams() )
+
+	const styles = (
+		<>
+			<BlockDiv.Style.Content { ...propsToPass } />
+			<Advanced.Style.Content { ...propsToPass } />
+			<EffectsAnimations.Style.Content { ...propsToPass } />
+			<StyleComponent.Content
+				styles={ spacerStyles }
+				versionAdded="3.0.0"
+				versionDeprecated=""
+				{ ...propsToPass }
+			/>
+		</>
+	)
+
+	return renderToString( styles ) ? <style>{ styles }</style> : null
+}
+
