300f669e1bc79ac44c5d7502770cac00095e09a9
blankart <blankartwp@gmail.com>
2021-08-04 09:34:46 +0800

    Add `stackable/` blocks support for `get_the_excerpt` API (#1414)
    * [V3] Add stackable/ blocks support for get_the_excerpt API
    
    * separated deprecated v2 blocks
    
    * Refactor
    
    * dry code
    
    * added caching to prevent multiple file reads
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

--- src/blocks.php
+++ src/blocks.php
@@ -13,33 +13,55 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! function_exists( 'stackable_register_blocks' ) ) {
-	function stackable_register_blocks() {
-		// Blocks directory may not exist if working from a fresh clone.
-		$blocks_dir = dirname( __FILE__ ) . '/block';
-		if ( ! file_exists( $blocks_dir ) ) {
-			return;
+/**
+ * folders containing stackable blocks without inner blocks.
+ */
+$stk_block_folders = array(
+	'button',
+	'count-up',
+	'heading',
+	'icon',
+	'icon-list',
+	'image',
+	'text'
+);
+
+/**
+ * folders containing stackable blocks with inner blocks.
+ */
+$stk_wrapper_block_folders = array(
+	'button-group',
+	'card',
+	'call-to-action',
+	'column',
+	'columns',
+	'container',
+	'feature',
+	'feature-grid',
+	'header',
+	'icon-label',
+);
+
+if ( ! function_exists( 'stackable_get_metadata_by_folders' ) ) {
+	/**
+	 * Function for getting the block.json metadata
+	 * based on folder names array.
+	 *
+	 * @array array folders
+	 * @return array metadata
+	 */
+	function stackable_get_metadata_by_folders( $block_folders ) {
+		// Check if the metadata have been parsed already.
+		global $stk_block_meta_data_cache;
+		if ( ! empty( $stk_block_meta_data_cache ) ) {
+			return $stk_block_meta_data_cache;
 		}
 
-		$block_folders = array(
-			'button',
-			'button-group',
-			'card',
-			'call-to-action',
-			'column',
-			'columns',
-			'count-up',
-			'container',
-			'feature',
-			'feature-grid',
-			'header',
-			'heading',
-			'icon',
-			'icon-label',
-			'icon-list',
-			'image',
-			'text',
-		);
+		$blocks = array();
+		$blocks_dir = dirname( __FILE__ ) . '/block';
+		if ( ! file_exists( $blocks_dir ) ) {
+			return $blocks;
+		}
 
 		foreach ( $block_folders as $folder_name ) {
 			$block_json_file = $blocks_dir . '/' . $folder_name . '/block.json';
@@ -48,7 +70,29 @@ if ( ! function_exists( 'stackable_register_blocks' ) ) {
 			}
 
 			$metadata = json_decode( file_get_contents( $block_json_file ), true );
+			array_push( $blocks, array_merge( $metadata, array( 'block_json_file' => $block_json_file ) ) );
+		}
 
+		$stk_block_meta_data_cache = $blocks; // Cache.
+		return $blocks;
+	}
+}
+
+if ( ! function_exists( 'stackable_register_blocks' ) ) {
+	function stackable_register_blocks() {
+		global $stk_block_folders, $stk_wrapper_block_folders;
+		// Blocks directory may not exist if working from a fresh clone.
+		$blocks_dir = dirname( __FILE__ ) . '/block';
+		if ( ! file_exists( $blocks_dir ) ) {
+			return;
+		}
+
+		$blocks_metadata = stackable_get_metadata_by_folders( array_merge(
+			$stk_block_folders,
+			$stk_wrapper_block_folders
+		) );
+
+		foreach ( $blocks_metadata as $metadata ) {
 			$registry = WP_Block_Type_Registry::get_instance();
 			if ( $registry->is_registered( $metadata['name'] ) ) {
 				$registry->unregister( $metadata['name'] );
@@ -66,8 +110,62 @@ if ( ! function_exists( 'stackable_register_blocks' ) ) {
 				$metadata
 			);
 
-			register_block_type_from_metadata( $block_json_file, $register_options );
+			register_block_type_from_metadata( $metadata['block_json_file'], $register_options );
 		}
 	}
 	add_action( 'init', 'stackable_register_blocks' );
+}
+
+/**
+ * Allow our blocks to display post excerpts
+ * when calling `get_the_excerpt` function.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/excerpt_allowed_blocks/
+ */
+if ( ! function_exists( 'stackable_add_excerpt_wrapper_blocks' ) ) {
+	/**
+	 * Register stackable blocks with inner blocks.
+	 */
+	function stackable_add_excerpt_wrapper_blocks( $allowed_wrapper_blocks ) {
+		$blocks_dir = dirname( __FILE__ ) . '/block';
+		if ( ! file_exists( $blocks_dir ) ) {
+			return $allowed_wrapper_blocks;
+		}
+
+		global $stk_wrapper_block_folders;
+		$allowed_stackable_wrapper_blocks = array();
+		$blocks_metadata = stackable_get_metadata_by_folders( $stk_wrapper_block_folders );
+
+		foreach ( $blocks_metadata as $metadata ) {
+			array_push( $allowed_stackable_wrapper_blocks, $metadata['name'] );
+		}
+
+		return array_merge( $allowed_stackable_wrapper_blocks, $allowed_wrapper_blocks );
+	}
+
+	add_filter( 'excerpt_allowed_wrapper_blocks', 'stackable_add_excerpt_wrapper_blocks' );
+}
+
+if ( ! function_exists( 'stackable_add_excerpt_blocks' ) ) {
+	/**
+	 * Register "unit" stackable blocks (blocks without inner blocks)
+	 */
+	function stackable_add_excerpt_blocks( $allowed_blocks ) {
+		$blocks_dir = dirname( __FILE__ ) . '/block';
+		if ( ! file_exists( $blocks_dir ) ) {
+			return $allowed_blocks;
+		}
+
+		global $stk_block_folders;
+		$allowed_stackable_blocks = array();
+		$blocks_metadata = stackable_get_metadata_by_folders( $stk_block_folders );
+
+		foreach ( $blocks_metadata as $metadata ) {
+			array_push( $allowed_stackable_blocks, $metadata['name'] );
+		}
+
+		return array_merge( $allowed_stackable_blocks, $allowed_blocks );
+	}
+
+	add_filter( 'excerpt_allowed_blocks', 'stackable_add_excerpt_blocks' );
 }
