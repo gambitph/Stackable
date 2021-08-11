7e2c2ca07424650f9016081c830deffdea0636ac
Benjamin Intal <bfintal@gmail.com>
2021-08-11 14:34:53 +0800

    corrected class names

--- src/block/call-to-action/style.js
+++ src/block/call-to-action/style.js
@@ -25,7 +25,7 @@ import {
 import { useBlockEditContext } from '@wordpress/block-editor'
 
 const containerDivOptions = {
-	sizeSelector: '.stk-block-header__content',
+	sizeSelector: '.stk-block-call-to-action__content',
 	sizeVerticalAlignRule: 'justifyContent',
 	sizeHorizontalAlignRule: 'alignSelf',
 }
