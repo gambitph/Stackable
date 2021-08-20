72e14abe17a9e6e53aeeade75bc24ea8fe099956
Benjamin Intal <bfintal@gmail.com>
2021-08-20 21:09:54 +0800

    added testimonial block

---
+++ src/block/testimonial/save.js
@@ -0,0 +1,69 @@
+/**
+ * Internal dependencies
+ */
+import { ContainerStyles } from './style'
+
+/**
+ * External dependencies
+ */
+import { version as VERSION } from 'stackable'
+import { withVersion } from '~stackable/higher-order'
+import classnames from 'classnames'
+import {
+	BlockDiv,
+	ContainerDiv,
+	BlockLink,
+	getAlignmentClasses,
+	CustomCSS,
+	getResponsiveClasses,
+} from '~stackable/block-components'
+
+/**
+ * WordPress dependencies
+ */
+import { InnerBlocks } from '@wordpress/block-editor'
+import { compose } from '@wordpress/compose'
+
+export const Save = props => {
+	const {
+		attributes,
+		className,
+	} = props
+
+	const blockAlignmentClass = getAlignmentClasses( attributes )
+	const responsiveClass = getResponsiveClasses( attributes )
+
+	const blockClassNames = classnames( [
+		className,
+		'stk-block-testimonial',
+		responsiveClass,
+	] )
+
+	const contentClassNames = classnames( [
+		'stk-block-content',
+		'stk-inner-blocks',
+		blockAlignmentClass,
+		'stk-block-testimonial__content',
+	] )
+
+	return (
+		<BlockDiv.Content
+			className={ blockClassNames }
+			attributes={ attributes }
+		>
+			<ContainerStyles.Content version={ props.version } attributes={ attributes } />
+			<CustomCSS.Content attributes={ attributes } />
+			<ContainerDiv.Content
+				className={ contentClassNames }
+				attributes={ attributes }
+			>
+				<InnerBlocks.Content />
+				<BlockLink.Content attributes={ attributes } />
+			</ContainerDiv.Content>
+		</BlockDiv.Content>
+	)
+}
+
+export default compose(
+	withVersion( VERSION )
+)( Save )
