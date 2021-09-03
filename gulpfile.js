c2c25e3ab31b1fbeb0ab76bff74c1ae36c894aaf
Benjamin Intal <bfintal@gmail.com>
2021-09-03 17:43:27 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -134,6 +134,7 @@ gulp.task( 'style-deprecated-v2', function() {
 	return gulp.src( [ path.resolve( __dirname, './src/deprecated/v2/common.scss' ), path.resolve( __dirname, './src/deprecated/v2/**/style.scss' ) ] )
 		.pipe( sass( deprecatedV2SassOptions ).on( 'error', sass.logError ) )
 		.pipe( concat( 'frontend_blocks_deprecated_v2.css' ) )
+		// @see https://make.wordpress.org/core/2020/08/04/new-editor-preview-options/
 		.pipe( header( '#start-resizable-editor-section{display:none}' ) )
 		.pipe( postcss( postCSSOptions ) )
 		.pipe( footer( '#end-resizable-editor-section{display:none}' ) )
