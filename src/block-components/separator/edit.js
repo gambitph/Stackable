5b97281763d1be53d23e46f58e39612cb331dd46
Nico Mendoza <blankartwp@gmail.com>
2021-09-06 18:41:01 +0800

    refactor

--- src/block-components/separator/edit.js
+++ src/block-components/separator/edit.js
@@ -8,7 +8,7 @@ import {
 	ColorPaletteControl,
 	AdvancedToggleControl,
 	ProControlButton,
-	SeparatorShadowControl,
+	ShadowControl,
 } from '~stackable/components'
 import { i18n, showProNotice } from 'stackable'
 import {
@@ -25,6 +25,32 @@ import SeparatorControl from './separator-control'
  */
 import { __ } from '@wordpress/i18n'
 import { applyFilters } from '@wordpress/hooks'
+import { useMemo } from '@wordpress/element'
+
+const getShadows = () => {
+	return applyFilters( 'stackable.separator.shadows', [
+		'none',
+		'0px 0 1px rgba(120, 120, 120, 0.1)',
+		'0px 0 2px rgba(120, 120, 120, 0.1)',
+		'5px 5px 0 rgba(18, 63, 82, 0.035), 0 0 0 1px rgba(176, 181, 193, 0.2)',
+		'0px 2px 20px rgba(153, 153, 153, 0.2)',
+		'5px 30px -10px rgba(18, 63, 82, 0.3)',
+		'0px 10px 30px rgba(0, 0, 0, 0.05)',
+		'7px 5px 30px rgba(72, 73, 121, 0.15)',
+		'0px 10px 60px rgba(0, 0, 0, 0.1)',
+		'70px 130px -60px rgba(72, 73, 121, 0.38) ',
+	] )
+}
+
+const valueCallback = value => {
+	const shadows = getShadows()
+	return value ? shadows.indexOf( value ) : ''
+}
+
+const changeCallback = index => {
+	const shadows = getShadows()
+	return index !== '' ? shadows[ index ] : index
+}
 
 const SeparatorControls = props => {
 	const {
@@ -35,6 +61,8 @@ const SeparatorControls = props => {
 	const {
 		getAttrName,
 	} = useAttributeEditHandlers( attrNameTemplate )
+
+	const shadows = useMemo( () => getShadows(), [] )
 
 	return (
 		<>
@@ -61,9 +89,12 @@ const SeparatorControls = props => {
 				sliderMax={ 4 }
 				step={ 0.1 }
 			/>
-			<SeparatorShadowControl
+			<ShadowControl
 				label={ __( 'Shadow / Outline', i18n ) }
 				attribute={ getAttrName( 'separatorShadow' ) }
+				valueCallback={ valueCallback }
+				changeCallback={ changeCallback }
+				max={ shadows.length - 1 }
 			/>
 			<AdvancedToggleControl
 				label={ __( 'Invert Design', i18n ) }
--- src/components/index.js
+++ src/components/index.js
@@ -96,7 +96,6 @@ export { default as AdvancedFocalPointControl } from './advanced-focal-point-con
 export { default as ImageFilterControl } from './image-filter-control'
 export { default as GroupPlaceholder } from './group-placeholder'
 export { default as ShadowControl } from './shadow-control'
-export { SeparatorShadowControl } from './shadow-control/separator-shadow-control'
 export { default as LinkControl } from './link-control'
 export { default as ColumnInserter } from './column-inserter'
 export { default as Style } from './style'
