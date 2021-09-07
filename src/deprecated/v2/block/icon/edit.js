64501bbfd8f8f049c32b20ed5964fc06f8fd7971
Nico Mendoza <blankartwp@gmail.com>
2021-09-07 17:14:45 +0800

    Fix: Reset the popover selected state in icon block

--- src/deprecated/v2/block/icon/edit.js
+++ src/deprecated/v2/block/icon/edit.js
@@ -264,6 +264,11 @@ const Edit = props => {
 			clearInterval( refreshPositionInterval )
 			setRefreshPositionInterval( null )
 		}
+
+		if ( ! isSelected ) {
+			setSelected( false )
+		}
+
 		return () => {
 			if ( refreshPositionInterval ) {
 				clearInterval( refreshPositionInterval )
