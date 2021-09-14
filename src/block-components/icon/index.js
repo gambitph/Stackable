245d295f8fc2ff973d84bdc8e1903656eb20dad9
blankart <blankartwp@gmail.com>
2021-09-15 00:55:32 +0800

    Fix: Icon should render if icon2 has a value (#1563)

--- src/block-components/icon/index.js
+++ src/block-components/icon/index.js
@@ -227,18 +227,20 @@ Icon.Content = props => {
 		{ 'stk--has-icon2': getValue( 'icon2' ) }
 	)
 
-	if ( ! getValue( 'icon' ) ) {
+	if ( ! getValue( 'icon' ) && ! getValue( 'icon2' ) ) {
 		return null
 	}
 
 	return (
 		<span className={ className }>
-			<SVGIcon.Content
-				className="stk--inner-svg"
-				prependRender={ linearGradient }
-				value={ getValue( 'icon' ) }
-				ariaLabel={ getValue( 'ariaLabel' ) }
-			/>
+			{ getValue( 'icon' ) && (
+				<SVGIcon.Content
+					className="stk--inner-svg"
+					prependRender={ linearGradient }
+					value={ getValue( 'icon' ) }
+					ariaLabel={ getValue( 'ariaLabel' ) }
+				/>
+			) }
 			{ getValue( 'showBackgroundShape' ) && (
 				<ShapeComp className="stk--shape-icon" />
 			) }
