6adc0baf486e277d87d5f0e43c126bce3a34ce31
Nico Mendoza <blankartwp@gmail.com>
2021-09-21 17:18:48 +0800

    fix

--- src/block/call-to-action/variations.js
+++ src/block/call-to-action/variations.js
@@ -7,129 +7,50 @@ import { i18n } from 'stackable'
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n'
+import { applyFilters } from '@wordpress/hooks'
 
 /**
  * Template option choices for predefined columns layouts.
  *
  * @type {WPBlockVariation[]}
  */
-const variations = [
-	{
-		name: 'default',
-		title: __( 'Default', i18n ),
-		description: __( 'Default Layout', i18n ),
-		isDefault: true,
-		attributes: {
-			hasContainer: true,
-		},
-		innerBlocks: [
-			[ 'stackable/heading', { text: __( 'Call to Action' ), textTag: 'h3' } ],
-			[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block.' } ],
-			[ 'stackable/button-group', {}, [
-				[ 'stackable/button', { text: 'Button' } ],
-			] ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'plain',
-		title: __( 'Plain', i18n ),
-		description: __( 'Plain Layout', i18n ),
-		attributes: {
-			hasContainer: false,
-		},
-		innerBlocks: [
-			[ 'stackable/heading', { text: __( 'Call to Action' ), textTag: 'h3' } ],
-			[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block.' } ],
-			[ 'stackable/button-group', {}, [
-				[ 'stackable/button', { text: 'Button' } ],
-			] ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'horizontal',
-		title: __( 'Horizontal', i18n ),
-		description: __( 'Horizontal Layout', i18n ),
-		attributes: {
-			hasContainer: true,
-			innerBlockOrientation: 'horizontal',
-		},
-		innerBlocks: [
-			[ 'stackable/heading', { text: __( 'Call to Action' ), textTag: 'h3' } ],
-			[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block.' } ],
-			[ 'stackable/button-group', {}, [
-				[ 'stackable/button', { text: 'Button' } ],
-			] ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'horizontal-2',
-		title: __( 'Horizontal 2', i18n ),
-		description: __( 'Horizontal 2 Layout', i18n ),
-		attributes: {
-			hasContainer: true,
-		},
-		innerBlocks: [
-			[ 'stackable/columns', { forceHideVariationPicker: true }, [
-				[ 'stackable/column', { contentAlign: 'left' }, [
-					[ 'stackable/heading', { text: __( 'Call to Action' ), textTag: 'h3' } ],
-					[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block.' } ],
+const variations = applyFilters(
+	'stackable.call-to-action.variations',
+	[
+		{
+			name: 'default',
+			title: __( 'Default', i18n ),
+			description: __( 'Default Layout', i18n ),
+			isDefault: true,
+			attributes: {
+				hasContainer: true,
+			},
+			innerBlocks: [
+				[ 'stackable/heading', { text: __( 'Call to Action' ), textTag: 'h3' } ],
+				[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block.' } ],
+				[ 'stackable/button-group', {}, [
+					[ 'stackable/button', { text: 'Button' } ],
 				] ],
-				[ 'stackable/column', { contentAlign: 'right' }, [
-					[ 'stackable/button-group', {}, [
-						[ 'stackable/button', { text: 'Button' } ],
-					] ],
-				] ],
-			] ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'horizontal-3',
-		title: __( 'Horizontal 3', i18n ),
-		description: __( 'Horizontal 3 Layout', i18n ),
-		attributes: {
-			hasContainer: true,
+			],
+			scope: [ 'block' ],
 		},
-		innerBlocks: [
-			[ 'stackable/columns', { forceHideVariationPicker: true }, [
-				[ 'stackable/column', { contentAlign: 'left' }, [
-					[ 'stackable/heading', { text: __( 'Call to Action' ), textTag: 'h3' } ],
+		{
+			name: 'plain',
+			title: __( 'Plain', i18n ),
+			description: __( 'Plain Layout', i18n ),
+			attributes: {
+				hasContainer: false,
+			},
+			innerBlocks: [
+				[ 'stackable/heading', { text: __( 'Call to Action' ), textTag: 'h3' } ],
+				[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block.' } ],
+				[ 'stackable/button-group', {}, [
+					[ 'stackable/button', { text: 'Button' } ],
 				] ],
-				[ 'stackable/column', { contentAlign: 'right' }, [
-					[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block.' } ],
-					[ 'stackable/button-group', {}, [
-						[ 'stackable/button', { text: 'Button' } ],
-					] ],
-				] ],
-			] ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'split-centered',
-		title: __( 'Split Centered', i18n ),
-		description: __( 'Split Centered Layout', i18n ),
-		attributes: {
-			hasContainer: true,
+			],
+			scope: [ 'block' ],
 		},
-		innerBlocks: [
-			[ 'stackable/columns', { forceHideVariationPicker: true }, [
-				[ 'stackable/column', { contentAlign: 'right' }, [
-					[ 'stackable/heading', { text: __( 'Call to Action' ), textTag: 'h1' } ],
-				] ],
-				[ 'stackable/column', { contentAlign: 'left' }, [
-					[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block.' } ],
-					[ 'stackable/button-group', {}, [
-						[ 'stackable/button', { text: 'Button' } ],
-					] ],
-				] ],
-			] ],
-		],
-		scope: [ 'block' ],
-	},
-]
+	]
+)
 
 export default variations
