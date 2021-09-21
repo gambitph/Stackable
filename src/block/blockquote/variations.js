a39a15249d8291c5c13678233528bb427fa67b07
Nico Mendoza <blankartwp@gmail.com>
2021-09-21 17:12:20 +0800

    Fix

--- src/block/blockquote/variations.js
+++ src/block/blockquote/variations.js
@@ -3,6 +3,7 @@
  */
 import { __ } from '@wordpress/i18n'
 import { renderToString } from '@wordpress/element'
+import { applyFilters } from '@wordpress/hooks'
 
 /**
  * Internal dependencies
@@ -19,87 +20,37 @@ import { i18n } from 'stackable'
  *
  * @type {WPBlockVariation[]}
  */
-const variations = [
-	{
-		name: 'basic',
-		title: __( 'Basic', i18n ),
-		description: __( 'Basic Layout', i18n ),
-		attributes: {
-			hasContainer: true,
+const variations = applyFilters(
+	'stackable.blockquote.variations',
+	[
+		{
+			name: 'basic',
+			title: __( 'Basic', i18n ),
+			description: __( 'Basic Layout', i18n ),
+			attributes: {
+				hasContainer: true,
+			},
+			innerBlocks: [
+				[ 'stackable/icon', { icon: renderToString( <SVGDefaultQuote /> ) } ],
+				[ 'stackable/text', {
+					text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.',
+				} ],
+			],
+			scope: [ 'block' ],
 		},
-		innerBlocks: [
-			[ 'stackable/icon', { icon: renderToString( <SVGDefaultQuote /> ) } ],
-			[ 'stackable/text', {
-				text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.',
-			} ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'plain',
-		title: __( 'Plain', i18n ),
-		description: __( 'Plain Layout', i18n ),
-		innerBlocks: [
-			[ 'stackable/icon', { icon: renderToString( <SVGDefaultQuote /> ) } ],
-			[ 'stackable/text', {
-				text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.',
-			} ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'highlighted',
-		title: __( 'Highlighted', i18n ),
-		description: __( 'Highlighted Layout', i18n ),
-		innerBlocks: [
-			[ 'stackable/icon', { icon: renderToString( <SVGDefaultQuote /> ) } ],
-			[ 'stackable/text', {
-				text: '<span style="background-color: #D1D1E4;" class="ugb-highlight">Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.</span>',
-				fontSize: 40,
-			} ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'huge',
-		title: __( 'Huge', i18n ),
-		description: __( 'Huge Layout', i18n ),
-		innerBlocks: [
-			[ 'stackable/icon', {
-				icon: renderToString( <SVGDefaultQuote /> ),
-				iconSize: 72,
-			} ],
-			[ 'stackable/text', {
-				fontSize: 42,
-				text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.',
-			} ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'centered-quote',
-		title: __( 'Centered Quote', i18n ),
-		description: __( 'Centered Quote Layout', i18n ),
-		innerBlocks: [
-			[ 'stackable/icon', {
-				icon: renderToString( <SVGDefaultQuote /> ),
-				position: 'absolute',
-				positionNum: {
-					top: 50,
-					right: '',
-					bottom: '',
-					left: 50,
-				},
-				transform: 'translateX(-50px) translateY(-50px)',
-				iconSize: 80,
-				positionNumUnit: '%',
-			} ],
-			[ 'stackable/text', {
-				text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.',
-			} ],
-		],
-		scope: [ 'block' ],
-	},
-]
+		{
+			name: 'plain',
+			title: __( 'Plain', i18n ),
+			description: __( 'Plain Layout', i18n ),
+			innerBlocks: [
+				[ 'stackable/icon', { icon: renderToString( <SVGDefaultQuote /> ) } ],
+				[ 'stackable/text', {
+					text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.',
+				} ],
+			],
+			scope: [ 'block' ],
+		},
+	]
+)
 
 export default variations
