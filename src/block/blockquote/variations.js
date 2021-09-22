80ac317e037668e725a98ac46eb2b5337e58f13c
Nico Mendoza <blankartwp@gmail.com>
2021-09-22 11:29:52 +0800

    fix

--- src/block/blockquote/variations.js
+++ src/block/blockquote/variations.js
@@ -4,6 +4,7 @@
 import { __ } from '@wordpress/i18n'
 import { renderToString } from '@wordpress/element'
 import { applyFilters } from '@wordpress/hooks'
+import { Path, SVG } from '@wordpress/components'
 
 /**
  * Internal dependencies
@@ -27,6 +28,21 @@ const variations = applyFilters(
 			name: 'basic',
 			title: __( 'Basic', i18n ),
 			description: __( 'Basic Layout', i18n ),
+			isDefault: true,
+			icon: (
+				<SVG
+					width="48"
+					height="48"
+					viewBox="0 0 48 48"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<Path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="m39.0625 14h-30.0625v20.0938h30.0625zm-30.0625-2c-1.10457 0-2 .8954-2 2v20.0938c0 1.1045.89543 2 2 2h30.0625c1.1046 0 2-.8955 2-2v-20.0938c0-1.1046-.8954-2-2-2z"
+					/>
+				</SVG>
+			),
 			attributes: {
 				hasContainer: true,
 			},
@@ -42,12 +58,89 @@ const variations = applyFilters(
 			name: 'plain',
 			title: __( 'Plain', i18n ),
 			description: __( 'Plain Layout', i18n ),
+			icon: (
+				<SVG
+					width="48"
+					height="48"
+					viewBox="0 0 48 48"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<Path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="m39.0625 14h-30.0625v20.0938h30.0625zm-30.0625-2c-1.10457 0-2 .8954-2 2v20.0938c0 1.1045.89543 2 2 2h30.0625c1.1046 0 2-.8955 2-2v-20.0938c0-1.1046-.8954-2-2-2z"
+					/>
+				</SVG>
+			),
 			innerBlocks: [
 				[ 'stackable/icon', { icon: renderToString( <SVGDefaultQuote /> ) } ],
 				[ 'stackable/text', {
 					text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.',
 				} ],
 			],
+			scope: [ 'block' ],
+		},
+		{
+			name: 'highlighted',
+			title: __( 'Highlighted', i18n ),
+			description: __( 'Highlighted Layout', i18n ),
+			icon: (
+				<SVG
+					width="48"
+					height="48"
+					viewBox="0 0 48 48"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<Path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="m39.0625 14h-30.0625v20.0938h30.0625zm-30.0625-2c-1.10457 0-2 .8954-2 2v20.0938c0 1.1045.89543 2 2 2h30.0625c1.1046 0 2-.8955 2-2v-20.0938c0-1.1046-.8954-2-2-2z"
+					/>
+				</SVG>
+			),
+			isPremium: true,
+			scope: [ 'block' ],
+		},
+		{
+			name: 'huge',
+			title: __( 'Huge', i18n ),
+			description: __( 'Huge Layout', i18n ),
+			icon: (
+				<SVG
+					width="48"
+					height="48"
+					viewBox="0 0 48 48"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<Path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="m39.0625 14h-30.0625v20.0938h30.0625zm-30.0625-2c-1.10457 0-2 .8954-2 2v20.0938c0 1.1045.89543 2 2 2h30.0625c1.1046 0 2-.8955 2-2v-20.0938c0-1.1046-.8954-2-2-2z"
+					/>
+				</SVG>
+			),
+			isPremium: true,
+			scope: [ 'block' ],
+		},
+		{
+			name: 'centered-quote',
+			title: __( 'Centered Quote', i18n ),
+			description: __( 'Centered Quote Layout', i18n ),
+			icon: (
+				<SVG
+					width="48"
+					height="48"
+					viewBox="0 0 48 48"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<Path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="m39.0625 14h-30.0625v20.0938h30.0625zm-30.0625-2c-1.10457 0-2 .8954-2 2v20.0938c0 1.1045.89543 2 2 2h30.0625c1.1046 0 2-.8955 2-2v-20.0938c0-1.1046-.8954-2-2-2z"
+					/>
+				</SVG>
+			),
+			isPremium: true,
 			scope: [ 'block' ],
 		},
 	]
