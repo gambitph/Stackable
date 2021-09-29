873d6c61f2f7c547b35c47f81e450b2a866609f4
Benjamin Intal <bfintal@gmail.com>
2021-09-29 17:17:37 +0800

    Update src/init.php

--- src/init.php
+++ src/init.php
@@ -45,7 +45,7 @@ if ( ! class_exists( 'Stackable_Init' ) ) {
 			// Load our editor scripts.
 			add_action( 'init', array( $this, 'register_block_editor_assets' ) );
 
-			add_filter( 'init', array( $this, 'register_frontend_assets_nodep' ), 10, 2 );
+			add_filter( 'init', array( $this, 'register_frontend_assets_nodep' ) );
 
 			add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
 
