720fa31b80e251170fed118b8c6bb9254fbbc99a
Benjamin Intal <bfintal@gmail.com>
2021-09-30 17:24:55 +0800

    added missing applyFilters

--- src/block/team-member/variations.js
+++ src/block/team-member/variations.js
@@ -9,6 +9,7 @@ import { i18n, isPro } from 'stackable'
 import {
 	__, _x, sprintf,
 } from '@wordpress/i18n'
+import { applyFilters } from '@wordpress/hooks'
 
 /**
  * Template option choices for predefined columns layouts.
@@ -43,55 +44,58 @@ import {
 	],
 ]
 
-const variations = [
-	{
-		name: 'default',
-		title: __( 'Default', i18n ),
-		description: sprintf( _x( '%s Layout', 'Block layout name', i18n ), __( 'Default', i18n ) ),
-		attributes: { className: 'is-style-default' },
-		isDefault: true,
-		innerBlocks: [
-			[ 'stackable/image', {
-				imageHeight: 75, imageWidth: 75, imageWidthUnit: 'px', imageBorderRadius: 90,
-			} ],
-			[ 'stackable/heading', {
-				text: __( 'Name', i18n ), textTag: 'h3', textRemoveTextMargins: true,
-			} ],
-			[ 'stackable/subtitle', {
-				text: __( 'Position', i18n ),
-			} ],
-			[ 'stackable/text', { text: _x( 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.', 'Content placeholder', i18n ) } ],
-			[ 'stackable/button-group', {}, buttonGroupInnerBlocks ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'plain',
-		title: __( 'Plain', i18n ),
-		description: sprintf( _x( '%s Layout', 'Block layout name', i18n ), __( 'Plain', i18n ) ),
-		attributes: { className: 'is-style-plain', hasContainer: false },
-		innerBlocks: [
-			[ 'stackable/image', {
-				imageHeight: 75, imageWidth: 75, imageWidthUnit: 'px', imageBorderRadius: 90,
-			} ],
-			[ 'stackable/heading', {
-				text: __( 'Name', i18n ), textTag: 'h3', textRemoveTextMargins: true,
-			} ],
-			[ 'stackable/subtitle', {
-				text: __( 'Position', i18n ),
-			} ],
-			[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.' } ],
-			[ 'stackable/button-group', {}, buttonGroupInnerBlocks ],
-		],
-		scope: [ 'block' ],
-	},
-	{
-		name: 'horizontal',
-		title: __( 'Horizontal', i18n ),
-		description: sprintf( _x( '%s Layout', 'Block layout name', i18n ), __( 'Horizontal', i18n ) ),
-		isPremium: ! isPro,
-		scope: [ 'block' ],
-	},
-]
+const variations = applyFilters(
+	'stackable.team-member.variations',
+	[
+		{
+			name: 'default',
+			title: __( 'Default', i18n ),
+			description: sprintf( _x( '%s Layout', 'Block layout name', i18n ), __( 'Default', i18n ) ),
+			attributes: { className: 'is-style-default' },
+			isDefault: true,
+			innerBlocks: [
+				[ 'stackable/image', {
+					imageHeight: 75, imageWidth: 75, imageWidthUnit: 'px', imageBorderRadius: 90,
+				} ],
+				[ 'stackable/heading', {
+					text: __( 'Name', i18n ), textTag: 'h3', textRemoveTextMargins: true,
+				} ],
+				[ 'stackable/subtitle', {
+					text: __( 'Position', i18n ),
+				} ],
+				[ 'stackable/text', { text: _x( 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.', 'Content placeholder', i18n ) } ],
+				[ 'stackable/button-group', {}, buttonGroupInnerBlocks ],
+			],
+			scope: [ 'block' ],
+		},
+		{
+			name: 'plain',
+			title: __( 'Plain', i18n ),
+			description: sprintf( _x( '%s Layout', 'Block layout name', i18n ), __( 'Plain', i18n ) ),
+			attributes: { className: 'is-style-plain', hasContainer: false },
+			innerBlocks: [
+				[ 'stackable/image', {
+					imageHeight: 75, imageWidth: 75, imageWidthUnit: 'px', imageBorderRadius: 90,
+				} ],
+				[ 'stackable/heading', {
+					text: __( 'Name', i18n ), textTag: 'h3', textRemoveTextMargins: true,
+				} ],
+				[ 'stackable/subtitle', {
+					text: __( 'Position', i18n ),
+				} ],
+				[ 'stackable/text', { text: 'Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.' } ],
+				[ 'stackable/button-group', {}, buttonGroupInnerBlocks ],
+			],
+			scope: [ 'block' ],
+		},
+		{
+			name: 'horizontal',
+			title: __( 'Horizontal', i18n ),
+			description: sprintf( _x( '%s Layout', 'Block layout name', i18n ), __( 'Horizontal', i18n ) ),
+			isPremium: ! isPro,
+			scope: [ 'block' ],
+		},
+	]
+)
 
 export default variations
