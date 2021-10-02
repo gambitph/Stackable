c3e2c5a296d5aa93dbf1bbe49f41ae880941d8a3
Nico Mendoza <blankartwp@gmail.com>
2021-10-02 12:16:27 +0800

    Fix: Fixes image shadow style

--- src/block-components/image/edit.js
+++ src/block-components/image/edit.js
@@ -30,6 +30,19 @@ import { __ } from '@wordpress/i18n'
 import { applyFilters } from '@wordpress/hooks'
 import { useMemo } from '@wordpress/element'
 
+const IMAGE_SHADOWS = [
+	'none',
+	'0px 0 1px rgba(120, 120, 120, 0.5)',
+	'0px 0 2px rgba(120, 120, 120, 0.5)',
+	'0px 5px 10px rgba(153, 153, 153, 0.35)',
+	'0px 2px 20px rgba(153, 153, 153, 0.2)',
+	'25px 10px 30px rgba(18, 63, 82, 0.3)',
+	'0px 10px 30px rgba(0, 0, 0, 0.05)',
+	'7px 5px 30px rgba(72, 73, 121, 0.15)',
+	'0px 10px 60px rgba(0, 0, 0, 0.1)',
+	'70px 130px -60px rgba(72, 73, 121, 0.38) ',
+]
+
 const Controls = props => {
 	const { clientId } = useBlockEditContext()
 
@@ -149,6 +162,7 @@ const Controls = props => {
 			/>
 
 			<ShadowControl
+				options={ IMAGE_SHADOWS }
 				attribute="imageShadow"
 				hover="all"
 			/>
