d520a6b87de69fd53f161054f603ab08f3057eb0
Benjamin Intal <bfintal@gmail.com>
2021-10-03 16:23:29 +0800

    use css instead

--- src/block-components/effects-animations/index.js
+++ src/block-components/effects-animations/index.js
@@ -3,21 +3,6 @@ import { Edit } from './edit'
 import { Style } from './style'
 
 import { applyFilters } from '@wordpress/hooks'
-import domReady from '@wordpress/dom-ready'
-
-let CALL_COUNT = 0
-
-/**
- * Append the `.stk--anim-init` in the editor.
- */
-if ( CALL_COUNT === 0 ) {
-	domReady( () => {
-		requestAnimationFrame( () => {
-			document.body.classList.add( 'stk--anim-init' )
-			CALL_COUNT++
-		} )
-	} )
-}
 
 export const EffectsAnimations = props => {
 	return applyFilters( 'stackable.block-component.effects-animations', null, props )
