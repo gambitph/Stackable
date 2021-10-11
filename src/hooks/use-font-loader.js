bb25164ffcc930071b304fa7f908ede63bd3823e
Benjamin Intal <bfintal@gmail.com>
2021-10-12 02:00:31 +0800

    Optimization: only call google font when needed

--- src/hooks/use-font-loader.js
+++ src/hooks/use-font-loader.js
@@ -1,8 +1,10 @@
+import { useEffect } from '@wordpress/element'
 import { doAction } from '@wordpress/hooks'
 import { loadGoogleFont } from '~stackable/util'
 
 export const useFontLoader = fontFamilyValue => {
-	loadGoogleFont( fontFamilyValue )
-
-	doAction( 'stackable.font-loader.load', fontFamilyValue )
+	useEffect( () => {
+		loadGoogleFont( fontFamilyValue )
+		doAction( 'stackable.font-loader.load', fontFamilyValue )
+	}, [ fontFamilyValue ] )
 }
