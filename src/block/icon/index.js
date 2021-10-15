cdc04f0e715f1b7f6a68c7eb82fffec5d24535c7
Nico Mendoza <blankartwp@gmail.com>
2021-10-15 13:48:59 +0800

    added transforms

--- src/block/icon/index.js
+++ src/block/icon/index.js
@@ -14,6 +14,7 @@ import edit from './edit'
 import save from './save'
 import metadata from './block.json'
 import schema from './schema'
+import transforms from './transforms'
 
 /**
  * WordPress dependencies
@@ -28,6 +29,7 @@ export const settings = applyFilters( 'stackable.block.metadata', {
 	supports: {
 		anchor: true,
 	},
+	transforms,
 
 	edit,
 	save,
