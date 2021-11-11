02253fc46413aa1b8ab8f09041aa93f18d556123
blankart <blankartwp@gmail.com>
2021-11-11 15:05:01 +0800

    Update src/block/heading/index.js

--- src/block/heading/index.js
+++ src/block/heading/index.js
@@ -23,7 +23,7 @@ export const settings = applyFilters( 'stackable.block.metadata', {
 	attributes: schema,
 	supports: {
 		anchor: true,
-		align: true,
+		align: [ 'center', 'wide', 'full' ],
 	},
 	edit,
 	save,
