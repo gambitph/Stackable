4e8b57b38e60d7b5556c334e227d3141f895f0a5
Benjamin Intal <bfintal@gmail.com>
2021-12-21 18:54:45 +0800

    fix: block alignments not inherited properly in tablet and mobile (#2056)
    * fix: block alignments not inherited properly in tablet and mobile
    
    * fix: button group collapsing alignment inheritance [skip ci]
    
    * fix: blog posts typography alignment for tablet and mobile not applied. fixes #2065
    
    * fix: alignment in mobile in editor

--- src/block-components/typography/get-typography-classes.js
+++ src/block-components/typography/get-typography-classes.js
@@ -9,6 +9,8 @@ export const getTypographyClasses = ( attributes = {}, attrNameTemplate = '%s' )
 	return classnames( {
 		'stk--is-gradient': attributes[ getAttributeName( 'textColorType' ) ] === 'gradient',
 		[ `has-text-align-${ attributes[ getAttributeName( 'textAlign' ) ] }` ]: attributes[ getAttributeName( 'textAlign' ) ],
+		[ `has-text-align-${ attributes[ getAttributeName( 'textAlignTablet' ) ] }-tablet` ]: attributes[ getAttributeName( 'textAlignTablet' ) ],
+		[ `has-text-align-${ attributes[ getAttributeName( 'textAlignMobile' ) ] }-mobile` ]: attributes[ getAttributeName( 'textAlignMobile' ) ],
 		'has-text-color': !! attributes[ getAttributeName( 'textColor1' ) ],
 		[ attributes[ getAttributeName( 'textColorClass' ) ] ]: !! attributes[ getAttributeName( 'textColorClass' ) ],
 	} )
