84d5670daffc90aaffa79c229c2d792f989928b0
Benjamin Intal <bfintal@gmail.com>
2022-01-15 00:30:33 +0800

    fix: column resizing now works in tablet and mobile. fixes #2095 (#2109)

---
+++ src/hooks/use-editor-dom.js
@@ -0,0 +1,61 @@
+/**
+ * Internal dependencies
+ */
+import { useDeviceType } from '.'
+
+/**
+ * WordPress dependencies
+ */
+import {
+	useState, useEffect, useMemo,
+} from '@wordpress/element'
+
+/**
+ * Gets the editor wrapper DOM element. This should be the way if you need to do
+ * a querySelector and to get the raw DOM element of a block. This handles
+ * iframe Tablet and Mobile previews introduced in WP 5.9.
+ *
+ * Take note that this can produce a `null` value if the editor is still
+ * transitioning from Desktop to Tablet or Mobile previews.
+ *
+ * @return {DomElement} This can be null when the editor is transitioning from
+ * one device size to another.
+ */
+export const useEditorDom = () => {
+	const deviceType = useDeviceType()
+	const [ iframeForceUpdate, setIframeForceUpdate ] = useState( 0 )
+
+	useEffect( () => {
+		if ( deviceType === 'Desktop' ) {
+			// Need to force an update because it takes some time for the iframe to disappear.
+			setTimeout( () => {
+				setIframeForceUpdate( iframeForceUpdate + 1 )
+			}, 200 )
+		} else { // Tablet or Mobile.
+			const iframeEl = document.querySelector( `iframe[name="editor-canvas"]` )
+			if ( iframeEl ) {
+				if ( iframeEl.contentDocument.body ) {
+					setIframeForceUpdate( iframeForceUpdate + 1 )
+				} else {
+					const timeout = setTimeout( () => {
+						if ( iframeEl.contentDocument.body ) {
+							setIframeForceUpdate( iframeForceUpdate + 1 )
+						}
+					}, 200 )
+					iframeEl.onload = () => {
+						clearTimeout( timeout )
+						setIframeForceUpdate( iframeForceUpdate + 1 )
+					}
+				}
+			}
+		}
+	}, [ deviceType ] )
+
+	return useMemo( () => {
+		const iframeEl = document.querySelector( `iframe[name="editor-canvas"]` )
+		if ( iframeEl ) {
+			return iframeEl.contentDocument.body
+		}
+		return document.querySelector( `.editor-styles-wrapper` )
+	}, [ iframeForceUpdate, deviceType ] )
+}
