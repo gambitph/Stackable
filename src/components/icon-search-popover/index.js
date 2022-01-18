f458734e1d9a38b07e4418a4dc61712d38e85e38
Pio Concepcion <49634311+prconcepcion@users.noreply.github.com>
2022-01-18 14:23:59 +0800

    fix: changed useRef to ref. (#2129)
    * Changed 'useRef=' to 'ref='.
    
    * Update src/block-components/icon/index.js [skip ci]
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

--- src/components/icon-search-popover/index.js
+++ src/components/icon-search-popover/index.js
@@ -160,7 +160,7 @@ const IconSearchPopover = props => {
 			onClickOutside={ props.onClickOutside }
 			position={ props.position }
 			anchorRef={ props.anchorRef }
-			useRef={ props.useRef }
+			ref={ props.useRef }
 		>
 			<PanelBody>
 				<FileDrop
