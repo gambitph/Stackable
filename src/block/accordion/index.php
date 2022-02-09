13694dfa534dec4d136fda8ecbdd8ebd31fbcf9a
Marco Enrico <mvalviar@gmail.com>
2022-02-09 20:59:21 +0800

    Update src/block/accordion/index.php
    Remember coding standard.
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

--- src/block/accordion/index.php
+++ src/block/accordion/index.php
@@ -27,7 +27,7 @@ if ( ! function_exists( 'stackable_load_accordion_frontend_polyfill_script' ) ) 
 	 */
 	function stackable_load_accordion_frontend_polyfill_script() {
 
-		 $user_agent = ! empty($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+		 $user_agent = ! empty( $_SERVER['HTTP_USER_AGENT'] ) ? $_SERVER['HTTP_USER_AGENT'] : '';
 
 		 if ( ! $user_agent ) {
 			 return;
