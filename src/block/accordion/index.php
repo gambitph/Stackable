648f31b84a8f3fed0797d7203f359a887fdaa7b9
Marco Enrico <mvalviar@gmail.com>
2022-02-11 15:27:42 +0800

    fix (accordion block): accordion wont open in old browsers (#2155)
    * Add polyfill for summary/details elements.
    
    * Add polyfill for summary and details elements.
    
    * Replace wp_check_browser_version with $_SERVER["HTTP_USER_AGENT"].
    
    * Update src/block/accordion/index.php
    
    Remember coding standard.
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>
    
    * Update src/block/accordion/index.php
    
    Remember coding standard,  fix phpcbf.
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>
    
    * Update src/block/accordion/index.php
    
    Remember coding standard.
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>
    
    * Update polyfill script.
    
    * line breaks become br tags
    
    * added user agent matching for sarafi 13.1.3 and Android 7
    
    * restructure to prevent preg_match
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

--- src/block/accordion/index.php
+++ src/block/accordion/index.php
@@ -18,3 +18,50 @@ if ( ! function_exists( 'stackable_load_accordion_frontend_script' ) ) {
 	}
 	add_action( 'stackable/accordion/enqueue_scripts', 'stackable_load_accordion_frontend_script' );
 }
+
+if ( ! function_exists( 'stackable_load_accordion_frontend_polyfill_script' ) ) {
+	/**
+	 * Adds polyfill for summary/details element that are * used in accordion blocks.
+	 *
+	 * TODO: confirm that this works on older browsers
+	 */
+	function stackable_load_accordion_frontend_polyfill_script() {
+
+		$user_agent = ! empty( $_SERVER['HTTP_USER_AGENT'] ) ? $_SERVER['HTTP_USER_AGENT'] : '';
+
+		if ( ! $user_agent ) {
+			return;
+		}
+
+		$load_polyfill = false;
+
+		if (
+			// Safari 13.1.3
+			( stripos( $user_agent, 'Version/13.' ) !== false && stripos( $user_agent, 'Safari/' ) !== false ) ||
+			// Adnroid 7.0 Samsung Galaxy J5
+			( stripos( $user_agent, 'Android 7.' ) !== false && stripos( $user_agent, 'Chrome/' ) !== false ) ||
+			// IE 11
+			stripos( $user_agent, 'Trident/7.0; rv:11.0' ) !== false
+		) {
+			$load_polyfill = true;
+		} else if ( stripos( $user_agent, ' Edge/' ) !== false || stripos( $user_agent, ' Edg/' ) !== false ) {
+			$matches = array();
+			if ( preg_match( '/(Edge?)\/(\d+)/', $user_agent, $matches ) ) {
+				$version = intval( $matches[2] );
+				if ( $version < 79 ) {
+					$load_polyfill = true;
+				}
+			}
+		}
+
+		if ( $load_polyfill ) {
+			wp_enqueue_script(
+				'stk-frontend-accordion-polyfill',
+				plugins_url( 'dist/frontend_block_accordion_polyfill.js', STACKABLE_FILE ),
+				array(),
+				STACKABLE_VERSION
+			);
+		}
+	}
+	add_action( 'wp_footer', 'stackable_load_accordion_frontend_polyfill_script' );
+}
