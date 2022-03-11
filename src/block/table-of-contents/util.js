8bdf42b2c758fd09e08d164f1242aa980b5b66fb
Benjamin Intal <bfintal@gmail.com>
2022-03-11 19:30:40 +0800

    fix: refactored flat to tree heading conversion

--- src/block/table-of-contents/util.js
+++ src/block/table-of-contents/util.js
@@ -139,79 +139,46 @@ export const getHeadingsFromEditorDom = editorDom => {
 		return convertBlockToHeadingObject( block )
 	} )
 
-	return headingObjects.map( ( heading, i ) => {
-		// Keep note of the heading tag level
+	// Keep note of the heading tag level
+	return headingObjects.map( heading => {
 		heading.tag = heading.level
-
-		// Ensure higher levels do not skip a level.
-		if ( i > 0 ) {
-			const prevLevel = headingObjects[ i - 1 ].level
-			const currLevel = heading.level
-
-			if ( currLevel > prevLevel ) {
-				heading.level = prevLevel + 1
-			}
-		}
 		return heading
 	} )
 }
 
 /**
- * Takes a flat list of heading parameters and nests them based on each header's
- * immediate parent's level.
+ * Converts a flat list of heading objects and nests them based on the immediate
+ * subheading's level. This takes into account incorrect heading level
+ * arrangements.
+ * .
  *
- * Lifted from Kadence block's implementation.
+ * @param {Array} headings Heading objects formed by getHeadingsFromEditorDom
+ * @param {Object} internalData Internal data object used to generated the
+ * nested heading list. The index used to generate the list is stored in the
+ * internalData.index property.
  *
- * @param {Array} headingList The flat list of headings to nest.
- * @param {number} index       The current list index.
- * @return {Array} The nested list of headings.
+ * @return {Array} A tree of heading objects.
  */
-export const linearToNestedHeadingList = ( headingList, index = 0 ) => {
-	const nestedHeadingList = []
-	headingList.forEach( ( heading, key ) => {
-		// if ( heading.content === '' ) {
-		// 	return
-		// }
-		// Make sure we are only working with the same level as the first iteration in our set.
-		if ( heading.level === headingList[ 0 ].level ) {
-			// Check that the next iteration will return a value.
-			// If it does and the next level is greater than the current level,
-			// the next iteration becomes a child of the current interation.
-			if (
-				headingList[ key + 1 ] !== undefined &&
-				headingList[ key + 1 ].level > heading.level
-			) {
-				// We need to calculate the last index before the next iteration that has the same level (siblings).
-				// We then use this last index to slice the array for use in recursion.
-				// This prevents duplicate nodes.
-				let endOfSlice = headingList.length
-				for ( let i = key + 1; i < headingList.length; i++ ) {
-					if ( headingList[ i ].level === heading.level ) {
-						endOfSlice = i
-						break
-					}
-				}
+export const linearToNestedHeadingList = ( headings, internalData = { index: 0 } ) => {
+	const newHeadings = []
 
-				// We found a child node: Push a new node onto the return array with children.
-				nestedHeadingList.push( {
-					heading,
-					index: index + key,
-					children: linearToNestedHeadingList(
-						headingList.slice( key + 1, endOfSlice ),
-						index + key + 1,
-						true
-					),
-				} )
-			} else {
-				// No child node: Push a new node onto the return array.
-				nestedHeadingList.push( {
-					heading,
-					index: index + key,
-					children: null,
-				} )
+	while ( headings.length ) {
+		const currHeading = headings.shift()
+		const children = []
+
+		while ( headings.length ) {
+			if ( headings[ 0 ].level <= currHeading.level ) {
+				break
 			}
+			children.push( { ...headings.shift() } )
 		}
-	} )
 
-	return nestedHeadingList
+		newHeadings.push( {
+			heading: { ...currHeading },
+			index: internalData.index++,
+			children: linearToNestedHeadingList( children, internalData ),
+		} )
+	}
+
+	return newHeadings
 }
