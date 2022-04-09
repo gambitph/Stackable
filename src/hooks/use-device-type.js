94d910e1fa412be2d894b7e8dda19740148212a6
Pio Concepcion <49634311+prconcepcion@users.noreply.github.com>
2022-04-09 20:14:00 +0800

    Fixes responsive toggle in in the widgets editor (#2228)
    * Prevent setPreviewDeviceType() in widget module.
    
    * added internal setter and getter for deviceType if a preview isn't available
    
    * Update src/components/responsive-toggle/index.js [skip ci]
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

--- src/hooks/use-device-type.js
+++ src/hooks/use-device-type.js
@@ -1,16 +1,48 @@
-import { useSelect } from '@wordpress/data'
+import {
+	useSelect, createReduxStore, register,
+} from '@wordpress/data'
 
 export const useDeviceType = () => {
-	const { deviceType } = useSelect(
-		select => {
-			return {
-				deviceType: select(
-					'core/edit-post'
-				)?.__experimentalGetPreviewDeviceType() || 'Desktop',
-			}
-		},
-		[]
-	)
+	const { deviceType } = useSelect( select => {
+		let deviceType = 'Desktop'
+
+		// In some editors, there is no edit-post / preview device type. If that
+		// happens, we just set our own internal device type.
+		if ( select( 'core/edit-post' ) ) {
+			deviceType = select( 'core/edit-post' ).__experimentalGetPreviewDeviceType()
+		} else {
+			deviceType = select( 'stackable/device-type' ).getDeviceType()
+		}
+		return { deviceType }
+	}, [] )
 
 	return deviceType || ''
 }
+
+/**
+ * Internal store for the device type just in case the editor doesn't have one.
+ */
+const STORE_ACTIONS = {
+	setDeviceType: deviceType => {
+		return { type: 'UPDATE_DEVICE_TYPE', deviceType }
+	},
+}
+
+const STORE_SELECTORS = {
+	getDeviceType: state => state,
+}
+
+const STORE_REDUCER = ( state = 'Desktop', action ) => {
+	switch ( action.type ) {
+		case 'UPDATE_DEVICE_TYPE': {
+			return action.deviceType
+		}
+	}
+	return state
+}
+
+register( createReduxStore( 'stackable/device-type', {
+	reducer: STORE_REDUCER,
+	actions: STORE_ACTIONS,
+	selectors: STORE_SELECTORS,
+} ) )
