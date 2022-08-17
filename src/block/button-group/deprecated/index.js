0b532376b1aa3bf778c8f8288ad9b18414edf439
Benjamin Intal <bfintal@gmail.com>
2022-08-17 18:40:33 +0800

    fix: added retained alignment bug

--- src/block/button-group/deprecated/index.js
+++ src/block/button-group/deprecated/index.js
@@ -9,12 +9,19 @@ const deprecated = [
 	{
 		attributes: attributes( '3.4.2' ),
 		save: withVersion( '3.4.2' )( Save ),
-		migrate: ( { collapseOn } ) => {
+		migrate: attributes => {
+			const {
+				collapseOn,
+				contentAlign,
+			} = attributes
 			return {
 				...attributes,
+				// Port collapse attribute to the new alignment option.
 				buttonAlign: collapseOn === 'desktop' ? 'vertical' : '',
 				buttonAlignTablet: collapseOn === 'tablet' ? 'vertical' : '',
 				buttonAlignMobile: collapseOn === 'mobile' ? 'vertical' : '',
+				// There was a bug <= v3.4.2 where collapsing on desktop would center the buttons.
+				contentAlign: collapseOn === 'desktop' ? 'center' : contentAlign,
 			}
 		},
 	},
