433c9ba75f2e6ea8babf942967ff9e7d737ba093
Pio Concepcion <49634311+prconcepcion@users.noreply.github.com>
2022-08-23 17:46:57 +0800

    fix ( global settings): global styles are now added to the iframe when changing device preview. (#2388)
    * Applies global styles to fse and preview types
    
    * Removed debugging messages
    
    * Refactored implementation
    
    * Lessened flickering when switching between tablet/mobile to desktop preview
    
    Co-authored-by: prconcepcion <pioconcepcion3@yahoo.com>
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

--- src/plugins/get-editor-dom/index.js
+++ src/plugins/get-editor-dom/index.js
@@ -8,9 +8,9 @@ import { useDeviceType } from '~stackable/hooks'
  * WordPress dependencies
  */
 import {
-	useState, useEffect, useMemo,
+	useState, useEffect, useMemo, useRef,
 } from '@wordpress/element'
-import { dispatch } from '@wordpress/data'
+import { dispatch, useSelect } from '@wordpress/data'
 
 /**
  * Gets the editor wrapper DOM element. This should be the way if you need to do
@@ -29,46 +29,70 @@ import { dispatch } from '@wordpress/data'
 export const EditorDom = () => {
 	const deviceType = useDeviceType()
 	const [ iframeForceUpdate, setIframeForceUpdate ] = useState( 0 )
-	const [ timeoutCache, setTimeoutCache ] = useState( null )
+	const timeout = useRef( null )
+	const interval = useRef( null )
+
+	// If in FSE, switching templates will recreate the editor.
+	const editedSitePostId = useSelect( select => {
+		return select( 'core/edit-site' )?.getEditedPostId?.()
+	} )
 
 	useEffect( () => {
+		// If the editor is available right away, use it.
+		const editorEl = document.querySelector( `.editor-styles-wrapper, iframe[name="editor-canvas"]` )
+		if ( editorEl ) {
+			setIframeForceUpdate( v => v + 1 )
+		}
+
 		if ( deviceType === 'Desktop' ) {
-			// Need to force an update because it takes some time for the iframe to disappear.
-			clearTimeout( timeoutCache )
-			const timeout = setTimeout( () => {
-				setIframeForceUpdate( iframeForceUpdate + 1 )
+			// We have to wait for the editor area to load (e.g. FSE iframe takes a long time to load)
+			interval.current = setInterval( () => {
+				const editorEl = document.querySelector( `.editor-styles-wrapper, iframe[name="editor-canvas"]` )
+				if ( editorEl ) {
+					clearInterval( interval.current )
+					clearTimeout( timeout.current )
+					setIframeForceUpdate( v => v + 1 )
+					// There's a chance that the editor STILL isn't ready, try again.
+					setTimeout( () => {
+						setIframeForceUpdate( v => v + 1 )
+					}, 200 )
+				}
 			}, 200 )
-			setTimeoutCache( timeout )
 		} else { // Tablet or Mobile.
 			const iframeEl = document.querySelector( `iframe[name="editor-canvas"]` )
 			if ( iframeEl ) {
 				const body = iframeEl.contentDocument.body
 				if ( body && body.querySelector( '.block-editor-block-list__layout' ) ) {
-					setIframeForceUpdate( iframeForceUpdate + 1 )
+					clearInterval( interval.current )
+					setIframeForceUpdate( v => v + 1 )
 				} else {
-					clearTimeout( timeoutCache )
-					const timeout = setTimeout( () => {
+					clearTimeout( timeout.current )
+					timeout.current = setTimeout( () => {
 						const body = iframeEl.contentDocument.body
 						if ( body && body.querySelector( '.block-editor-block-list__layout' ) ) {
-							setIframeForceUpdate( iframeForceUpdate + 1 )
+							setIframeForceUpdate( v => v + 1 )
 						}
 					}, 200 )
-					setTimeoutCache( timeout )
 					iframeEl.onload = () => {
-						clearTimeout( timeoutCache )
+						clearTimeout( timeout.current )
 						const body = iframeEl.contentDocument.body
 						if ( body && body.querySelector( '.block-editor-block-list__layout' ) ) {
-							setIframeForceUpdate( iframeForceUpdate + 1 )
+							setIframeForceUpdate( v => v + 1 )
 						} else {
 							setTimeout( () => {
-								setIframeForceUpdate( iframeForceUpdate + 1 )
+								setIframeForceUpdate( v => v + 1 )
 							}, 200 )
 						}
 					}
 				}
 			}
 		}
-	}, [ deviceType ] )
+
+		return () => {
+			clearInterval( interval.current )
+			clearTimeout( timeout.current )
+		}
+	}, [ deviceType, editedSitePostId ] )
 
 	useMemo( () => {
 		const iframeEl = document.querySelector( `iframe[name="editor-canvas"]` )
