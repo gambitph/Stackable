1381eed807c8f3d2705875f125d44378a2fa52c6
Benjamin Intal <bfintal@gmail.com>
2022-09-15 22:08:18 +0800

    fix: unique id isn't anymore updated for duplicated/copy pasted blocks

--- src/block-components/block-div/use-unique-id.js
+++ src/block-components/block-div/use-unique-id.js
@@ -1,3 +1,4 @@
+import { useEffect } from '@wordpress/element'
 import { useBlockEditContext } from '@wordpress/block-editor'
 
 export const createUniqueClass = uid => `${ uid.substring( 0, 7 ) }`
@@ -5,22 +6,26 @@ export const createUniqueClass = uid => `${ uid.substring( 0, 7 ) }`
 export const useUniqueId = ( attributes, autoApplyUniqueId = true ) => {
 	const { clientId } = useBlockEditContext()
 
-	// If auto apply unique id is disabled, don't generate a new one. But if
-	// there already is a unique id, we need to still check if it's unique.
-	if ( ! autoApplyUniqueId && ! attributes.uniqueId ) {
-		return
-	}
-
-	// When there's no unique ID yet, create one.
-	const uniqueClass = createUniqueClass( clientId )
-	if ( ! attributes.uniqueId ) {
-		attributes.uniqueId = uniqueClass
-
-		// If there's one already, check whether the we need to re-create one.
-		// Duplicating a block or copy pasting a block may give us duplicate IDs.
-	} else if ( uniqueClass !== attributes.uniqueId ) {
-		if ( document.querySelectorAll( `[data-block-id="${ attributes.uniqueId }"]` ).length > 1 ) {
-			attributes.uniqueId = uniqueClass
+	// Need to do this when the clientId changes (when a block is
+	// cloned/duplicated).
+	useEffect( () => {
+		// If auto apply unique id is disabled, don't generate a new one. But if
+		// there already is a unique id, we need to still check if it's unique.
+		if ( ! autoApplyUniqueId && ! attributes.uniqueId ) {
+			return
 		}
-	}
+
+		// When there's no unique ID yet, create one.
+		const uniqueClass = createUniqueClass( clientId )
+		if ( ! attributes.uniqueId ) {
+			attributes.uniqueId = uniqueClass
+
+			// If there's one already, check whether the we need to re-create one.
+			// Duplicating a block or copy pasting a block may give us duplicate IDs.
+		} else if ( uniqueClass !== attributes.uniqueId ) {
+			if ( document.querySelectorAll( `[data-block-id="${ attributes.uniqueId }"]` ).length > 1 ) {
+				attributes.uniqueId = uniqueClass
+			}
+		}
+	}, [ clientId ] )
 }
