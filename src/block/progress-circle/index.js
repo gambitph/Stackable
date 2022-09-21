f28dbb023ebf8daca213a0264d99dec806028633
Benjamin Intal <bfintal@gmail.com>
2022-09-21 17:55:52 +0800

    added icon

--- src/block/progress-circle/index.js
+++ src/block/progress-circle/index.js
@@ -9,17 +9,17 @@ import edit from './edit'
 import save from './save'
 import schema from './schema'
 import metadata from './block.json'
+import { ProgressCircleIcon } from '~stackable/icons'
 
 /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n'
 import { applyFilters } from '@wordpress/hooks'
-import { Icon, wordpress } from '@wordpress/icons'
 
 export const settings = applyFilters( 'stackable.block.metadata', {
 	...metadata,
-	icon: <Icon icon={ wordpress } />,
+	icon: ProgressCircleIcon,
 	attributes: schema,
 	edit,
 	save,
