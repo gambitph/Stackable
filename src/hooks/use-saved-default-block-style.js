e9789068b8301765ad2871be38acddf719b555b4
Benjamin Intal <bfintal@gmail.com>
2022-09-22 21:53:33 +0800

    fix: saved default blocks not applied

--- src/hooks/use-saved-default-block-style.js
+++ src/hooks/use-saved-default-block-style.js
@@ -23,75 +23,83 @@ export const useSavedDefaultBlockStyle = blockProps => {
 		clientId, name, attributes,
 	} = blockProps
 	const [ isApplied, setIsApplied ] = useState( false )
-	const [ canApply, setCanApply ] = useState( false )
+	const [ replaceInnerBlocks, setReplaceInnerBlocks ] = useState( null )
+
+	// Dispatch the replace inner blocks inside a useEffect to prevent updating
+	// during render.
+	useEffect( () => {
+		if ( clientId && replaceInnerBlocks ) {
+			dispatch( 'core/block-editor' ).replaceInnerBlocks(
+				clientId,
+				replaceInnerBlocks,
+				false,
+			)
+			setReplaceInnerBlocks( null )
+		}
+	}, [ clientId, replaceInnerBlocks ] )
 
 	// Unique ID setting is immediate, so we need to catch if we can apply the default block styles.
-	if ( name.startsWith( 'stackable/' ) && ! attributes.uniqueId && ! canApply ) {
-		setCanApply( true )
+	if ( ! name.startsWith( 'stackable/' ) || attributes.uniqueId ) {
+		return
 	}
 
 	// Apply the default saved styles to the block.
-	useEffect( () => {
-		if ( canApply && ! isApplied ) {
+	if ( ! isApplied ) {
 		// If there is a variation, it means there is a variation/layout picker, in
 		// this case the variation picker will do the job.
-			const hasVariations = getBlockVariations( name ).length > 0
-			if ( ! hasVariations ) {
+		const hasVariations = getBlockVariations( name ).length > 0
+		if ( ! hasVariations ) {
 			// Let others prevent default block styles to be added depending on the
 			// parent (e.g. helpful for preventing the Accordion block from getting the
 			// default saved styles of the Icon Label block)
-				const parentBlocks = select( 'stackable/block-context' ).getBlockContext( clientId ).parentTree?.map( block => block.name ) || []
-				const enable = applyFilters( 'stackable.block-default-styles.use-saved-style', true, blockProps, parentBlocks )
-				if ( ! enable ) {
-					return
-				}
+			const parentBlocks = select( 'stackable/block-context' ).getBlockContext( clientId ).parentTree?.map( block => block.name ) || []
+			const enable = applyFilters( 'stackable.block-default-styles.use-saved-style', true, blockProps, parentBlocks )
+			if ( ! enable ) {
+				return
+			}
 
-				// Get the default block styles.
-				const blockStyle = select( 'stackable/block-styles' ).getDefaultBlockStyle( name )
-				if ( ! blockStyle ) {
-					return
-				}
-				const blockData = JSON.parse( blockStyle.data )
-				// Filter out content attributes.
-				let newAttributes = omit( blockData.attributes, CONTENT_ATTRIBUTES, 'uniqueId' )
+			// Get the default block styles.
+			const blockStyle = select( 'stackable/block-styles' ).getDefaultBlockStyle( name )
+			if ( ! blockStyle ) {
+				return
+			}
+			const blockData = JSON.parse( blockStyle.data )
+			// Filter out content attributes.
+			let newAttributes = omit( blockData.attributes, CONTENT_ATTRIBUTES, 'uniqueId' )
 
-				// Check if there are any block styles added in the block, if there
-				// are, the default attributes may not work properly, we may need to
-				// call the onSelect to apply the style.
-				if ( attributes.className?.includes( 'is-style-' ) ) {
-					let potentialStyleName = attributes.className.match( /is-style-([^\s]+)/ )
-					potentialStyleName = potentialStyleName ? potentialStyleName[ 1 ] : ''
-					const blockStyles = getDefinedBlockStyles( name )
-					const blockStyle = find( blockStyles, { name: potentialStyleName } )
+			// Check if there are any block styles added in the block, if there
+			// are, the default attributes may not work properly, we may need to
+			// call the onSelect to apply the style.
+			if ( attributes.className?.includes( 'is-style-' ) ) {
+				let potentialStyleName = attributes.className.match( /is-style-([^\s]+)/ )
+				potentialStyleName = potentialStyleName ? potentialStyleName[ 1 ] : ''
+				const blockStyles = getDefinedBlockStyles( name )
+				const blockStyle = find( blockStyles, { name: potentialStyleName } )
 
-					if ( blockStyle ) {
-						newAttributes = {
-							...newAttributes,
-							...blockStyle.onSelect( newAttributes ),
-						}
+				if ( blockStyle ) {
+					newAttributes = {
+						...newAttributes,
+						...blockStyle.onSelect( newAttributes ),
 					}
 				}
-
-				// Apply the saved attributes to the block.
-				Object.keys( newAttributes ).forEach( attrName => {
-					attributes[ attrName ] = newAttributes[ attrName ]
-				} )
-
-				// Create and apply the innerBlocks.
-				if ( blockData.innerBlocks?.length ) {
-					const innerBlocks = createBlocksFromInnerBlocksTemplate( blockData.innerBlocks )
-					// We need to add unique Ids to prevent the default styles from getting applied.
-					recursivelyAddUniqueIdToInnerBlocks( innerBlocks )
-					dispatch( 'core/block-editor' ).replaceInnerBlocks(
-						clientId,
-						innerBlocks,
-						false,
-					)
-				}
-
-				// This can repeat because of nested blocks, ens
-				setIsApplied( true )
 			}
+
+			// Apply the saved attributes to the block.
+			// console.log( 'applied!' )
+			Object.keys( newAttributes ).forEach( attrName => {
+				attributes[ attrName ] = newAttributes[ attrName ]
+			} )
+
+			// Create and apply the innerBlocks.
+			if ( blockData.innerBlocks?.length ) {
+				const innerBlocks = createBlocksFromInnerBlocksTemplate( blockData.innerBlocks )
+				// We need to add unique Ids to prevent the default styles from getting applied.
+				recursivelyAddUniqueIdToInnerBlocks( innerBlocks )
+				setReplaceInnerBlocks( innerBlocks ) // replaceInnerBlocks in useEffect to prevent React error.
+			}
+
+			// This can repeat because of nested blocks, ens
+			setIsApplied( true )
 		}
-	}, [ clientId, name, canApply, attributes.className, isApplied ] )
+	}
 }
