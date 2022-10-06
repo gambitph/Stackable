d87d195597a3e2a92c39b2ac8e980e00c5c7edcb
prconcepcion <pioconcepcion3@yahoo.com>
2022-10-07 06:37:42 +0800

    Added filter

---
+++ src/block/table-of-contents/index.php
@@ -0,0 +1,15 @@
+<?
+
+/**
+ * Filter to add our plugin to the list of compatible plugins in rank math.
+ *
+ * @param array TOC plugins.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_filter( 'rank_math/researches/toc_plugins', function( $toc_plugins ) {
+       $toc_plugins['Stackable/plugin.php'] = 'Stackable';
+    return $toc_plugins;
+});
