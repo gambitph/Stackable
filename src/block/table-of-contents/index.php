ad57d15e0b3f3184e6964ffb16369c406fdb6421
Pio Concepcion <49634311+prconcepcion@users.noreply.github.com>
2022-10-21 11:20:02 +0800

    fix (seo): added a filter that would add stackable to the list of compatible plugins with Rank Math (#2441)
    * Added filter
    
    * Update src/block/table-of-contents/index.php [skip ci]
    
    * load php script
    
    * refactored code
    
    Co-authored-by: prconcepcion <pioconcepcion3@yahoo.com>
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

---
+++ src/block/table-of-contents/index.php
@@ -0,0 +1,16 @@
+<?
+
+/**
+ * Filter to add our plugin to the list of compatible plugins in rank math.
+ *
+ * @param array TOC plugins.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_filter( 'rank_math/researches/toc_plugins', function( $toc_plugins ) {
+	$toc_plugins['stackable-ultimate-gutenberg-blocks-premium/plugin.php'] = 'Stackable';
+	$toc_plugins['stackable-ultimate-gutenberg-blocks/plugin.php'] = 'Stackable';
+ 	return $toc_plugins;
+} );
