8b654ff1be7c60a345a827716864e8adf28d737d
Benjamin Intal <bfintal@gmail.com>
2022-11-04 11:27:07 +0800

    refactored color control

--- src/components/color-palette-control/index.js
+++ src/components/color-palette-control/index.js
@@ -10,11 +10,18 @@ import { useControlHandlers } from '../base-control2/hooks'
 import { __ } from '@wordpress/i18n'
 import {
 	getColorObjectByColorValue,
-	__experimentalColorGradientSettingsDropdown as ColorGradientSettingsDropdown, // eslint-disable-line @wordpress/no-unsafe-wp-apis
 	useSetting,
 } from '@wordpress/block-editor'
 import { memo, useMemo } from '@wordpress/element'
 import { applyFilters } from '@wordpress/hooks'
+import {
+	Button,
+	ColorIndicator,
+	ColorPalette,
+	Dropdown,
+	FlexItem,
+	__experimentalHStack as HStack, // eslint-disable-line @wordpress/no-unsafe-wp-apis
+} from '@wordpress/components'
 
 /**
  * External dependencies
@@ -22,6 +29,13 @@ import { applyFilters } from '@wordpress/hooks'
 import { i18n } from 'stackable'
 import classnames from 'classnames'
 import { isPlainObject, compact } from 'lodash'
+import { ResetButton } from '../base-control2/reset-button'
+
+const popoverProps = {
+	placement: 'left-start',
+	offset: 36,
+	shift: true,
+}
 
 const ColorPaletteControl = memo( props => {
 	const {
@@ -69,32 +83,94 @@ const ColorPaletteControl = memo( props => {
 	const colorObject = getColorObjectByColorValue( colors, value )
 	const colorName = colorObject && colorObject.name
 
+	const toggleSettings = {
+		colorValue: value,
+		label: colorName || value,
+	}
+
 	return (
 		<AdvancedControl
 			{ ...controlProps }
 			className={ classnames( [ className, 'editor-color-palette-control', 'stk-color-palette-control' ] ) }
 			label={ label }
 		>
-			<ColorGradientSettingsDropdown
-				__experimentalIsRenderedInSidebar
-				settings={ [
-					{
-						colorValue: value,
-						label: colorName || value,
-						onColorChange: value => {
-							// Allow the selected color to be overridden.
-							const colorObject = getColorObjectByColorValue( colors, value )
-							onChange( applyFilters( 'stackable.color-palette-control.change', value, colorObject ) )
-						},
-						isShownByDefault: true,
-						enableAlpha: true,
-					},
-				] }
-				colors={ colors }
-				disableCustomColors={ disableCustomColors }
+			<Dropdown
+				popoverProps={ popoverProps }
+				className="block-editor-tools-panel-color-gradient-settings__dropdown"
+				renderToggle={ renderToggle( toggleSettings ) }
+				renderContent={ () => (
+					<div className="stk-color-palette-control__popover-content">
+						<ColorPalette
+							value={ value }
+							onChange={ value => {
+								// Allow the selected color to be overridden.
+								const colorObject = getColorObjectByColorValue( colors, value )
+								onChange( applyFilters( 'stackable.color-palette-control.change', value, colorObject ) )
+							} }
+							label={ colorName || value }
+							clearable={ false }
+							{ ...{ colors, disableCustomColors } }
+						/>
+					</div>
+				) }
+			/>
+			<ResetButton
+				allowReset={ props.allowReset }
+				value={ value }
+				default={ props.default }
+				onChange={ onChange }
 			/>
 		</AdvancedControl>
 	)
 } )
 
+ColorPaletteControl.defaultProps = {
+	allowReset: true,
+	default: '',
+
+	attribute: '',
+
+	value: undefined,
+	onChange: undefined,
+}
+
 export default ColorPaletteControl
+
+const renderToggle =
+	settings =>
+		( { onToggle, isOpen } ) => {
+			const { colorValue, label } = settings
+
+			const toggleProps = {
+				onClick: onToggle,
+				className: classnames(
+					'block-editor-panel-color-gradient-settings__dropdown',
+					{ 'is-open': isOpen }
+				),
+				'aria-expanded': isOpen,
+			}
+
+			return (
+				<Button { ...toggleProps }>
+					<LabeledColorIndicator
+						colorValue={ colorValue }
+						label={ label }
+					/>
+				</Button>
+			)
+		}
+
+const LabeledColorIndicator = ( { colorValue, label } ) => (
+	<HStack justify="flex-start">
+		<ColorIndicator
+			className="block-editor-panel-color-gradient-settings__color-indicator"
+			colorValue={ colorValue }
+		/>
+		<FlexItem
+			className="block-editor-panel-color-gradient-settings__color-name"
+			title={ label }
+		>
+			{ label }
+		</FlexItem>
+	</HStack>
+)
