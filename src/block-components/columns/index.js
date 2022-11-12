482bcb28da93e1be59fee6b41f09c5575d793bbd
Benjamin Intal <bfintal@gmail.com>
2022-11-12 15:03:16 +0800

    brought back stk--has-column-order class

--- src/block-components/columns/index.js
+++ src/block-components/columns/index.js
@@ -1,3 +1,4 @@
+import { addFilter } from '@wordpress/hooks'
 import { addAttributes } from './attributes'
 import { Edit } from './edit'
 import { Style } from './style'
@@ -12,3 +13,10 @@ Columns.Style = Style
 
 Columns.addAttributes = addAttributes
 
+// We need to add a special class for column orders.
+addFilter( 'stackable.block-components.block-div.classnames.content', 'stackable/column-order', ( classes, attributes ) => {
+	if ( attributes.columnArrangementMobile || attributes.columnArrangementTablet ) {
+		return [ ...classes, 'stk--has-column-order' ]
+	}
+	return classes
+} )
