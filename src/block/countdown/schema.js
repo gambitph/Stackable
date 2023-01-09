83b24eb0a4339683c317b7e33d4b8522aa41c170
prconcepcion <pioconcepcion3@yahoo.com>
2023-01-09 15:20:20 +0800

    added defaults and other necessary attributes

--- src/block/countdown/schema.js
+++ src/block/countdown/schema.js
@@ -4,35 +4,110 @@
 import { AttributeObject } from '~stackable/util'
 import { version as VERSION } from 'stackable'
 import {
-	 BlockDiv,
-	 Style,
-	 CustomCSS,
-	 Responsive,
-	 Advanced,
-	 Alignment,
-	 MarginBottom,
-	 CustomAttributes,
-	 EffectsAnimations,
-	 ConditionalDisplay,
-	 Transform,
+	BlockDiv,
+	Divider,
+	Style,
+	ContainerDiv,
+	CustomCSS,
+	Responsive,
+	Advanced,
+	Alignment,
+	MarginBottom,
+	CustomAttributes,
+	EffectsAnimations,
+	ConditionalDisplay,
+	Transform,
+	Typography,
 } from '~stackable/block-components'
 
 export const attributes = ( version = VERSION ) => {
-	 const attrObject = new AttributeObject()
+	const attrObject = new AttributeObject()
 
-	 BlockDiv.addAttributes( attrObject )
-	 Style.addAttributes( attrObject )
-	 EffectsAnimations.addAttributes( attrObject )
-	 CustomAttributes.addAttributes( attrObject )
-	 CustomCSS.addAttributes( attrObject )
-	 Responsive.addAttributes( attrObject )
-	 ConditionalDisplay.addAttributes( attrObject )
-	 Advanced.addAttributes( attrObject )
-	 Transform.addAttributes( attrObject )
-	 Alignment.addAttributes( attrObject )
-	 MarginBottom.addAttributes( attrObject )
+	Divider.addAttributes( attrObject )
+	BlockDiv.addAttributes( attrObject )
+	ContainerDiv.addAttributes( attrObject )
+	Style.addAttributes( attrObject )
+	EffectsAnimations.addAttributes( attrObject )
+	CustomAttributes.addAttributes( attrObject )
+	CustomCSS.addAttributes( attrObject )
+	Responsive.addAttributes( attrObject )
+	ConditionalDisplay.addAttributes( attrObject )
+	Advanced.addAttributes( attrObject )
+	Transform.addAttributes( attrObject )
+	Alignment.addAttributes( attrObject )
+	MarginBottom.addAttributes( attrObject )
 
-	 return attrObject.getMerged( version )
+	Typography.addAttributes( attrObject, 'stk-block-countdown__digit', {
+		hasTextTag: false,
+		hasTextContent: false,
+		attrNameTemplate: 'digit%s',
+	} )
+
+	Typography.addAttributes( attrObject, 'stk-block-countdown__label', {
+		hasTextTag: false,
+		hasTextContent: false,
+		attrNameTemplate: 'label%s',
+	} )
+
+	attrObject.add( {
+		attributes: {
+			days: { // The inner text `p` tag can be overridden.
+				type: 'string',
+				default: '',
+			},
+			hours: {
+				type: 'string',
+				default: '',
+			},
+			minutes: {
+				type: 'string',
+				default: '',
+			},
+			seconds: {
+				type: 'string',
+				default: '',
+			},
+			dayText: {
+				type: 'string',
+				default: '',
+			},
+			hourText: {
+				type: 'string',
+				default: '',
+			},
+			minuteText: {
+				type: 'string',
+				default: '',
+			},
+			secondText: {
+				type: 'string',
+				default: '',
+			},
+			endDate: {
+				type: 'string',
+				default: '',
+			},
+		},
+		versionAdded: '3.0.0',
+		versionDeprecated: '',
+	} )
+
+	attrObject.addDefaultValues( {
+		attributes: {
+			days: '0',
+			hours: '0',
+			minutes: '0',
+			seconds: '0',
+			dayText: 'Days',
+			hourText: 'Hours',
+			minuteText: 'Minutes',
+			secondText: 'Seconds',
+		},
+		versionAdded: '3.0.0',
+		versionDeprecated: '',
+	} )
+
+	return attrObject.getMerged( version )
 }
 
 export default attributes( VERSION )
