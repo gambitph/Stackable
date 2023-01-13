dd847c69b6103bd1b63880f7d4ea6278ed3af6ca
vmc08 <vmc08@yahoo.com>
2023-01-13 17:40:16 +0800

    chore (app): follow php code standard

--- src/css-optimize.php
+++ src/css-optimize.php
@@ -262,8 +262,8 @@ if ( ! class_exists( 'Stackable_CSS_Optimize' ) ) {
 			}
 
 			// Only do this to our blocks.
-			$blockName = isset($block['blockName']) ? $block['blockName'] : ''
-			if ( ! empty( $block ) && is_array( $block ) && stripos( $blockName, 'stackable/' ) === 0 ) {
+			$block_name = isset( $block['blockName'] ) ? $block['blockName'] : '';
+			if ( ! empty( $block ) && is_array( $block ) && stripos( $block_name, 'stackable/' ) === 0 ) {
 				if ( stripos( $block_content, '<style' ) !== false ) {
 
 					// We need the unique id for tracking.
