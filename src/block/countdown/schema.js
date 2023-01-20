ad72f3d9dd2c84dc7d6654b1264f50e7d68f23bd
prconcepcion <pioconcepcion3@yahoo.com>
2023-01-20 18:59:01 +0800

    separated attributes and added defaults

--- src/block/countdown/schema.js
+++ src/block/countdown/schema.js
@@ -26,6 +26,121 @@ import { date } from '@wordpress/date'
 
 const DEFAULT_DATE = date( 'Y-m-d\\TH:i', Date.now() + 345600000 )
 
+const countdownAttributes = {
+	countdownType: {
+		type: 'string',
+		selector: '[data-stk-countdown-type]',
+		source: 'attribute',
+		attribute: 'data-stk-countdown-type',
+		default: 'dueDate',
+	},
+	date: {
+		type: 'string',
+		selector: '[data-stk-countdown-date]',
+		source: 'attribute',
+		attribute: 'data-stk-countdown-date',
+		default: DEFAULT_DATE,
+	},
+	restartInterval: {
+		type: 'string',
+		selector: '[data-stk-countdown-restart-interval]',
+		source: 'attribute',
+		attribute: 'data-stk-countdown-restart-interval',
+		default: '',
+	},
+	actionOnExpiration: {
+		type: 'string',
+		selector: '[data-stk-countdown-action]',
+		source: 'attribute',
+		attribute: 'data-stk-countdown-action',
+		default: '',
+	},
+	timezone: {
+		type: 'string',
+		selector: '[data-stk-countdown-timezone]',
+		source: 'attribute',
+		attribute: 'data-stk-countdown-timezone',
+		default: '',
+	},
+	dayText: {
+		type: 'string',
+		selector: '.stk-block-countdown__digit_day .stk-block-countdown__label',
+		source: 'text',
+		default: __( 'Days', i18n ),
+	},
+	hourText: {
+		type: 'string',
+		selector: '.stk-block-countdown__digit_hour .stk-block-countdown__label',
+		source: 'text',
+		default: __( 'Hours', i18n ),
+	},
+	minuteText: {
+		type: 'string',
+		selector: '.stk-block-countdown__digit_minute .stk-block-countdown__label',
+		source: 'text',
+		default: __( 'Minutes', i18n ),
+	},
+	secondText: {
+		type: 'string',
+		selector: '.stk-block-countdown__digit_second .stk-block-countdown__label',
+		source: 'text',
+		default: __( 'Seconds', i18n ),
+	},
+	daysLeft: {
+		type: 'number',
+		default: '',
+	},
+	hoursLeft: {
+		type: 'number',
+		default: '',
+	},
+	minutesLeft: {
+		type: 'number',
+		default: '',
+	},
+	secondsLeft: {
+		type: 'number',
+		default: '',
+	},
+
+	boxGap: {
+		type: 'number',
+		default: '16',
+	},
+	labelMarginTop: {
+		type: 'number',
+		default: '8',
+	},
+	dayShow: {
+		type: 'boolean',
+		default: true,
+	},
+	hourShow: {
+		type: 'boolean',
+		default: true,
+	},
+	minuteShow: {
+		type: 'boolean',
+		default: true,
+	},
+	secondShow: {
+		type: 'boolean',
+		default: true,
+	},
+	digitFontSize: {
+		type: 'number',
+		default: '40',
+	},
+	dividerType: {
+		type: 'string',
+		default: ':',
+	},
+	is00: {
+		type: 'boolean',
+		default: true,
+	},
+}
+
 export const attributes = ( version = VERSION ) => {
 	const attrObject = new AttributeObject()
 
@@ -62,115 +177,7 @@ export const attributes = ( version = VERSION ) => {
 	} )
 
 	attrObject.add( {
-		attributes: {
-			countdownType: {
-				type: 'string',
-				selector: '[data-stk-countdown-type]',
-				source: 'attribute',
-				attribute: 'data-stk-countdown-type',
-				default: 'dueDate',
-			},
-			dayText: {
-				type: 'string',
-				selector: '.stk-block-countdown__digit_day .stk-block-countdown__label',
-				source: 'text',
-				default: __( 'Days', i18n ),
-			},
-			hourText: {
-				type: 'string',
-				selector: '.stk-block-countdown__digit_hour .stk-block-countdown__label',
-				source: 'text',
-				default: __( 'Hours', i18n ),
-			},
-			minuteText: {
-				type: 'string',
-				selector: '.stk-block-countdown__digit_minute .stk-block-countdown__label',
-				source: 'text',
-				default: __( 'Minutes', i18n ),
-			},
-			secondText: {
-				type: 'string',
-				selector: '.stk-block-countdown__digit_second .stk-block-countdown__label',
-				source: 'text',
-				default: __( 'Seconds', i18n ),
-			},
-			daysLeft: {
-				type: 'number',
-				default: '',
-			},
-			hoursLeft: {
-				type: 'number',
-				default: '',
-			},
-			minutesLeft: {
-				type: 'number',
-				default: '',
-			},
-			secondsLeft: {
-				type: 'number',
-				default: '',
-			},
-			date: {
-				type: 'string',
-				selector: '[data-stk-countdown-date]',
-				source: 'attribute',
-				attribute: 'data-stk-countdown-date',
-				default: DEFAULT_DATE,
-			},
-			restartInterval: {
-				type: 'string',
-				selector: '[data-stk-countdown-restart-interval]',
-				source: 'attribute',
-				attribute: 'data-stk-countdown-restart-interval',
-				default: '',
-			},
-			actionOnExpiration: {
-				type: 'string',
-				selector: '[data-stk-countdown-action]',
-				source: 'attribute',
-				attribute: 'data-stk-countdown-action',
-				default: '',
-			},
-			boxGap: {
-				type: 'number',
-				default: '',
-			},
-			labelMarginTop: {
-				type: 'number',
-				default: '',
-			},
-			timezone: {
-				type: 'string',
-				selector: '[data-stk-countdown-timezone]',
-				source: 'attribute',
-				attribute: 'data-stk-countdown-timezone',
-				default: '',
-			},
-			dayShow: {
-				type: 'boolean',
-				default: 'true',
-			},
-			hourShow: {
-				type: 'boolean',
-				default: 'true',
-			},
-			minuteShow: {
-				type: 'boolean',
-				default: 'true',
-			},
-			secondShow: {
-				type: 'boolean',
-				default: 'true',
-			},
-		},
-		versionAdded: '3.0.0',
-		versionDeprecated: '',
-	} )
-
-	attrObject.addDefaultValues( {
-		attributes: {
-			dividerType: ':',
-		},
+		attributes: countdownAttributes,
 		versionAdded: '3.0.0',
 		versionDeprecated: '',
 	} )
