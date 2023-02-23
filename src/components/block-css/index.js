59b7214bd6ed267225d40857b2703afa805b0ce5
Juan de Paco Moreno <admin@decodekult.com>
2023-02-23 12:04:41 +0100

    fix (css): added filter on the block unique classname for saving styles (#2589)
    * fix: filter on the block unique classname for saving styles
    
    Add a way to match the modified block unique class on styles and on HTML nodes.
    
    Ref: https://github.com/gambitph/Stackable/issues/2588
    
    * chore: apply filter on edit
    
    Filter the block css unique classname.

--- src/components/block-css/index.js
+++ src/components/block-css/index.js
@@ -345,6 +345,12 @@ const BlockCss = props => {
 		collapsedSelector = collapsedSelector.replace( /[^^?](.%s)([^-])/g, `$1-${ instanceId }$2` )
 	}
 
+	if ( ! props.editorMode ) {
+		blockUniqueClassName = applyFilters( 'stackable.block-css.uniqueClass.save', blockUniqueClassName, attributes )
+	} else {
+		blockUniqueClassName = applyFilters( 'stackable.block-css.uniqueClass.edit', blockUniqueClassName )
+	}
+
 	// Selectors can be arrays, flatten them.
 	if ( Array.isArray( selector ) ) {
 		selector = selector.join( ', ' )
@@ -410,6 +416,7 @@ const BlockCss = props => {
 	// the save function, the edit does the filter in the BlockCssEdit
 	// component.
 	if ( ! props.editorMode ) {
+		// Note that the original css value is always empty, see addCssToCssSaveObject.
 		css = applyFilters( 'stackable.block-styles.save', css, blockUniqueClassName, attributes )
 	}
 
