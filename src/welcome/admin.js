1db4f24e145ad6c162d4157719b3b41c9345d8df
DAnn2012 <dannunziata@hotmail.it>
2023-05-16 04:50:26 +0200

    fix: block names and descriptions are not translated in the Getting Started section (#2644)
    Thanks @DAnn2012!

--- src/welcome/admin.js
+++ src/welcome/admin.js
@@ -68,8 +68,8 @@ const BlockList = () => {
 										key={ i }
 										className="s-box"
 									>
-										<h4>{ block.title }</h4>
-										<p>{ block.description }</p>
+										<h4>{ __( block.title, i18n ) }</h4>
+										<p>{ __( block.description, i18n ) }</p>
 										{ block[ 'stk-demo' ] && <a href={ block[ 'stk-demo' ] } target="_example">{ __( 'See example', i18n ) }</a> }
 									</div>
 								)
