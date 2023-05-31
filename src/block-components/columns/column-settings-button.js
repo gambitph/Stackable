4ab6b1233f690b9cd4ae77b936c08acb24996175
mxkae <tapiamkr@gmail.com>
2023-05-31 11:34:47 +0800

    checks if input is blank, added timeout

--- src/block-components/columns/column-settings-button.js
+++ src/block-components/columns/column-settings-button.js
@@ -1,7 +1,5 @@
 import { i18n } from 'stackable'
-import {
-	last, min, pick,
-} from 'lodash'
+import { last, pick } from 'lodash'
 import { useBlockContext } from '~stackable/hooks'
 import { AdvancedRangeControl, Tooltip } from '~stackable/components'
 
@@ -18,89 +16,58 @@ export const ColumnsControl = ( { sliderMax = 6 } ) => {
 		numInnerBlocks, innerBlocks,
 	} = useBlockContext()
 	const [ isDuplicate, setIsDuplicate ] = useLocalStorage( 'stk__columns_new_duplicate', false )
-	const [ localNumInnerBlocks, setLocalNumInnerBlocks ] = useLocalStorage( 'stk__columns_num_inner_blocks' )
-	const [ localInnerBlocks, setLocalInnerBlocks ] = useLocalStorage( 'stk__columns_inner_blocks' )
+	let debounceTimeout
 
 	const setColumns = numColumns => {
 		const { insertBlock, removeBlocks } = dispatch( 'core/block-editor' )
 
-		// Remove the columns.
-		if ( numColumns < numInnerBlocks ) {
-			const columnClientIds = innerBlocks.slice( numColumns ).map( ( { clientId } ) => clientId )
-			removeBlocks( columnClientIds, false )
+		// prevents the previous setTimeouts from executing
+		clearTimeout( debounceTimeout )
+		// the timeout allows the user to finish typing before making changes to the number of columns
+		debounceTimeout = setTimeout( () => {
+			// do nothing if input field is blank
+			if ( numColumns === '' ) {
 
-			// backspacing sets numInnerBlocks to 0 and removes all columns
-			// this saves the deleted columns which can be retrieved later
-			setLocalNumInnerBlocks( numInnerBlocks )
-			setLocalInnerBlocks( innerBlocks )
+				// Remove the columns.
+			} else if ( numColumns < numInnerBlocks ) {
+				const columnClientIds = innerBlocks.slice( numColumns ).map( ( { clientId } ) => clientId )
+				removeBlocks( columnClientIds, false )
 
-		// Add a blank column.
-		} else if ( numColumns > numInnerBlocks && ! isDuplicate ) {
-			let numToAdd = numColumns - numInnerBlocks
-			let index = numInnerBlocks
+				// Add a blank column.
+			} else if ( numColumns > numInnerBlocks && ! isDuplicate ) {
+				const numToAdd = numColumns - numInnerBlocks
 
-			// gets the deleted columns from memory
-			if ( localNumInnerBlocks > 0 && numInnerBlocks === 0 ) {
-				// adjust the number of columns to add and the index to which the column will be inserted
-				numToAdd = numColumns - localNumInnerBlocks
-				index = localNumInnerBlocks
+				// add more empty columns if necessary
+				for ( let i = 0; i < numToAdd; i++ ) {
+					const block = getBlockFromExample( 'stackable/column', {} )
+					insertBlock( block, numInnerBlocks + i + 1, clientId, false )
+				}
 
-				// Retrieve columns based on the value of numColumns:
-				// - If numColumns is greater than the number of saved columns,
-				//   retrieve all saved columns.
-				// - Otherwise, if there are enough saved columns available,
-				//   retrieve only the first numColumns columns.
-				const minimum = min( [ numColumns, localNumInnerBlocks ] )
-				for ( let i = 0; i < minimum; i++ ) {
-					insertBlock( localInnerBlocks[ i ], i + 1, clientId, false )
+				// Duplicate the last column.
+			} else if ( numColumns > numInnerBlocks ) {
+				const numToAdd = numColumns - numInnerBlocks
+
+				// This is not guaranteed to have the latest attributes and values
+				const lastColumnBlock = last( innerBlocks )
+
+				// Retrieve block details to get the latest attributes and values,
+				// If there's no block, then use a blank column.
+				const newBlock = lastColumnBlock
+					? select( 'core/block-editor' ).getBlock( lastColumnBlock.clientId )
+					: {}
+
+				for ( let i = 0; i < numToAdd; i++ ) {
+					const block = getBlockFromExample( 'stackable/column', pick( newBlock, [ 'attributes', 'innerBlocks' ] ) )
+					insertBlock( block, numInnerBlocks + i + 1, clientId, false )
 				}
 			}
-
-			// add more empty columns if necessary
-			for ( let i = 0; i < numToAdd; i++ ) {
-				const block = getBlockFromExample( 'stackable/column', {} )
-				insertBlock( block, index + i + 1, clientId, false )
-			}
-
-		// Duplicate the last column.
-		} else if ( numColumns > numInnerBlocks ) {
-			let numToAdd = numColumns - numInnerBlocks
-			let index = numInnerBlocks
-
-			// This is not guaranteed to have the latest attributes and values
-			let lastColumnBlock = last( innerBlocks )
-
-			if ( localNumInnerBlocks > 0 && numInnerBlocks === 0 ) {
-				numToAdd = numColumns - localNumInnerBlocks
-				index = localNumInnerBlocks
-
-				// retrieve the last column of the saved columns
-				lastColumnBlock = last( localInnerBlocks )
-
-				const minimum = min( [ numColumns, localNumInnerBlocks ] )
-				for ( let i = 0; i < minimum; i++ ) {
-					insertBlock( localInnerBlocks[ i ], i + 1, clientId, false )
-				}
-			}
-
-			// Retrieve block details to get the latest attributes and values,
-			// If there's no block, then use a blank column.
-			const newBlock = lastColumnBlock
-				? select( 'core/block-editor' ).getBlock( lastColumnBlock.clientId )
-				: {}
-
-			for ( let i = 0; i < numToAdd; i++ ) {
-				const block = getBlockFromExample( 'stackable/column', pick( newBlock, [ 'attributes', 'innerBlocks' ] ) )
-				insertBlock( block, index + i + 1, clientId, false )
-			}
-		}
+		}, 750 )
 	}
 
 	return (
 		<AdvancedRangeControl
 			label={ __( 'Columns', i18n ) }
 			min={ 1 }
-			max={ sliderMax }
 			sliderMax={ sliderMax }
 			placeholder="1"
 			value={ numInnerBlocks }
