9bef288fd796283df0b2abae2663eea6bffb7753
prconcepcion <pioconcepcion3@yahoo.com>
2023-07-20 19:34:13 +0800

    added vw unit

--- src/block/card/edit.js
+++ src/block/card/edit.js
@@ -51,8 +51,8 @@ import { __ } from '@wordpress/i18n'
 
 const TEMPLATE = variations[ 0 ].innerBlocks
 
-const widthUnit = [ 'px' ]
-const heightUnit = [ 'px' ]
+const widthUnit = [ 'px', 'vw' ]
+const heightUnit = [ 'px', 'vh' ]
 
 const Edit = props => {
 	const {
