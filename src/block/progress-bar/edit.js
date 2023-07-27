3cc9ac359873630f7bd7662c1d499ec330e31874
Benjamin Intal <bfintal@gmail.com>
2023-07-27 14:39:43 +0800

    Update src/block/progress-bar/edit.js [skip ci]

--- src/block/progress-bar/edit.js
+++ src/block/progress-bar/edit.js
@@ -139,6 +139,7 @@ const Edit = props => {
 						</div>
 					</div>
 				</div>
+				{ /* Add our progress style here because we're adjusting the value using a hook */ }
 				<style>
 					{ `.editor-styles-wrapper .stk-${ props.attributes.uniqueId } .stk-progress-bar { --progress-value:${ derivedValue }% !important; }` }
 				</style>
