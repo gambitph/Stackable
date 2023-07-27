4678c160481ada5951f5b66be1576928eade6f83
Benjamin Intal <bfintal@gmail.com>
2023-07-27 15:07:53 +0800

    fix (popover): close popovers on escape (#2767)
    Co-authored-by: Benjamin Intal <benjaminintal@Benjamins-MacBook-Pro.local>

--- src/components/popover/index.js
+++ src/components/popover/index.js
@@ -1,15 +1,31 @@
 import { Popover as Popover_ } from '@wordpress/components'
 import classNames from 'classnames'
 
+const NOOP = () => {}
+
 const Popover = props => {
 	return <Popover_
 		{ ...props }
 		className={ classNames( 'stk-popover', props.className ) }
+		onKeyDown={ ev => {
+			// Hitting escape blurs the block, so we need to prevent that.
+			if ( ev.keyCode === 27 ) {
+				ev.preventDefault()
+				ev.stopPropagation()
+				props.onEscape()
+				return
+			}
+
+			if ( props.onKeyDown ) {
+				props.onKeyDown( ev )
+			}
+		} }
 	/>
 }
 
 Popover.defaultProps = {
 	className: '',
+	onEscape: NOOP,
 }
 
 export default Popover
