2ec212990ee509ddfca885b123d68d9af3def2e2
mxkae <tapiamkr@gmail.com>
2023-08-08 13:07:27 +0800

    update detection for Safari

--- src/block-components/alignment/index.php
+++ src/block-components/alignment/index.php
@@ -21,7 +21,7 @@ if ( ! function_exists( 'stackable_load_alignment_frontend_polyfill_script' ) ) 
 		$load_polyfill = false;
 
 		// Safari <= 15.3
-		if ( stripos( $user_agent, 'Safari/' ) !== false && strpos($user_agent, 'Chrome/') === false ) {
+		if ( preg_match( '/Version\/(\d+(\.\d+)*) Safari\//', $user_agent ) ) {
 			$start = stripos( $user_agent, 'Version/' ) + 8;
 			$end = strpos( $user_agent, ' ', $start );
 			$safari_version = substr( $user_agent, $start, $end - $start );
