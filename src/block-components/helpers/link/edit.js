59fe03f5a0e9aeda8a0884c06a06a1358b698c3c
prconcepcion <pioconcepcion3@yahoo.com>
2023-08-17 18:18:34 +0800

    added hint if user tries to enable lightbox for google map link

--- src/block-components/helpers/link/edit.js
+++ src/block-components/helpers/link/edit.js
@@ -11,6 +11,10 @@ import {
  */
 import { __ } from '@wordpress/i18n'
 import { useAttributeEditHandlers } from '~stackable/hooks'
+import { useEffect, useState } from '@wordpress/element'
+
+// @see https://stackoverflow.com/questions/14729191/google-maps-link-validation-using-regex
+const GoogleMapRegex = /^https?\:\/\/(www\.)?google\.(com|[a-z]{1,3})\/maps\b/gm
 
 export const LinkControls = props => {
 	const {
@@ -24,6 +28,21 @@ export const LinkControls = props => {
 		getAttribute,
 		updateAttributeHandler,
 	} = useAttributeEditHandlers( props.attrNameTemplate )
+
+	const url = getAttribute( 'url' )
+	const lightbox = getAttribute( 'hasLightbox' )
+
+	const [ showGoogleMapHint, setShowGoogleMapHint ] = useState( false )
+
+	useEffect( () => {
+		if ( hasLightbox ) {
+			if ( GoogleMapRegex.test( url ) && lightbox ) {
+				setShowGoogleMapHint( true )
+			} else {
+				setShowGoogleMapHint( false )
+			}
+		}
+	}, [ url, lightbox ] )
 
 	return (
 		<>
@@ -39,14 +58,24 @@ export const LinkControls = props => {
 				checked={ getAttribute( 'newTab' ) }
 				onChange={ updateAttributeHandler( 'newTab' ) }
 			/>
-			{ hasLightbox &&
-				<AdvancedToggleControl
-					label={ __( 'Open Link in Lightbox', i18n ) }
-					help={ lightboxHelp }
-					checked={ getAttribute( 'hasLightbox' ) }
-					onChange={ updateAttributeHandler( 'hasLightbox' ) }
-				/>
-			}
+			{ hasLightbox && (
+				<>
+					<AdvancedToggleControl
+						label={ __( 'Open Link in Lightbox', i18n ) }
+						help={ lightboxHelp }
+						checked={ getAttribute( 'hasLightbox' ) }
+						onChange={ updateAttributeHandler( 'hasLightbox' ) }
+					/>
+					{ showGoogleMapHint && (
+						<div className="stk-inspector-hint__google-map-embedd stk-inspector-hint-layout">
+							<span>
+								{ __( 'Displaying a Google Map? Use the embed iframe URL instead. Need help finding it? Check out our ', i18n ) }
+								<a href="https://docs.wpstackable.com/article/528-how-to-add-a-google-map-in-a-lightbox?utm_source=inspector&utm_campaign=learnmore&utm_medium=gutenberg" target="_blank" rel="noreferrer">docs</a>.
+							</span>
+						</div>
+					) }
+				</>
+			) }
 			<AdvancedTextControl
 				label={ __( 'Link rel', i18n ) }
 				help={ __( 'Link relationship keywords, e.g. nofollow noreferrer prefetch', i18n ) }
