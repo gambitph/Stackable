a825060276bd7a5c641d665f793727a743abceda
Benjamin Intal <benjaminintal@Benjamins-MacBook-Pro.local>
2023-08-18 21:58:53 +0800

    fix (WP 6.3): desktop block widths not applied in the editor, stk-preview-device-desktop is not applied anymore to the editor

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import './global-settings'
+import './theme-block-size'
 import './design-library-button'
 import './layout-picker-reset'
 // import './v2-migration-popup' // Probably 1.5yrs of checking for backward compatibility is enough.
@@ -12,7 +13,6 @@ import { BlockLinking } from './block-linking'
 import { BlockHoverState } from './block-hover-state'
 import { ContentAlign } from './content-align'
 import { EditorDom } from './get-editor-dom'
-import { ThemeBlockSize } from './theme-block-size'
 
 /**
  * WordPress dependencies
@@ -30,7 +30,6 @@ registerPlugin( 'stackable-convert-to-container-button', { render: ConvertToCont
 registerPlugin( 'stackable-block-hover-state', { render: BlockHoverState } )
 registerPlugin( 'stackable-content-align', { render: ContentAlign } )
 registerPlugin( 'stackable-editor-dom', { render: EditorDom } )
-registerPlugin( 'stackable-theme-block-size', { render: ThemeBlockSize } )
 
 if ( devMode ) {
 	registerPlugin( 'stackable-block-attributes-get-button', { render: GetBlockAttributesButton } )
--- src/plugins/theme-block-size/index.js
+++ src/plugins/theme-block-size/index.js
@@ -3,20 +3,46 @@
  * blocks as defined in the current theme.json and adding them as custom CSS
  * properties for our blocks to pick up in the editor.
  */
+import { useDeviceType } from '~stackable/hooks'
 import { useSetting } from '@wordpress/block-editor'
+import domReady from '@wordpress/dom-ready'
+import { createRoot, useEffect } from '@wordpress/element'
+import { useSelect } from '@wordpress/data'
 
 export const ThemeBlockSize = () => {
 	const contentSize = useSetting( 'layout.contentSize' )
 	const wideSize = useSetting( 'layout.wideSize' )
+
+	const deviceType = useDeviceType()
+	const editorDom = useSelect( select => {
+		return select( 'stackable/editor-dom' ).getEditorDom()
+	} )
+
+	useEffect( () => {
+		const editorBody = editorDom?.closest( 'body' )
+		if ( editorBody ) {
+			editorBody.appendChild( themeBlockSizeWrapper )
+		}
+	}, [ deviceType, editorDom ] )
 
 	if ( ! contentSize && ! wideSize ) {
 		return null
 	}
 
 	return (
-		<style id="stk-theme-block-size">
+		<>
 			{ contentSize && `:root { --stk-block-width-default-detected: ${ contentSize }}` }
 			{ wideSize && `:root { --stk-block-width-wide-detected: ${ wideSize }}` }
-		</style>
+		</>
 	)
 }
+
+const themeBlockSizeWrapper = document?.createElement( 'style' )
+if ( themeBlockSizeWrapper ) {
+	themeBlockSizeWrapper.setAttribute( 'id', 'stk-theme-block-size' )
+}
+
+domReady( () => {
+	document?.body?.appendChild( themeBlockSizeWrapper )
+	createRoot( themeBlockSizeWrapper ).render( <ThemeBlockSize /> )
+} )
