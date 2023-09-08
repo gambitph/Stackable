37e876591c7922d7ba62f12e9731056987a14508
mxkae <tapiamkr@gmail.com>
2023-09-08 08:09:04 +0800

    load polyfill if Firefox or Safari <= 15.3

--- src/block-components/alignment/index.php
+++ src/block-components/alignment/index.php
@@ -10,7 +10,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $user_agent = ! empty( $_SERVER['HTTP_USER_AGENT'] ) ? $_SERVER['HTTP_USER_AGENT'] : '';
-if ( ! empty( $user_agent ) && stripos( $user_agent, 'Firefox/' ) !== false ) {
+$load_polyfill = false;
+
+// Safari <= 15.3
+if ( stripos( $user_agent, 'Version/' ) !== false && stripos( $user_agent, 'Safari/' ) !== false ) {
+	$start = stripos( $user_agent, 'Version/' ) + 8;
+	$end = strpos( $user_agent, ' ', $start );
+	$safari_version = substr( $user_agent, $start, $end - $start );
+
+	// Convert version string to an array of parts
+	$version_parts = explode( '.', $safari_version );
+
+	if (
+		// Safari < 15
+		( isset( $version_parts[ 0 ] ) && intval( $version_parts[ 0 ] ) < 15 ) ||
+		// Safari <= 15.3
+		( isset( $version_parts[ 0 ] ) && intval( $version_parts[ 0 ] ) == 15 &&
+			(
+				( isset( $version_parts[ 1 ] ) && intval( $version_parts[ 1 ] ) <= 3 ) ||
+				! isset( $version_parts[ 1 ] )
+			)
+		)
+	) {
+		$load_polyfill = true;
+	}
+} else if ( stripos( $user_agent, 'Firefox/' ) !== false ) {
+	$load_polyfill = true;
+}
+
+if ( ! empty( $user_agent ) && $load_polyfill ) {
 	if ( ! function_exists( 'stackable_render_block_alignment_frontend_polyfill' ) ) {
 
 		function stackable_render_block_alignment_frontend_polyfill ( $block_content, $block ) {
