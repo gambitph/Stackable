a33e0d5ab18bb48e5da74030e14904f74ae0618e
Benjamin Intal <benjaminintal@Benjamins-MacBook-Pro.local>
2023-09-13 18:01:53 +0800

    added support for new opacity in the number box block

--- src/block/number-box/deprecated.js
+++ src/block/number-box/deprecated.js
@@ -2,12 +2,33 @@ import { Save } from './save'
 import { attributes } from './schema'
 
 import { withVersion } from '~stackable/higher-order'
+import { deprecateBlockBackgroundColoropacity, deprecationBackgrounColorOpacity } from '~stackable/block-components'
 
 const deprecated = [
+	// Support the new combined opacity and color.
+	{
+		attributes: attributes( '3.11.9' ),
+		save: withVersion( '3.11.9' )( Save ),
+		isEligible: attributes => {
+			return deprecateBlockBackgroundColoropacity.isEligible( attributes ) ||
+				deprecationBackgrounColorOpacity.isEligible( 'shape%s' )( attributes )
+		},
+		migrate: attributes => {
+			let newAttributes = deprecateBlockBackgroundColoropacity.migrate( attributes )
+			newAttributes = deprecationBackgrounColorOpacity.migrate( 'shape%s' )( newAttributes )
+			return newAttributes
+		},
+	},
 	// Support new margin-top/bottom classes.
+	// Support the new combined opacity and color.
 	{
 		attributes: attributes( '3.7.9' ),
 		save: withVersion( '3.7.9' )( Save ),
+		migrate: attributes => {
+			let newAttributes = deprecateBlockBackgroundColoropacity.migrate( attributes )
+			newAttributes = deprecationBackgrounColorOpacity.migrate( 'shape%s' )( newAttributes )
+			return newAttributes
+		},
 	},
 ]
 export default deprecated
