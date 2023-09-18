eb778635f0c3e47212a38e4c1b2d8853abc6c314
Benjamin Intal <benjaminintal@Benjamins-MacBook-Pro.local>
2023-09-18 17:06:00 +0800

    fix: deleted old gradient-overlay code

--- plugin.php
+++ plugin.php
@@ -253,7 +253,6 @@ if ( sugb_fs()->is__premium_only() ) {
 // Deprecated.
 require_once( plugin_dir_path( __FILE__ ) . 'src/deprecated/native-global-colors.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'src/deprecated/navigation-panel-pre-enabled.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'src/block-components/image/deprecated/gradient-overlay.php' );
 
 /**
  * V2 Deprecated
