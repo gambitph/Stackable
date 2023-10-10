9a41cc6b140a4988db93646c8423c1673bbe21c0
mxkae <tapiamkr@gmail.com>
2023-10-11 00:49:02 +0800

    show % unit & actual value of resizable box

--- src/components/resizable-bottom-margin/index.js
+++ src/components/resizable-bottom-margin/index.js
@@ -51,6 +51,7 @@ const ResizableBottomMarginSingle = props => {
 	const { deviceType } = props
 	const { name } = useBlockEditContext()
 	const [ currentHeight, setCurrentHeight ] = useState( null )
+	const [ currentHeightLabel, setCurrentHeightLabel ] = useState( null )
 	const [ isResizing, setIsResizing ] = useState( false )
 
 	const [ snapWidths, setSnapWidths ] = useState( SNAP_HEIGHTS )
@@ -69,7 +70,26 @@ const ResizableBottomMarginSingle = props => {
 		'stk--is-tiny': ( props.value !== '' ? props.value : defaultBottomMargin ) < 5,
 	} )
 
-	const height = props.value || props.value === 0 ? props.value : defaultBottomMargin
+	const getHeightInPixels = val => {
+		const body = document.querySelector( 'iframe[name="editor-canvas"]' ).contentWindow.document.body
+		const block = body.querySelector( `${ props.previewSelector }` )
+		if ( block ) {
+			const parent = block.parentElement
+
+			// use parentWidth because https://medium.com/coding-blocks/css-padding-a-magic-wand-2b8a66b84ebe#:~:text=Margin%20and%20Padding%20in%20percentages,height%20of%20the%20parent%20element.
+			const parentWidth = parseFloat( getComputedStyle( parent ).width )
+
+			// Convert the margin-bottom percentage to pixels
+			val = ( val / 100 ) * parentWidth
+		}
+		return val
+	}
+
+	const unit = props.value || props.value === 0 ? props.unit : 'px'
+	const heightLabel = props.value || props.value === 0 ? props.value : defaultBottomMargin
+	const height =
+	// heightLabel
+	 unit === '%' ? getHeightInPixels( heightLabel ) : heightLabel
 
 	return (
 		<ResizableBox
@@ -82,10 +102,12 @@ const ResizableBottomMarginSingle = props => {
 			snapGap={ 5 }
 			onResizeStart={ () => {
 				setCurrentHeight( height )
+				setCurrentHeightLabel( heightLabel )
 				setIsResizing( true )
 			} }
 			onResize={ ( _event, _direction, elt, delta ) => {
-				setCurrentHeight( height + delta.height )
+				setCurrentHeight( getHeightInPixels( heightLabel + delta.height ) )
+				setCurrentHeightLabel( heightLabel + delta.height )
 
 				// Set snap widths. We need to do this here not on
 				// ResizeStart or it won't be used at first drag.
@@ -94,17 +116,19 @@ const ResizableBottomMarginSingle = props => {
 				}
 			} }
 			onResizeStop={ () => {
-				props.onChange( parseInt( currentHeight, 10 ) === parseInt( defaultBottomMargin, 10 ) ? '' : parseInt( currentHeight, 10 ) )
+				props.onChange( parseInt( currentHeightLabel, 10 ) === parseInt( defaultBottomMargin, 10 ) ? '' : parseInt( currentHeightLabel, 10 ) )
+
 				setCurrentHeight( null )
+				setCurrentHeightLabel( null )
 				setIsResizing( false )
 			} }
 		>
 			{ props.previewSelector && isResizing &&
 				// .editor-styles-wrapper so that our styles will override the currently set margin.
-				<style>{ `.editor-styles-wrapper ${ props.previewSelector } { margin-bottom: ${ currentHeight }px !important; }` }</style>
+				<style>{ `.editor-styles-wrapper ${ props.previewSelector } { margin-bottom: ${ currentHeightLabel }${ unit } !important; }` }</style>
 			}
 			<span className="stk-resizable-bottom-margin__label">
-				{ `${ isResizing ? currentHeight : height }px` }
+				{ `${ isResizing ? currentHeightLabel : heightLabel }${ unit }` }
 			</span>
 		</ResizableBox>
 	)
@@ -120,20 +144,26 @@ const ResizableBottomMargin = props => {
 	const setAttributes = useBlockSetAttributesContext()
 	const deviceType = useDeviceType()
 	const attrName = getAttributeName( props.attribute, deviceType )
+	const unitAttrName = getAttributeName( `${ props.attribute }Unit`, deviceType )
 
 	let value = props[ attrName ]?.bottom
+	let unit = props[ unitAttrName ]
 
 	if ( deviceType === 'Mobile' ) {
 		if ( typeof value === 'undefined' || value === '' ) {
 			const attrNameTablet = getAttributeName( props.attribute, 'Tablet' )
+			const unitAttrNameTablet = getAttributeName( `${ props.attribute }Unit`, 'Tablet' )
 			value = props[ attrNameTablet ]?.bottom
+			unit = props[ unitAttrNameTablet ]
 		}
 	}
 
 	if ( deviceType === 'Tablet' || deviceType === 'Mobile' ) {
 		if ( typeof value === 'undefined' || value === '' ) {
 			const attrNameDesktop = getAttributeName( props.attribute, 'Desktop' )
+			const unitAttrNameDesktop = getAttributeName( `${ props.attribute }Unit`, 'Desktop' )
 			value = props[ attrNameDesktop ]?.bottom
+			unit = props[ unitAttrNameDesktop ]
 		}
 	}
 
@@ -151,6 +181,7 @@ const ResizableBottomMargin = props => {
 			deviceType={ deviceType }
 			previewSelector={ props.previewSelector }
 			value={ value }
+			unit={ unit }
 			onChange={ onChange }
 		/>
 	)
