9784140f1ea54e60588a40de4a9d5059c0a74b21
prconcepcion <pioconcepcion3@yahoo.com>
2023-10-31 13:31:53 +0800

    added file path

--- plugin.php
+++ plugin.php
@@ -227,6 +227,7 @@ if ( ! is_admin() ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'src/block-components/alignment/index.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'src/block/columns/index.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'src/block/timeline/index.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'src/block-components/progress-bar/index.php' );
 }
 
 /**
