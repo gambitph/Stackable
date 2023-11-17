fac868f50eb64f796efec9334cd0eb6b0c78346d
prconcepcion <pioconcepcion3@yahoo.com>
2023-11-17 15:05:47 +0800

    remove lazy loading from img if contained inside a carousel

--- src/block/carousel/index.php
+++ src/block/carousel/index.php
@@ -19,3 +19,26 @@ if ( ! function_exists( 'stackable_load_carousel_frontend_script' ) ) {
 	}
 	add_action( 'stackable/carousel/enqueue_scripts', 'stackable_load_carousel_frontend_script' );
 }
+
+if ( ! function_exists( 'stackable_carousel_add_class_images' ) ) {
+	function stackable_carousel_test($block_content, $block) {
+		$block_content = str_replace( "\"stk-img\"", "\"stk-img stk-img-carousel\"", $block_content );
+		return $block_content;
+	}
+
+	add_filter( 'render_block_stackable/carousel', 'stackable_carousel_test', 1, 2 );
+}
+
+if ( ! function_exists( 'stackable_skip_loading_lazy_carousel_image' ) ) {
+	function stackable_skip_loading_lazy_carousel_image( $value, $image, $context ) {
+		if ( ! strpos( $image, 'stk-img-carousel' ) === false ) {
+			return false;
+		}
+
+		return $value;
+	}
+
+	add_filter( 'wp_img_tag_add_loading_attr', 'stackable_skip_loading_lazy_carousel_image', 10, 3 );
+}
+
+
