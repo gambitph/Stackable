c9bf7f0c97b7be49e72eea291e385812fb1396d2
prconcepcion <pioconcepcion3@yahoo.com>
2023-11-29 14:14:09 +0800

    changed version

--- freemius/start.php
+++ freemius/start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.6.1';
+	$this_sdk_version = '2.6.0.1';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
