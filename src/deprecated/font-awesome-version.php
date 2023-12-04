33a237fb3cdd9e0d0a572a72f7d7ec6e997a3330
bfintal@gmail.com <>
2023-12-04 12:22:39 +0800

    fixed update version checking since this release will be at least 3.12.7

--- src/deprecated/font-awesome-version.php
+++ src/deprecated/font-awesome-version.php
@@ -16,7 +16,7 @@ if ( ! function_exists( 'stackable_font_awesome_icons_version_set_default' ) ) {
 	 *
 	 */
 	function stackable_font_awesome_icons_version_set_default( $old_version, $new_version ) {
-		if ( ! empty( $old_version ) && version_compare( $old_version, "3.12.6", "<" ) ) {
+		if ( ! empty( $old_version ) && version_compare( $old_version, "3.12.7", "<" ) ) {
 			// Use FontAwesome icons v5.15.4 for users upgrading from v3.12.6 and below.
 			if ( ! get_option( 'stackable_fa_icons_version' ) || get_option( 'stackable_fa_icons_version' ) === '5.15.4' ) {
 				update_option( 'stackable_fa_icons_version', '5.15.4' );
