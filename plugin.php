76c2cd640fca79f9d38e6bb52cec90925452d706
bfintal@gmail.com <>
2024-06-04 21:52:44 +0800

    chore: version bump to 3.13.0

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Author: Gambit Technologies, Inc
  * Author URI: http://gambit.ph
  * Text Domain: stackable-ultimate-gutenberg-blocks
- * Version: 3.13.0-beta1
+ * Version: 3.13.0
  *
  * @package Stackable
  */
