0bd0a5daee782e71c75d04c423b068c354129b22
mxkae <tapiamkr@gmail.com>
2024-07-02 13:09:19 +0800

    remove unused useCopy

--- src/block/icon-list-item/util.js
+++ src/block/icon-list-item/util.js
@@ -71,40 +71,6 @@ export const useOnSplit = ( clientId, attributes ) => {
 	}, [ clientId, attributes ] )
 }
 
-export const useCopy = clientId => {
-	const {
-		getBlockRootClientId, getBlockName, getBlockAttributes,
-	} =
-		useSelect( blockEditorStore )
-
-	return useRefEffect( node => {
-		function onCopy( event ) {
-			// The event propagates through all nested lists, so don't override
-			// when copying nested list items.
-			if ( event.clipboardData.getData( '__unstableWrapperBlockName' ) ) {
-				return
-			}
-
-			const rootClientId = getBlockRootClientId( clientId )
-			event.clipboardData.setData(
-				'__unstableWrapperBlockName',
-				getBlockName( rootClientId )
-			)
-			event.clipboardData.setData(
-				'__unstableWrapperBlockAttributes',
-				JSON.stringify( getBlockAttributes( rootClientId ) )
-			)
-		}
-
-		node.addEventListener( 'copy', onCopy )
-		node.addEventListener( 'cut', onCopy )
-		return () => {
-			node.removeEventListener( 'copy', onCopy )
-			node.removeEventListener( 'cut', onCopy )
-		}
-	}, [] )
-}
-
 export const useEnter = ( text, clientId ) => {
 	const {
 		removeBlocks, selectionChange, insertBlocks,
