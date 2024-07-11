00e24b72ba3aeedfff64950dfea4e9d1e9f135fc
Mikhaela Tapia <98727316+mxkae@users.noreply.github.com>
2024-07-11 16:40:47 +0800

    feat (theme fonts): display theme fonts in font picker (#3227)
    * display theme fonts in font picker
    
    * add index.php
    
    * do not load google font if using theme font
    
    * remove index.php
    
    * Update src/fonts.php [skip ci]
    
    * optimize fix
    
    ---------
    
    Co-authored-by: Benjamin Intal <bfintal@gmail.com>

--- src/hooks/use-font-loader.js
+++ src/hooks/use-font-loader.js
@@ -2,9 +2,16 @@ import { useEffect } from '@wordpress/element'
 import { doAction } from '@wordpress/hooks'
 import { loadGoogleFont } from '~stackable/util'
 
+import { useThemeFonts } from './use-theme-fonts'
+
 export const useFontLoader = fontFamilyValue => {
+	const { loadingThemeFont, themeFonts } = useThemeFonts()
 	useEffect( () => {
-		loadGoogleFont( fontFamilyValue )
-		doAction( 'stackable.font-loader.load', fontFamilyValue )
-	}, [ fontFamilyValue ] )
+		if ( ! loadingThemeFont ) {
+			if ( ! themeFonts.includes( fontFamilyValue ) ) {
+				loadGoogleFont( fontFamilyValue )
+				doAction( 'stackable.font-loader.load', fontFamilyValue )
+			}
+		}
+	}, [ loadingThemeFont, fontFamilyValue ] )
 }
