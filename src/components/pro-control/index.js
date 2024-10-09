357db74777089e4ea6ee4d82752ba2dde33e5f4a
bfintal@gmail.com <>
2024-10-09 10:49:56 +0800

    updated premium notice text

--- src/components/pro-control/index.js
+++ src/components/pro-control/index.js
@@ -110,8 +110,11 @@ const LABELS = {
 		</ul>,
 	},
 	'icon-library': {
+		title: __( 'Unlock Your Icon Library', i18n ),
 		description: <ul>
-			<li>{ __( 'Icon Library', i18n ) }</li>
+			<li>{ __( 'Add your custom SVG icons', i18n ) }</li>
+			<li>{ __( 'Easily access your custom icons in the icon picker', i18n ) }</li>
+			<li>{ __( 'Organize your custom icons in your library', i18n ) }</li>
 		</ul>,
 	},
 }
