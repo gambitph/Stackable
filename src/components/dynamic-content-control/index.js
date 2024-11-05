f6991442073ca3c4c18571f6c29dc5f95f3e2a2a
Benjamin Intal <bfintal@gmail.com>
2024-11-05 14:56:27 +0800

    Update src/components/dynamic-content-control/index.js [skip ci]

--- src/components/dynamic-content-control/index.js
+++ src/components/dynamic-content-control/index.js
@@ -499,6 +499,7 @@ export const useValueWithFieldsTitle = ( value = '' ) => {
 const dynamicContent = <SVGDatabaseIcon />
 
 export const DynamicContentButton = memo( props => {
+	// Hide the button if inside the Customizer
 	if ( select( 'core/customize-widgets' ) ) {
 		return null
 	}
