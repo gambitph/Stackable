aac312bcff37ef692fa4d4be77093acf099fa45d
mxkae <tapiamkr@gmail.com>
2024-12-18 00:17:21 +0800

    move condition

--- src/kses.php
+++ src/kses.php
@@ -12,11 +12,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  * In WooCommerce Shop page, <style> tags are stripped out and the CSS styles are displayed in the frontend.
  * This function removes the <style> tags and CSS styles before they are stripped out.
  */
-if ( ! function_exists( 'stackable_pre_kses_woocomerce_shop' ) && function_exists( 'wc_get_page_id' ) ) {
+if ( ! function_exists( 'stackable_pre_kses_woocomerce_shop' ) ) {
 
 	function stackable_pre_kses_woocomerce_shop( $content, $allowed_html, $context ) {
 		// Check if we are on the WooCommerce Shop page
-		if ( is_shop() ) {
+		if ( function_exists('is_shop' ) && is_shop() ) {
 			$optimized_css = get_post_meta( wc_get_page_id( 'shop' ), 'stackable_optimized_css', true );
 
 			// remove CSS before kses strips out <style> tags
