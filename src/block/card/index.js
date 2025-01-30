00db4561cf3427106713379b2bf0d97d02a9268a
bfintal@gmail.com <>
2025-01-30 19:13:41 +0800

    added card substitute

--- src/block/card/index.js
+++ src/block/card/index.js
@@ -17,6 +17,7 @@ import schema from './schema'
 import metadata from './block.json'
 import example from './example'
 import deprecated from './deprecated'
+import substitute from './substitute'
 
 /**
  * WordPress dependencies
@@ -40,4 +41,5 @@ export const settings = {
 	variations,
 	edit,
 	save,
+	substitute,
 }
---
+++ src/block/card/substitute.js
@@ -0,0 +1,26 @@
+export const substitute = {
+	from: 'stackable/card',
+	variants: [],
+	to: [ 'stackable/columns' ],
+	transform: ( oldAttributes, innerBlocks ) => {
+		return [
+			'stackable/columns',
+			{ uniqueId: 'test' },
+			[
+				[
+					'stackable/column',
+					{
+						align: oldAttributes.align,
+						hasContainer: true,
+					},
+					[
+						[ 'stackable/image', { imageUrl: oldAttributes.imageUrl } ],
+						...innerBlocks,
+					],
+				],
+			],
+		]
+	},
+}
+
+export default substitute
