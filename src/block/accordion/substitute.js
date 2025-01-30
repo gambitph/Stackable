0fce0755bbaa588905d293be0bcdd749fcb0bf73
bfintal@gmail.com <>
2025-01-30 19:04:27 +0800

    added accordion and cta

---
+++ src/block/accordion/substitute.js
@@ -0,0 +1,21 @@
+export const substitute = {
+	from: 'stackable/accordion',
+	variations: [ 'stackable/accordion' ],
+	to: 'core/details',
+	transform: ( oldAttributes, innerBlocks ) => {
+		// Get the heading
+		const heading = innerBlocks[ 0 ][ 2 ][ 0 ][ 2 ][ 0 ][ 1 ].text
+		const insideBlocks = innerBlocks[ 1 ][ 2 ]
+
+		return [
+			'core/details',
+			{
+				open: false,
+				summary: heading,
+			},
+			[ insideBlocks ],
+		]
+	},
+}
+
+export default substitute
