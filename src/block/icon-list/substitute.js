9f65930085d3ed3bfffcb9ac4691ff4310f46987
Alquen Sarmiento <sarmientoalquen@gmail.com>
2025-01-31 14:52:46 +0800

    fix: change dummy text for iconlist

--- src/block/icon-list/substitute.js
+++ src/block/icon-list/substitute.js
@@ -5,9 +5,9 @@ export const substitute = {
 			'core/list',
 			{},
 			[
-				[ 'core/list-item', { content: 'First line of content' } ],
-				[ 'core/list-item', { content: 'Second line of content' } ],
-				[ 'core/list-item', { content: 'Third line of content' } ],
+				[ 'core/list-item', { content: 'First item list' } ],
+				[ 'core/list-item', { content: 'Second item list' } ],
+				[ 'core/list-item', { content: 'Third item list' } ],
 			],
 		]
 	},
