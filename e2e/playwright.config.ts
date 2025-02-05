bd3377486d37c57a5faec317d7d2cc8fff9e46f1
mxkae <tapiamkr@gmail.com>
2025-02-05 17:07:46 +0800

    comment out site editor tests, add reporter

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -32,7 +32,10 @@ export default defineConfig( {
 	// workers: process.env.CI ? 1 : undefined,
 	workers: 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	// reporter: 'html',
+	reporter: [
+		[ 'list' ],
+		[ 'html', { outputFolder: '../playwright-report' } ],
+	  ],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
