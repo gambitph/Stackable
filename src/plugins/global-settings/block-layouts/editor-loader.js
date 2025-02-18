5ec5a9994fcc4bc32c4c75133c86bc91e0e0aaf9
mxkae <tapiamkr@gmail.com>
2025-02-18 12:43:31 +0800

    hover state

--- src/plugins/global-settings/block-layouts/editor-loader.js
+++ src/plugins/global-settings/block-layouts/editor-loader.js
@@ -19,46 +19,64 @@ const renderGlobalStyles = ( blockLayouts, setStyles, breakDesktop = 1024, break
 	let css = ''
 
 	const desktopCss = []
-	const tabletMobileCss = []
 	const tabletCss = []
 	const mobileCss = []
 
-	const getDeclaration = ( property, value ) => {
-		if ( typeof value === 'object' ) {
-			return `${ property }: ${ value.top }px ${ value.right }px ${ value.left }px ${ value.bottom }px;`
+	const getUnit = ( property, state ) => {
+		return blockLayouts[ property ][ `${ state }Unit` ] ?? 'px'
+	}
+
+	const getValue = ( _property, state, value, unit ) => {
+		let property = _property
+
+		if ( state.indexOf( 'ParentHover' ) !== -1 ) {
+			property += '-parent-hover'
+		} else if ( state.indexOf( 'Hover' ) !== -1 ) {
+			property += '-hover'
 		}
 
-		return `${ property }: ${ value };`
+		if ( property.indexOf( 'shadow' ) !== -1 ) {
+			return `${ property }: ${ value };`
+		}
+
+		if ( typeof value === 'object' ) {
+			return `${ property }: ${ value.top }${ unit } ${ value.right }${ unit } ${ value.left }${ unit } ${ value.bottom }${ unit };`
+		}
+
+		return `${ property }: ${ value }${ unit };`
 	}
 
 	Object.keys( blockLayouts ).forEach( property => {
-		const desktop = blockLayouts[ property ].desktop
-		const tablet = blockLayouts[ property ].tablet
-		const mobile = blockLayouts[ property ].mobile
+		const values = Object.keys( blockLayouts[ property ] )
+			.filter( key => key.indexOf( 'Unit' ) === -1 )
+			.reduce( ( obj, key ) => {
+		  return {
+					...obj,
+					[ key ]: blockLayouts[ property ][ key ],
+		  }
+			}, {} )
 
-		if ( desktop ) {
-			desktopCss.push( getDeclaration( property, desktop ) )
-		}
+		Object.entries( values ).forEach( ( [ state, value ] ) => {
+			const unit = getUnit( property, state )
 
-		if ( tablet && mobile ) {
-			tabletCss.push( getDeclaration( property, tablet ) )
-		} else if ( tablet && ! mobile ) {
-			tabletMobileCss.push( getDeclaration( property, tablet ) )
-		}
+			if ( state.indexOf( 'desktop' ) !== -1 ) {
+				desktopCss.push( getValue( property, state, value, unit ) )
+			}
 
-		if ( mobile ) {
-			mobileCss.push( getDeclaration( property, mobile ) )
-		}
+			if ( state.indexOf( 'tablet' ) !== -1 ) {
+				tabletCss.push( getValue( property, state, value, unit ) )
+			}
+
+			if ( state.indexOf( 'mobile' ) !== -1 ) {
+				mobileCss.push( getValue( property, state, value, unit ) )
+			}
+		} )
 	} )
 
 	css += `:root { ${ compact( desktopCss ).join( '' ) }}`
 
-	if ( tabletMobileCss.length > 0 ) {
-		css += `@media screen and (max-width: ${ breakDesktop - 1 }px){ :root { ${ compact( tabletMobileCss ).join( '' ) }} }`
-	}
-
 	if ( tabletCss.length > 0 ) {
-		css += `@media screen and (min-width: ${ breakTablet }px) and (max-width: ${ breakDesktop - 1 }px){:root { ${ compact( tabletCss ).join( '' ) }}}`
+		css += `@media screen and (max-width: ${ breakDesktop - 1 }px){ :root { ${ compact( tabletCss ).join( '' ) }} }`
 	}
 
 	if ( mobileCss.length > 0 ) {
