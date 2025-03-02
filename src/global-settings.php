1522c145e599aacf2a2215e22352215d60450078
Alquen Sarmiento <sarmientoalquen@gmail.com>
2025-03-02 16:52:46 +0800

    fix: use new scraped font pairs; adjust code to simpler json structure

--- src/global-settings.php
+++ src/global-settings.php
@@ -309,17 +309,19 @@ if ( ! class_exists( 'Stackable_Global_Settings' ) ) {
 									'name' => array(
 										'type' => 'string',
 									),
-									'value' => array(
-										'type' => 'array',
-										'items' => array(
-											'type' => 'object',
-											'properties' => array(
-												'selector' => array(
-													'type' => 'string',
-												),
-												'styles' => $stackable_global_typography_schema
-											),
-										),
+									'typography' => array(
+										'type' => 'object',
+										'properties' => array(
+											'h1' => $stackable_global_typography_schema,
+											'h2' => $stackable_global_typography_schema,
+											'h2' => $stackable_global_typography_schema,
+											'h3' => $stackable_global_typography_schema,
+											'h4' => $stackable_global_typography_schema,
+											'h5' => $stackable_global_typography_schema,
+											'h6' => $stackable_global_typography_schema,
+											'p' => $stackable_global_typography_schema,
+											'.stk-subtitle' => $stackable_global_typography_schema,
+										)
 									),
 								)
 							)
