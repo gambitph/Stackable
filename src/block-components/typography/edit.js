2cc5caadfe454d992849088de29f0f29724d1e2d
Alquen Sarmiento <sarmientoalquen@gmail.com>
2025-03-09 16:43:33 +0800

    fix: add placeholder

--- src/block-components/typography/edit.js
+++ src/block-components/typography/edit.js
@@ -165,6 +165,7 @@ export const Controls = props => {
 					label={ __( 'Font Family', i18n ) }
 					onChange={ updateAttributeHandler( 'fontFamily' ) }
 					value={ getAttribute( 'fontFamily' ) }
+					placeholder="Theme Default"
 					helpTooltip={ {
 						video: 'typography-family',
 						description: __( 'Sets the font set to be used for the element', i18n ),
