d840726e2ce378106a3e374a0813d86b90d7c578
Alquen Sarmiento <sarmientoalquen@gmail.com>
2025-03-18 17:04:53 +0800

    feat: generate global css for presets

---
+++ src/plugins/global-settings/preset-controls/index.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Size and Spacing Preset Controls
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Stackable_Size_And_Spacing_Preset_Controls' ) ) {
+
+	/**
+	 * Size and Spacing Preset Controls
+	 */
+    class Stackable_Size_And_Spacing_Preset_Controls {
+
+		public $stackable_presets;
+		public $theme_presets;
+
+		/**
+		 * Initialize
+		 */
+  		function __construct() {
+			$this->stackable_presets = $this->load_presets( __DIR__ . '/presets.json');
+			$this->theme_presets = $this->load_presets( get_template_directory() . '/theme.json' );
+			// Register our settings.
+			// add_action( 'register_stackable_global_settings', array( $this, 'register_preset_controls' ) );
+
+			add_filter( 'stackable_inline_styles_nodep', array( $this, 'add_preset_controls_styles' ) );
+		}
+
+		/**
+		 * Register the settings we need for preset controls
+		 *
+		 * @return void
+		 */
+		// public function register_preset_controls() {
+			
+		// }
+
+		public function sanitize_array_setting( $input ) {
+			return ! is_array( $input ) ? array( array() ) : $input;
+		}
+
+		private function load_presets( $json_path ) {
+			if ( file_exists( $json_path ) ) {
+				$json_data = file_get_contents( $json_path );
+				$decoded_data = json_decode( $json_data, true );
+				return $decoded_data[ 'settings' ] ?? [];
+			}
+			return [];
+		}
+
+		// Generate CSS variables based on preset type (e.g., fontSizes, spacing)
+		public function generate_css_variables( $presets, $prefix ) {
+			$css = "";
+			foreach ( $presets as $preset ) {
+				$slug = $preset[ 'slug' ];
+				$size = $preset[ 'size' ];
+				$css .= "--stk--preset--$prefix--$slug: $size;\n";
+			}
+	
+			return $css;
+		}
+
+		/**
+		 * Add our global preset control styles.
+		 *
+		 * @param String $current_css
+		 * @return String
+		 */
+		public function add_preset_controls_styles( $current_css ) {
+			$presets = $this->stackable_presets;
+			
+			if ( isset ( $this->theme_presets ) ) {
+				$presets = $this->theme_presets;
+			}
+	
+			$generated_css = ":root {\n";
+			$generated_css .= $this->generate_css_variables( $presets[ 'spacing' ][ 'spacingSizes' ], 'spacing' );
+			$generated_css .= $this->generate_css_variables( $presets[ 'typography' ][ 'fontSizes' ], 'font-size' );
+			$generated_css .= "}\n";
+
+			if ( ! $generated_css ) {
+				return $current_css;
+			}
+	
+			$current_css .= $generated_css;
+			return apply_filters( 'stackable_frontend_css' , $current_css );
+		}
+	}
+
+	new Stackable_Size_And_Spacing_Preset_Controls();
+}
