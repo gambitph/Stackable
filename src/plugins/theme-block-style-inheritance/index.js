e0bb85e671a1bbd5b1df34df9cc16cdff45146e8
Mikhaela Tapia <98727316+mxkae@users.noreply.github.com>
2025-05-15 20:28:21 +0800

    Feat: theme.json block style inheritance (#3490)
    * theme block style inheritance
    
    * added doc comment and changed description
    
    * update version, fix styles in editor
    
    * add theme compatibility for twenty twenty-five
    
    * fix php error
    
    * Added caching
    
    * updated version to 3.16.0
    
    ---------
    
    Co-authored-by: bfintal@gmail.com <>

---
+++ src/plugins/theme-block-style-inheritance/index.js
@@ -0,0 +1,80 @@
+/**
+ * External dependencies
+ */
+import { fetchSettings } from '~stackable/util'
+
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useState } from '@wordpress/element'
+import { useSelect } from '@wordpress/data'
+import { registerPlugin } from '@wordpress/plugins'
+
+// Adds a body class for block style inheritance
+const ThemeBlockStyleInheritanceClass = () => {
+	const [ disableBlockStyleInheritance, setDisableBlockStyleInheritance ] = useState( true )
+	const editorEl = useSelect( select => {
+		return select( 'stackable/editor-dom' ).getEditorDom()
+	}, [] )
+
+	useEffect( () => {
+		fetchSettings().then( response => {
+			setDisableBlockStyleInheritance( response.stackable_disable_block_style_inheritance )
+		} )
+	}, [] )
+
+	// Update the editor class
+	useEffect( () => {
+		if ( editorEl ) {
+			// Add block style inheritance class
+			if ( ! disableBlockStyleInheritance && editorEl.classList.contains( `stk-has-block-style-inheritance` ) === false ) {
+				editorEl.classList.add( `stk-has-block-style-inheritance` )
+			}
+
+			if ( disableBlockStyleInheritance ) {
+				editorEl.classList.remove( `stk-has-block-style-inheritance` )
+			}
+
+			// At first load of the editor, the block style inheritance class is removed, so we have to re-add it.
+			const mo = onClassChange( editorEl, () => {
+				if ( ! disableBlockStyleInheritance && editorEl.classList.contains( `stk-has-block-style-inheritance` ) === false ) {
+					editorEl.classList.add( `stk-has-block-style-inheritance` )
+				}
+
+				if ( disableBlockStyleInheritance ) {
+					editorEl.classList.remove( `stk-has-block-style-inheritance` )
+				}
+			} )
+
+			return () => mo.disconnect()
+		}
+	}, [ editorEl, disableBlockStyleInheritance ] )
+
+	return null
+}
+
+registerPlugin( 'stackable-theme-block-style-inheritance-class', {
+	render: ThemeBlockStyleInheritanceClass,
+} )
+
+// Listener when a class is changed on an element.
+const onClassChange = ( node, callback ) => {
+	let lastClassString = node.classList.toString()
+
+	const mutationObserver = new MutationObserver( mutationList => {
+		for ( const item of mutationList ) {
+			if ( item.attributeName === 'class' ) {
+				const classString = node.classList.toString()
+				if ( classString !== lastClassString ) {
+					callback( mutationObserver )
+					lastClassString = classString
+					break
+				}
+			}
+	  }
+	} )
+
+	mutationObserver.observe( node, { attributes: true } )
+
+	return mutationObserver
+}
