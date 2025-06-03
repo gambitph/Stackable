9bca657c219ca1cfee5e6b68c9c596f873a351c5
Alquen Sarmiento <sarmientoalquen@gmail.com>
2025-06-03 20:06:28 +0800

    fix: move db lookup

--- src/plugins/global-settings/preset-controls/index.php
+++ src/plugins/global-settings/preset-controls/index.php
@@ -87,11 +87,10 @@ if ( ! class_exists( 'Stackable_Size_And_Spacing_Preset_Controls' ) ) {
 		 * @since 3.16.0
 		 */
 		public function migrate_global_typography_font_size( $old_version, $new_version ) {
-			$typography_option = get_option( 'stackable_global_typography' );
-
 			if ( ! empty( $old_version ) && version_compare( $old_version, "3.16.0", "<" )
 				&& ! empty( $typography_option ) && isset( $typography_option[ 0 ] )
 			) {
+				$typography_option = get_option( 'stackable_global_typography' );
 				$typography = $typography_option[ 0 ];
 				$updated = false;
 
