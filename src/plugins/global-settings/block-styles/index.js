16d326fc8adae04313c536a07f4788813b184460
Mikhaela Tapia <mikhaelatapia@gmail.com>
2025-07-04 20:17:37 +0800

    update block attributes on page load

--- src/plugins/global-settings/block-styles/index.js
+++ src/plugins/global-settings/block-styles/index.js
@@ -14,20 +14,32 @@ import {
 	PanelAdvancedSettings,
 	ProControl,
 } from '~stackable/components'
+import {
+	getFilteredAttributes, STACKABLE_FILTERS, isBlockStyleAttributesModified,
+} from '~stackable/util'
 
 /**
  * WordPress dependencies
  */
 import { addFilter, applyFilters } from '@wordpress/hooks'
-import { Fragment, useState } from '@wordpress/element'
+import {
+	Fragment, useState, useEffect,
+} from '@wordpress/element'
+import { dispatch, select } from '@wordpress/data'
 import { __ } from '@wordpress/i18n'
+import { getBlockType } from '@wordpress/blocks'
 
 export { GlobalBlockStyles } from './editor-loader'
 
-if ( showProNotice || isPro ) {
-	addFilter( 'stackable.global-settings.inspector', 'stackable/global-block-styles', output => {
-		const [ isOpen, setIsOpen ] = useState( false )
+addFilter( 'stackable.global-settings.inspector', 'stackable/global-block-styles', output => {
+	const [ isOpen, setIsOpen ] = useState( false )
 
+	// Update block attributes if there have been changes with the block styles
+	useEffect( () => {
+		setTimeout( updateAllBlocksWithBlockStyle, 500 )
+	}, [] )
+
+	if ( showProNotice || isPro ) {
 		return (
 			<Fragment>
 				{ output }
@@ -43,5 +55,71 @@ if ( showProNotice || isPro ) {
 				</PanelAdvancedSettings>
 			</Fragment>
 		)
-	}, 8 )
+	}
+
+	return <Fragment />
+}, 8 )
+
+const updateAllBlocksWithBlockStyle = () => {
+	const clientIds = []
+	const clientIdAttributeMap = {}
+
+	const allBlockStyles = select( 'stackable/global-block-styles' ).getAllBlockStyles()
+
+	if ( ! Object.keys( allBlockStyles ).length ) {
+		return
+	}
+
+	const defaultBlockSettings = Object.keys( allBlockStyles ).reduce( ( output, blockName ) => {
+		const blockAttrs = getBlockType( blockName ).attributes
+		const blockAttrsFilter = [ ...( STACKABLE_FILTERS[ blockName ] || [] ), 'uniqueId' ]
+		const defaultBlockAttrs = getFilteredAttributes( blockAttrs, blockAttrsFilter )
+
+		const blockStyles = allBlockStyles[ blockName ].reduce( ( styles, blockStyle, index ) => {
+			styles[ blockStyle.slug ] = index
+			return styles
+		}, {} )
+		output[ blockName ] = {
+			blockAttributes: defaultBlockAttrs,
+			blockStyles,
+		}
+		return output
+	}, {} )
+
+	const updateBlockAttributesRecursive = blocks => {
+		blocks.forEach( block => {
+			if ( ! block.name.startsWith( 'stackable/' ) ||
+				! block.attributes.blockStyle ||
+				( block.attributes.blockStyle && block.attributes.modifiedBlockStyle ) ||
+				! ( block.name in defaultBlockSettings &&
+					block.attributes.blockStyle in defaultBlockSettings[ block.name ].blockStyles
+				)
+			) {
+				return
+			}
+
+			const blockStyle = block.attributes.blockStyle
+
+			const blockStyleIndex = defaultBlockSettings[ block.name ].blockStyles[ blockStyle ]
+
+			if ( isBlockStyleAttributesModified( block.name, blockStyle, block.attributes ) ) {
+				clientIds.push( block.clientId )
+				clientIdAttributeMap[ block.clientId ] = {
+					...defaultBlockSettings[ block.name ].blockAttributes,
+					...allBlockStyles[ block.name ][ blockStyleIndex ].attributes,
+					blockStyle,
+				}
+			}
+
+			// Also adjust the inner blocks.
+			if ( block.innerBlocks && block.innerBlocks.length ) {
+				updateBlockAttributesRecursive( block.innerBlocks )
+			}
+		} )
+	}
+	updateBlockAttributesRecursive( select( 'core/block-editor' ).getBlocks() )
+
+	if ( clientIds.length ) {
+		dispatch( 'core/block-editor' ).updateBlockAttributes( clientIds, clientIdAttributeMap, true )
+	}
 }
