ba5035f04cb035cae4c2dea1599380ab99e7afdb
bfintal@gmail.com <>
2025-08-02 14:09:25 +0800

    fix: change import to require to prevent new files from being generated

--- src/deprecated/block-defaults/index.js
+++ src/deprecated/block-defaults/index.js
@@ -2,6 +2,7 @@ import { settings as stackableSettings } from 'stackable'
 
 // Conditionally import scripts
 if ( stackableSettings.stackable_enable_block_defaults ) {
-	import( './save-block' )
-	import( './global-settings' )
+	// Use require instead of dynamic import to avoid code splitting
+	require( './save-block' )
+	require( './global-settings' )
 }
