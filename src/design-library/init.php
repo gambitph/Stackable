9cc1006f281bd5a8bd0a15bd05fcf3ece20dc35f
Mikhaela Tapia <mikhaelatapia@gmail.com>
2025-08-04 15:11:39 +0800

    update url

--- src/design-library/init.php
+++ src/design-library/init.php
@@ -232,7 +232,7 @@ if ( ! class_exists( 'Stackable_Design_Library' ) ) {
 				$designs = array();
 				$content = null;
 
-				$response = wp_remote_get( self::get_cdn_url() . 'library-v4/library.json' );
+				$response = wp_remote_get( self::get_cdn_url() . 'library-v5/library.json' );
 
 				if ( is_wp_error( $response ) ) {
 					// Add our error message so we can see it in the network tab.
