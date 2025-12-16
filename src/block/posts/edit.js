ad1ed03798191a13fb56cf35d9707039ef89436c
Mikhaela Tapia <mikhaelatapia@gmail.com>
2025-12-16 19:30:27 +0800

    fix typo

--- src/block/posts/edit.js
+++ src/block/posts/edit.js
@@ -182,7 +182,7 @@ const Edit = props => {
 				blockState={ props.blockState }
 				contentOrderOptions={ contentOrderOptions }
 				contentOrder={ contentOrder }
-				DefaultContentOrder={ defaultContentOrder }
+				defaultContentOrder={ defaultContentOrder }
 				orderBy={ orderBy }
 				order={ order }
 				type={ type }
