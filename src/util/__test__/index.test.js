0aa79f69bc407f0adb6214175ceb60bf6f1652c8
Alquen Sarmiento <sarmientoalquen@gmail.com>
2026-07-01 11:16:27 +0800

    fix: update the test

--- src/util/__test__/index.test.js
+++ src/util/__test__/index.test.js
@@ -2,8 +2,21 @@
  * Internal dependencies
  */
 import {
-	hexToRgba, prependCSSClass, compileCSS,
+	hexToRgba, prependCSSClass, compileCSS, maskSensitiveValue,
 } from '../'
+
+describe( 'maskSensitiveValue', () => {
+	it( 'fully masks values with 12 or fewer characters', () => {
+		expect( maskSensitiveValue( '' ) ).toBe( '' )
+		expect( maskSensitiveValue( '123456' ) ).toBe( '******' )
+		expect( maskSensitiveValue( '123456789012' ) ).toBe( '************' )
+	} )
+
+	it( 'keeps the first 6 and last 6 characters for longer values', () => {
+		expect( maskSensitiveValue( '1234567890123' ) ).toBe( '123456*890123' )
+		expect( maskSensitiveValue( 'abcdefghijklmnopqrstuvwxyz' ) ).toBe( 'abcdef**************uvwxyz' )
+	} )
+} )
 
 describe( 'hexToRgba', () => {
 	it( 'should work', () => {
